ea0198388ee25d9f664b317bb9e8b9ac93f3ccb4
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-03-14 18:45:22 +0100

    Humour (bis)

--- AST2.ml
+++ AST2.ml
@@ -70,7 +70,7 @@ module O = struct
 
   and operator =
     Function    of var_name
-  | Construcor  of var_name
+  | Constructor of var_name
   | UpdateField of field_name
   | GetField    of field_name
   | Or | And | Lt | Leq | Gt | Geq | Equal | Neq | Cat | Cons | Add | Sub | Mult | Div | Mod
