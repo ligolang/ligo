afca07d888b4aa8f245a83571320f0de667fa24e
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-03-23 13:41:49 +0100

    This version of OCaml (4.06.1 with dune/opam) seems to not like shadowing vs. open
    The shadowing worked fine with a standalone ocaml 4.07.1 (no dune or opam use)

--- src/ligo/rope_test.ml
+++ src/ligo/rope_test.ml
@@ -2,27 +2,27 @@ module A = struct
   open Rope_top_level_open
   open Rope
 
-  let x = d
-  let x = ~%d
-  let x = (~%d) #% 42
-  let x = (~%d%d)
-  let x = (~%d%d) #% 42 43
-  let x = (~%d%s) #% 42 "foo"
-  let x = (~%(S"foo")%s) #% ""
-  let x = (~%d%S"tralala"%d%s) #% 42 43 "foo"
+  let _ = d
+  let _ = ~%d
+  let _ = (~%d) #% 42
+  let _ = (~%d%d)
+  let _ = (~%d%d) #% 42 43
+  let _ = (~%d%s) #% 42 "foo"
+  let _ = (~%(S"foo")%s) #% ""
+  let _ = (~%d%S"tralala"%d%s) #% 42 43 "foo"
 end
 
 module B = struct
   open Rope_top_level_open
 
-  type foo = S | NotCaptured
-  let d = NotCaptured
-  let s = NotCaptured
+  (* type foo = S | NotCaptured *)
+  (* let d = NotCaptured *)
+  (* let s = NotCaptured *)
 
-  let x = Rope.(~%d) #% 42
-  let x = Rope.(~%d%d)
-  let x = Rope.(~%d%d) #% 42 43
-  let x = Rope.(~%d%s) #% 42 "foo"
-  let x = Rope.(~%(S"foo")%s) #% ""
-  let x = Rope.(~%d%S"tralala"%d%s) #% 42 43 "foo"
+  let _ = Rope.(~%d) #% 42
+  let _ = Rope.(~%d%d)
+  let _ = Rope.(~%d%d) #% 42 43
+  let _ = Rope.(~%d%s) #% 42 "foo"
+  let _ = Rope.(~%(S"foo")%s) #% ""
+  let _ = Rope.(~%d%S"tralala"%d%s) #% 42 43 "foo"
 end
