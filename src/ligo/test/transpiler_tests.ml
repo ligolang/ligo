7ba31d4d7dde4140048047100c1f50afbd788333
Galfour <gabriel.alfour@gmail.com>
2019-03-24 18:53:20 +0000

    introspection

--- src/ligo/test/transpiler_tests.ml
+++ src/ligo/test/transpiler_tests.ml
@@ -3,7 +3,10 @@
  * open Combinators
  * open Test_helpers *)
 
-(* How should one test the transpiler? *)
+(*
+  How should one test the transpiler?
+  I'm doing the dumb thing.
+*)
 
-let main = "Transpiler (from Mini_C)", [
+let main = "Transpiler (from Ast_typed)", [
   ]
