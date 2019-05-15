2b4eaa29d1c4066ddaba047216e27719ea737581
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-05-15 16:44:45 +0200

    Rewrite local opam repository into a different folder

--- rewrite-local-opam-repository.sh
+++ rewrite-local-opam-repository.sh
@@ -2,7 +2,9 @@
 set -euET -o pipefail
 main(){
   root_dir="$(pwd | sed -e 's/\\/\\\\/' | sed -e 's/&/\\\&/' | sed -e 's/~/\\~/')"
-  cd vendors/ligo-opam-repository
+  rm -fr vendors/ligo-opam-repository-local
+  cp -a vendors/ligo-opam-repository vendors/ligo-opam-repository-local
+  cd vendors/ligo-opam-repository-local
   git grep -z -l src: | grep -z 'opam$' | xargs -0 \
     sed -i -e 's~src:  *"https://gitlab.com/gabriel.alfour/ligo/-/archive/master/ligo.tar.gz"~src: "file://'"$root_dir"'"~' \
            -e 's~src:  *"https://gitlab.com/gabriel.alfour/ligo-utils/-/archive/master/ligo-utils.tar.gz"~src: "file://'"$root_dir"'/vendors/ligo-utils"~' \
