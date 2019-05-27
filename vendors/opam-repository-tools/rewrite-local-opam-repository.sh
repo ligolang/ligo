5a13789a11ca8920c3e5cb47e6bbe0df9252c02f
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-05-27 13:11:57 +0200

    cleanup

--- vendors/opam-repository-tools/rewrite-local-opam-repository.sh
+++ vendors/opam-repository-tools/rewrite-local-opam-repository.sh
@@ -6,7 +6,6 @@ main(){
   mkdir vendors/ligo-opam-repository-local-generated
   cp -a index.tar.gz packages repo urls.txt vendors/ligo-opam-repository-local-generated
   cd vendors/ligo-opam-repository-local-generated
-  grep -r --null -l src: | grep -z 'opam$' | tr '\0' '\n'
   grep -r --null -l src: | grep -z 'opam$' | xargs -0 \
     sed -i -e 's~src:  *"https://gitlab.com/ligolang/ligo/-/archive/master/ligo\.tar\.gz"~src: "file://'"$root_dir"'"~'
   # TODO: run the update.sh script adequately to regenerate the index.tar.gz etc. in the local repo
