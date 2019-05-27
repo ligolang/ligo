15d1e0e580f25ac6da429a25088c49bfb0a564c8
Matej Sima <matej.sima@gmail.com>
2019-05-27 18:04:26 +0200

    Update paths to from gabriel.alfour to ligolang, add example ligo run with --help

--- scripts/installer.sh
+++ scripts/installer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # You can run this installer like this:
-# curl https://gitlab.com/gabriel.alfour/ligo/blob/master/scripts/installer.sh | bash
+# curl https://gitlab.com/ligolang/ligo/blob/master/scripts/installer.sh | bash
 # Make sure the marigold/ligo image is published at docker hub first 
 set -euET -o pipefail
 echo "Installing LIGO"
 
 # Install the ligo.sh from master
-wget https://gitlab.com/gabriel.alfour/ligo/blob/master/scripts/ligo.sh
+wget https://gitlab.com/ligolang/ligo/blob/master/scripts/ligo.sh
 
 # Copy the exucutable to the appropriate directory
 sudo cp ligo.sh /usr/local/bin/ligo
@@ -14,4 +14,5 @@ sudo chmod +x /usr/local/bin/ligo
 rm ligo.sh
 
 # Installation finished, try running 'ligo' from your CLI
-echo "Installation successful, try running 'ligo' now. \n"
+echo "Installation successful, trying to run 'ligo' now. \n"
+ligo --help
