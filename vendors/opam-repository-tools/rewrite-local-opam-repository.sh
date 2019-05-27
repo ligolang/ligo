b3b87543ba943f8f0e9407fede93ad91591e06ac
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-05-27 11:30:28 +0200

    Place the local copy of ligo in a -generated folder

--- vendors/opam-repository-tools/rewrite-local-opam-repository.sh
+++ vendors/opam-repository-tools/rewrite-local-opam-repository.sh
@@ -2,10 +2,10 @@
 set -euET -o pipefail
 main(){
   root_dir="$(pwd | sed -e 's/\\/\\\\/' | sed -e 's/&/\\\&/' | sed -e 's/~/\\~/')"
-  rm -fr vendors/ligo-opam-repository-local
-  mkdir vendors/ligo-opam-repository-local
-  cp -a index.tar.gz packages repo urls.txt vendors/ligo-opam-repository-local
-  cd vendors/ligo-opam-repository-local
+  rm -fr vendors/ligo-opam-repository-local-generated
+  mkdir vendors/ligo-opam-repository-local-generated
+  cp -a index.tar.gz packages repo urls.txt vendors/ligo-opam-repository-local-generated
+  cd vendors/ligo-opam-repository-local-generated
   git grep -z -l src: | grep -z 'opam$' | xargs -0 \
     sed -i -e 's~src:  *"https://gitlab.com/ligolang/ligo/-/archive/master/ligo.tar.gz"~src: "file://'"$root_dir"'"~'
   # TODO: run the update.sh script adequately to regenerate the index.tar.gz etc. in the local repo
