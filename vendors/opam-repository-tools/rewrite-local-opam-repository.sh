0880fb0cbf8cfacba137e800f39f15499fe0ef1f
Georges Dupéron <georges.duperon@nomadic-labs.com>
2019-05-27 13:10:29 +0200

    typo in sed script

--- vendors/opam-repository-tools/rewrite-local-opam-repository.sh
+++ vendors/opam-repository-tools/rewrite-local-opam-repository.sh
@@ -6,8 +6,9 @@ main(){
   mkdir vendors/ligo-opam-repository-local-generated
   cp -a index.tar.gz packages repo urls.txt vendors/ligo-opam-repository-local-generated
   cd vendors/ligo-opam-repository-local-generated
-  git grep -z -l src: | grep -z 'opam$' | xargs -0 \
-    sed -i -e 's~src:  *"https://gitlab.com/ligolang/ligo/-/archive/master/ligo.tar.gz"~src: "file://'"$root_dir"'"~'
+  grep -r --null -l src: | grep -z 'opam$' | tr '\0' '\n'
+  grep -r --null -l src: | grep -z 'opam$' | xargs -0 \
+    sed -i -e 's~src:  *"https://gitlab.com/ligolang/ligo/-/archive/master/ligo\.tar\.gz"~src: "file://'"$root_dir"'"~'
   # TODO: run the update.sh script adequately to regenerate the index.tar.gz etc. in the local repo
 }
 if main; then exit 0; else exit $?; fi
