a729ec95b46f2de0bb3564c86d35b398a45a4b6d
John David Pressman <jd@jdpressman.com>
2019-06-09 22:03:08 -0700

    Add tuareg to dev switch setup

--- scripts/setup_dev_switch.sh
+++ scripts/setup_dev_switch.sh
@@ -1,4 +1,4 @@
 opam switch create . ocaml-base-compiler.4.06.1
 eval $(opam env)
-opam install -y ocp-indent merlin alcotest-lwt crowbar
+opam install -y ocp-indent tuareg merlin alcotest-lwt crowbar
 opam -y user-setup install
