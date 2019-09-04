32599ae90bd39061e31aa15034f6f5b2d5e6d38d
galfour <gabriel.alfour@gmail.com>
2019-09-04 20:19:09 +0200

    removing tezos-modded

--- vendors/tezos-modded/src/lib_memory_protocol_alpha/memory_proto_alpha.ml
+++
@@ -1,8 +0,0 @@
-module Name = struct let name = "alpha" end
-module Alpha_environment = Tezos_protocol_environment_memory.MakeV1(Name)()
-
-type alpha_error = Alpha_environment.Error_monad.error
-type 'a alpha_tzresult = 'a Alpha_environment.Error_monad.tzresult
-module Alpha_error_monad = Alpha_environment.Error_monad
-module Proto = Tezos_protocol_alpha.Functor.Make(Alpha_environment)
-include Proto
