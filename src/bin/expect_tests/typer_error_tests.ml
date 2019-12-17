2a11c6d180c3b241396e4ff0f2ae6dd34d978b88
Suzanne Dupéron <georges.duperon@nomadic-labs.com>
2019-12-17 18:16:25 +0000

    test for typer error message: different keys

--- src/bin/expect_tests/typer_error_tests.ml
+++ src/bin/expect_tests/typer_error_tests.ml
@@ -9,3 +9,7 @@ let%expect_test _ =
 
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/error_typer_3.mligo" ; "foo" ] ;
   [%expect {| ligo: in file "error_typer_3.mligo", line 3, characters 34-53. tuples have different sizes: Expected these two types to be the same, but they're different (both are tuples, but with a different number of arguments) {"a":"tuple[int , string , bool]","b":"tuple[int , string]"} |} ] ;
+
+  run_ligo_bad [ "compile-contract" ; "../../test/contracts/error_typer_4.mligo" ; "foo" ] ;
+  [%expect {| ligo: in file "error_typer_4.mligo", line 4, characters 17-56. different keys in record:  {"key_a":"d","key_b":"c"} |} ] ;
+
