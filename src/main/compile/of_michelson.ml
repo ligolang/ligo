e834e2ac202a75d0eec2eca70a52d259bc5f535b
Lesenechal Remi <lesenechal.remi@gmail.com>
2019-12-31 19:14:42 +0100

    More informative errors when calling michelson typecheck function

--- src/main/compile/of_michelson.ml
+++ src/main/compile/of_michelson.ml
@@ -2,20 +2,45 @@ open Tezos_utils
 open Proto_alpha_utils
 open Trace
 
+module Errors = struct
+  let title_type_check_msg () = "Invalid contract"
+  let bad_parameter () =
+    let message () =
+      "bad contract parameter type (some michelson type are forbidden as contract parameter)" in
+    error title_type_check_msg message
+  let bad_storage () =
+    let message () =
+      "bad storage type (some michelson type are forbidden as contract storage)" in
+    error title_type_check_msg message
+  let bad_contract () =
+    let message () =
+      "bad contract type (contract entry point is expected to be of the form `parameter * storage -> list(operation) * storage`)" in
+    error title_type_check_msg message
+  let unknown () =
+    let message () =
+      "unknown error" in
+    error title_type_check_msg message
+end
+
 let build_contract : Compiler.compiled_expression -> Michelson.michelson result =
   fun compiled ->
   let%bind ((Ex_ty _param_ty),(Ex_ty _storage_ty)) = Self_michelson.fetch_contract_inputs compiled.expr_ty in
   let%bind param_michelson =
-    Trace.trace_tzresult_lwt (simple_error "Invalid contract: Could not unparse parameter") @@
+    Trace.trace_tzresult_lwt (simple_error "Could not unparse parameter") @@
     Proto_alpha_utils.Memory_proto_alpha.unparse_ty_michelson _param_ty in
   let%bind storage_michelson =
-    Trace.trace_tzresult_lwt (simple_error "Invalid contract: Could not unparse storage") @@
+    Trace.trace_tzresult_lwt (simple_error "Could not unparse storage") @@
     Proto_alpha_utils.Memory_proto_alpha.unparse_ty_michelson _storage_ty in
   let contract = Michelson.contract param_michelson storage_michelson compiled.expr in
-  let%bind () = 
-    Trace.trace_tzresult_lwt (simple_error "Invalid contract: Contract did not typecheck") @@
+  let%bind res = 
+    Trace.trace_tzresult_lwt (simple_error "Could not typecheck the code") @@
     Proto_alpha_utils.Memory_proto_alpha.typecheck_contract contract in
-  ok contract
+  match res with
+  | Type_checked  -> ok contract
+  | Err_parameter -> fail @@ Errors.bad_parameter ()
+  | Err_storage   -> fail @@ Errors.bad_storage ()
+  | Err_contract  -> fail @@ Errors.bad_contract ()
+  | Err_unknown   -> fail @@ Errors.unknown ()
 
 type check_type = Check_parameter | Check_storage
 let assert_equal_contract_type : check_type -> Compiler.compiled_expression -> Compiler.compiled_expression -> unit result =
