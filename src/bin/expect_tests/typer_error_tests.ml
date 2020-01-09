4ee75afb0a4a7fc4c97fc11c12ce7c13950f47f5
Suzanne Dupéron <ligo@suzanne.soy>
2020-01-09 19:14:10 +0100

    fix test: add helper blurb

--- src/bin/expect_tests/typer_error_tests.ml
+++ src/bin/expect_tests/typer_error_tests.ml
@@ -105,4 +105,13 @@ let%expect_test _ =
     * Check the changelog by running 'ligo changelog' |} ] ;
 
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/id.mligo" ; "main" ] ;
-  [%expect {| ligo: in file "id.mligo", line 45, characters 4-51. Expected a different type: Expected the type option but got the type record[profile -> bytes , owner -> address , controller -> address] |}] ;
+  [%expect {|
+    ligo: in file "id.mligo", line 45, characters 4-51. Expected a different type: Expected the type option but got the type record[profile -> bytes , owner -> address , controller -> address]
+
+     If you're not sure how to fix this error, you can
+     do one of the following:
+
+    * Visit our documentation: https://ligolang.org/docs/intro/what-and-why/
+    * Ask a question on our Discord: https://discord.gg/9rhYaEt
+    * Open a gitlab issue: https://gitlab.com/ligolang/ligo/issues/new
+    * Check the changelog by running 'ligo changelog' |}] ;
