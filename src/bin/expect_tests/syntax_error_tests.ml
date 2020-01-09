8e18c1f8bd4e432c63d3df306dc1d3611e5c9a75
Suzanne Dupéron <ligo@suzanne.soy>
2020-01-09 18:59:10 +0100

    update tests with the helper blurb

--- src/bin/expect_tests/syntax_error_tests.ml
+++ src/bin/expect_tests/syntax_error_tests.ml
@@ -4,4 +4,14 @@ let%expect_test _ =
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/error_syntax.ligo" ; "main" ] ;
   [%expect {|
     ligo: parser error: Parse error at "-" from (1, 16) to (1, 17). In file "|../../test/contracts/negative/error_syntax.ligo"
-           {"parser_loc":"in file \"\", line 1, characters 16-17"} |} ] ;
+           {"parser_loc":"in file \"\", line 1, characters 16-17"}
+
+
+     If you're not sure how to fix this error, you can
+     do one of the following:
+
+    * Visit our documentation: https://ligolang.org/docs/intro/what-and-why/
+    * Ask a question on our Discord: https://discord.gg/9rhYaEt
+    * Open a gitlab issue: https://gitlab.com/ligolang/ligo/issues/new
+    * Check the changelog by running 'ligo changelog' |} ] ;
+
--- src/bin/expect_tests/typer_error_tests.ml
+++ src/bin/expect_tests/typer_error_tests.ml
@@ -2,7 +2,16 @@ open Cli_expect
 
 let%expect_test _ =
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/error_type.ligo" ; "main" ] ;
-  [%expect {| ligo: in file "error_type.ligo", line 3, characters 18-28. Adding with wrong types. Expected nat, int or tez. |} ] ;
+  [%expect {|
+    ligo: in file "error_type.ligo", line 3, characters 18-28. Adding with wrong types. Expected nat, int or tez.
+
+     If you're not sure how to fix this error, you can
+     do one of the following:
+
+    * Visit our documentation: https://ligolang.org/docs/intro/what-and-why/
+    * Ask a question on our Discord: https://discord.gg/9rhYaEt
+    * Open a gitlab issue: https://gitlab.com/ligolang/ligo/issues/new
+    * Check the changelog by running 'ligo changelog' |} ] ;
 
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/error_typer_1.mligo" ; "main" ] ;
   [%expect {|
