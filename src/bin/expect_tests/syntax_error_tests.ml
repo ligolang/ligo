6beccd09a8f020b12f1cf8736ee8ea651cca0f9a
Sander Spies <sandermail@gmail.com>
2020-02-02 18:08:16 +0100

    Wrong function arguments error message.

--- src/bin/expect_tests/syntax_error_tests.ml
+++ src/bin/expect_tests/syntax_error_tests.ml
@@ -15,3 +15,22 @@ let%expect_test _ =
     * Open a gitlab issue: https://gitlab.com/ligolang/ligo/issues/new
     * Check the changelog by running 'ligo changelog' |} ] ;
 
+  run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/error_function_arguments.ligo" ; "main" ] ;
+  [%expect {|
+    ligo: in file "basic.religo", line 1, characters 14-27. : It looks like you are defining a function, however we do not
+      understand the parameters declaration.
+      Examples of valid functions:
+      let x = (a: string, b: int) : int => 3;
+      let tuple = ((a, b): (int, int)) => a + b; 
+      let x = (a: string) : string => "Hello, " ++ a;
+       {"location":"in file \"basic.religo\", line 1, characters 14-27"}
+      
+
+     If you're not sure how to fix this error, you can
+     do one of the following:
+
+    * Visit our documentation: https://ligolang.org/docs/intro/what-and-why/
+    * Ask a question on our Discord: https://discord.gg/9rhYaEt
+    * Open a gitlab issue: https://gitlab.com/ligolang/ligo/issues/new
+    * Check the changelog by running 'ligo changelog' |} ] ;
+
