5a15feadc157f1f87a44408643069f46d33e02e0
Lesenechal Remi <lesenechal.remi@gmail.com>
2020-02-21 17:07:48 +0100

    now map over type declarations

--- src/passes/3-self_ast_simplified/helpers.ml
+++ src/passes/3-self_ast_simplified/helpers.ml
@@ -90,8 +90,12 @@ and fold_cases : 'a folder -> 'a -> matching_expr -> 'a result = fun f init m ->
       ok res
     )
 
-type mapper = expression -> expression result
-let rec map_expression : mapper -> expression -> expression result = fun f e ->
+type exp_mapper = expression -> expression result
+type ty_exp_mapper = type_expression -> type_expression result
+type abs_mapper =
+  | Expression of exp_mapper
+  | Type_expression of ty_exp_mapper 
+let rec map_expression : exp_mapper -> expression -> expression result = fun f e ->
   let self = map_expression f in
   let%bind e' = f e in
   let return expression_content = ok { e' with expression_content } in
@@ -167,8 +171,54 @@ let rec map_expression : mapper -> expression -> expression result = fun f e ->
     )
   | E_literal _ | E_variable _ | E_skip as e' -> return e'
 
+and map_type_expression : ty_exp_mapper -> type_expression -> type_expression result = fun f te ->
+  let self = map_type_expression f in
+  let%bind te' = f te in
+  let return type_content = ok { te' with type_content } in
+  match te'.type_content with
+  | T_sum temap ->
+    let%bind temap' = bind_map_cmap self temap in
+    return @@ (T_sum temap')
+  | T_record temap ->
+    let%bind temap' = bind_map_lmap self temap in
+    return @@ (T_record temap')
+  | T_arrow {type1 ; type2} ->
+    let%bind type1' = self type1 in
+    let%bind type2' = self type2 in
+    return @@ (T_arrow {type1=type1' ; type2=type2'})
+  | T_operator type_op ->
+    let%bind to' = map_type_operator f type_op in
+    return @@ (T_operator to')
+  | T_variable _ | T_constant _ -> ok te'
 
-and map_cases : mapper -> matching_expr -> matching_expr result = fun f m ->
+and map_type_operator : ty_exp_mapper -> type_operator -> type_operator result = fun f te ->
+  match te with
+  | TC_contract e ->
+    let%bind e' = map_type_expression f e in
+    ok @@ TC_contract e'
+  | TC_option e ->
+    let%bind e' = map_type_expression f e in
+    ok @@ TC_option e'
+  | TC_list e ->
+    let%bind e' = map_type_expression f e in
+    ok @@ TC_list e'
+  | TC_set e -> 
+    let%bind e' = map_type_expression f e in
+    ok @@ TC_set e'
+  | TC_map (a , b) ->
+    let%bind a' = map_type_expression f a in
+    let%bind b' = map_type_expression f b in
+    ok @@ TC_map (a' , b')
+  | TC_big_map (a , b) ->
+    let%bind a' = map_type_expression f a in
+    let%bind b' = map_type_expression f b in
+    ok @@ TC_big_map (a' , b')
+  | TC_arrow (a , b) -> 
+    let%bind a' = map_type_expression f a in
+    let%bind b' = map_type_expression f b in
+    ok @@ TC_arrow (a' , b')
+
+and map_cases : exp_mapper -> matching_expr -> matching_expr result = fun f m ->
   match m with
   | Match_bool { match_true ; match_false } -> (
       let%bind match_true = map_expression f match_true in
@@ -198,14 +248,19 @@ and map_cases : mapper -> matching_expr -> matching_expr result = fun f m ->
       ok @@ Match_variant (lst', ())
     )
 
-and map_program : mapper -> program -> program result = fun m p ->
+and map_program : abs_mapper -> program -> program result = fun m p ->
   let aux = fun (x : declaration) ->
-    match x with
-    | Declaration_constant (t , o , i, e) -> (
-        let%bind e' = map_expression m e in
+    match x,m with
+    | (Declaration_constant (t , o , i, e), Expression m') -> (
+        let%bind e' = map_expression m' e in
         ok (Declaration_constant (t , o , i, e'))
       )
-    | Declaration_type _ -> ok x
+    | (Declaration_type (tv,te), Type_expression m') -> (
+        let%bind te' = map_type_expression m' te in
+        ok (Declaration_type (tv, te'))
+      )
+    | decl,_ -> ok decl
+  (* | Declaration_type of (type_variable * type_expression) *)
   in
   bind_map_list (bind_map_location aux) p
 
--- src/passes/3-self_ast_simplified/self_ast_simplified.ml
+++ src/passes/3-self_ast_simplified/self_ast_simplified.ml
@@ -1,17 +1,24 @@
 open Trace
 
-let all = [
+let all_expression_mapper = [
   Tezos_type_annotation.peephole_expression ;
   None_variant.peephole_expression ;
   Literals.peephole_expression ;
 ]
+let all_type_expression_mapper = [
+  Entrypoints_lenght_limit.peephole_type_expression ;
+]
+
+let all_exp = List.map (fun el -> Helpers.Expression el) all_expression_mapper
+let all_ty = List.map (fun el -> Helpers.Type_expression el) all_type_expression_mapper
 
 let all_program =
-  let all_p = List.map Helpers.map_program all in
-  bind_chain all_p
+  let all_p = List.map Helpers.map_program all_exp in
+  let all_p2 = List.map Helpers.map_program all_ty in
+  bind_chain (List.append all_p all_p2)
 
 let all_expression =
-  let all_p = List.map Helpers.map_expression all in
+  let all_p = List.map Helpers.map_expression all_expression_mapper in
   bind_chain all_p
 
 let map_expression = Helpers.map_expression
