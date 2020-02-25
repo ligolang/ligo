8229d6a6af2e18419908e587b9590a04b83371d5
Tom Jack <tom@tomjack.co>
2020-02-25 12:25:15 -0600

    Remove (unused) assignment from mini_c

--- src/passes/8-compiler/compiler_environment.mli
+++ src/passes/8-compiler/compiler_environment.mli
@@ -8,7 +8,6 @@ module Stack = Meta_michelson.Stack
 *)
 val empty: environment
 val get : environment -> expression_variable -> michelson result
-val set : environment -> expression_variable -> michelson result
 
 val pack_closure : environment -> selector -> michelson result
 val unpack_closure : environment -> michelson result
