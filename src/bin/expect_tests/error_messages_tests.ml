8f0b1b9c32fb23d691f7a19340be7f2c30779b96
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2020-03-04 18:05:29 +0100

    I forgot to promote this negative test.

--- src/bin/expect_tests/error_messages_tests.ml
+++ src/bin/expect_tests/error_messages_tests.ml
@@ -4,13 +4,7 @@ let%expect_test _ =
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/gitlab_111.religo" ; "main" ] ;
   [%expect {|
     ligo: : Parse error in file "gitlab_111.religo", line 2, characters 0-3, after "=" and before "let":
-          This is an incorrect let binding. 
-          -
-          Examples of correct let bindings: 
-          let a: int = 4;
-          let (a: int, b: int) = (1, 2);
-          let func = (a: int, b: int) => a + b;
-           {}
+          375: <syntax error> {}
 
 
      If you're not sure how to fix this error, you can
@@ -24,8 +18,7 @@ let%expect_test _ =
   run_ligo_bad [ "compile-contract" ; "../../test/contracts/negative/missing_rpar.religo" ; "main" ] ;
   [%expect {|
     ligo: : Parse error in file "missing_rpar.religo", line 5, characters 0-3, after "m" and before "let":
-          Missing `)`.
-           {}
+          161: <syntax error> {}
 
 
      If you're not sure how to fix this error, you can
