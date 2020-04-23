9de281e7ef6581909bfbe82bd1ad0415c38aa775
Maksym Bykovskyy <maksym@ecadlabs.com>
2020-04-23 16:46:23 +0000

    Add build info to ide metrics and state
    - Add ligo_webide_build_info metric that surface revision and branch from CI
    - Add  a version sub state which contains the current revision and branch of frontend
    
    - Added back express-prometheus-middleware

--- tools/webide/packages/server/src/index.ts
+++ tools/webide/packages/server/src/index.ts
@@ -13,12 +13,17 @@ import { shareHandler } from './handlers/share';
 import { sharedLinkHandler } from './handlers/shared-link';
 import { loadDefaultState } from './load-state';
 import { errorLoggerMiddleware, loggerMiddleware } from './logger';
+require('./metrics');
 
-var bodyParser = require('body-parser');
-var escape = require('escape-html');
+const bodyParser = require('body-parser');
+const escape = require('escape-html');
+const prometheus = require('express-prometheus-middleware');
 
 const app = express();
-const port = 8080;
+const APP_PORT = 8080;
+
+const metrics = express();
+const METRICS_PORT = 8081;
 
 const appRootDirectory =
   process.env['STATIC_ASSETS'] ||
@@ -27,6 +32,15 @@ const appBundleDirectory = join(appRootDirectory, 'build');
 
 app.use(bodyParser.json());
 app.use(loggerMiddleware);
+app.use(
+  prometheus({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    collectDefaultBuckets: true,
+    requestDurationBuckets: [0.5, 0.6, 0.7, 1, 10, 20, 30, 60],
+    metricsApp: metrics
+  })
+);
 
 const file = fs.readFileSync(join(appBundleDirectory, 'index.html'));
 
@@ -61,6 +75,10 @@ app.post('/api/deploy', deployHandler);
 
 app.use(errorLoggerMiddleware);
 
-app.listen(port, () => {
-  console.log(`Listening on: ${port}`);
+app.listen(APP_PORT, () => {
+  console.log(`API listening on: ${APP_PORT}`);
+});
+
+metrics.listen(METRICS_PORT, () => {
+  console.log(`Metrics listening on: ${METRICS_PORT}`);
 });
