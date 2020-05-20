b6aa71d6acc1160748bdc8472a7d70c296cbc177
Alexander Bantyev <balsoft@balsoft.ru>
2020-05-20 22:00:10 +0300

    Remove /tmp from docker container

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,11 +1,7 @@
 { dockerTools, writeShellScriptBin, runCommand, mcpp, bash, coreutils, ligo, name ? "ligo" }:
-let
-  # LIGO requires /tmp for compilation, which is missing in the default image
-  tmp = runCommand "tmp" {} "mkdir -p $out/tmp";
-in
 dockerTools.buildLayeredImage {
   inherit name;
   tag = "latest";
-  contents = [ ligo tmp bash ];
+  contents = [ ligo bash ];
   config.Entrypoint = name;
 }
