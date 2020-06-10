acaa900b7e9d1735477625ecb669f05e90e8c15c
Suzanne Dupéron <ligo@suzanne.soy>
2020-06-10 19:06:42 +0100

    Fixes to instant-editor.sh after my changes

--- scripts/instant-editor.sh
+++ scripts/instant-editor.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -euEo pipefail
 
-if [ $# -eq 1 ]; then
-    if [ "$1" == "emacs" ]; then
-        shift;
-        nix-shell shell.nix --run "nix-shell tools/instant-editor/emacs.nix --run \"emacs '${1//"'"/"'\\''"}'\"";
-    fi
+if [ $# -eq 2 ]; then
+    nix-shell shell.nix --run "nix-shell tools/instant-editor/emacs.nix --run \"$1 '${2//"'"/"'\\''"}'\"";
 elif [ $# -eq 0 ]; then
     nix-shell shell.nix;
 else
-  echo "Usage: ./instant-editor.sh [file]"
+  echo "Usage: ./scripts/instant-editor.sh [editor file]"
   exit 1
 fi
