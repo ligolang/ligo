143024f111a9f766106664721e6a207a833dbcc1
Suzanne Dupéron <ligo@suzanne.soy>
2020-06-10 17:25:39 +0000

    Apply 5 suggestion(s) to 1 file(s)

--- instant-editor.sh
+++ instant-editor.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euEo pipefail
 
-if [ $# -gt 0 ]; then
+if [ $# -eq 1 ]; then
     if [ "$1" == "emacs" ]; then
         shift;
-        nix-shell shell.nix --run "nix-shell tools/instant-editor/emacs.nix --run \"emacs $@\"";
+        nix-shell shell.nix --run "nix-shell tools/instant-editor/emacs.nix --run \"emacs '${1//"'"/"'\\''"}'\"";
     fi
-else
+elif [ $# -eq 0 ]; then
     nix-shell shell.nix;
+else
+  echo "Usage: ./instant-editor.sh [file]"
+  exit 1
 fi
