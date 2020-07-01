728cf3f1ba7048943f55f17207b3ea8094c7317c
Lesenechal Remi <lesenechal.remi@gmail.com>
2020-07-01 13:10:52 +0200

    get_scope fixes: - merge inner_def maps in the main fold - make use of the expression variable locations

--- src/bin/expect_tests/get_scope.ml
+++ src/bin/expect_tests/get_scope.ml
@@ -13,13 +13,13 @@ let%expect_test _ =
     [ ] in file "lambda_letin.mligo", line 1, characters 0-9
 
     Variable definitions:
-    (k#4 -> k) in file "lambda_letin.mligo", line 6, characters 12-25
-    (j#2 -> j) in file "lambda_letin.mligo", line 5, character 4 to line 7, character 21
-    (i#1 -> i) in file "lambda_letin.mligo", line 4, character 32 to line 7, character 21
-    (g#3 -> g) in file "lambda_letin.mligo", line 5, characters 12-21
-    (f#5 -> f) in file "lambda_letin.mligo", line 4, characters 8-9
-    (b#6 -> b) in file "lambda_letin.mligo", line 3, character 0 to line 9, character 7
-    (a#0 -> a) in file "lambda_letin.mligo", line 1, characters 0-9
+    (k#4 -> k) in file "lambda_letin.mligo", line 6, characters 8-9
+    (j#2 -> j) in file "lambda_letin.mligo", line 4, characters 47-48
+    (i#1 -> i) in file "lambda_letin.mligo", line 4, characters 37-38
+    (g#3 -> g) in file "lambda_letin.mligo", line 5, characters 8-9
+    (f#5 -> f) in file "lambda_letin.mligo", line 4, characters 6-7
+    (b#6 -> b) in file "lambda_letin.mligo", line 3, characters 4-5
+    (a#0 -> a) in file "lambda_letin.mligo", line 1, characters 4-5
     Type definitions: |} ];
 
   run_ligo_good [ "get-scope" ; gs "letin.mligo" ; "--format=dev" ] ;
@@ -33,12 +33,12 @@ let%expect_test _ =
     [ ] in file "letin.mligo", line 1, characters 0-9
 
     Variable definitions:
-    (f#3 -> f) in file "letin.mligo", line 7, characters 12-21
-    (e#2 -> e) in file "letin.mligo", line 6, characters 12-17
-    (d#4 -> d) in file "letin.mligo", line 6, character 4 to line 8, character 17
-    (c#1 -> c) in file "letin.mligo", line 4, characters 10-15
-    (b#5 -> b) in file "letin.mligo", line 3, character 0 to line 10, character 11
-    (a#0 -> a) in file "letin.mligo", line 1, characters 0-9
+    (f#3 -> f) in file "letin.mligo", line 7, characters 8-9
+    (e#2 -> e) in file "letin.mligo", line 6, characters 8-9
+    (d#4 -> d) in file "letin.mligo", line 5, characters 6-7
+    (c#1 -> c) in file "letin.mligo", line 4, characters 6-7
+    (b#5 -> b) in file "letin.mligo", line 3, characters 4-5
+    (a#0 -> a) in file "letin.mligo", line 1, characters 4-5
     Type definitions: |} ] ;
 
   run_ligo_good [ "get-scope" ; gs "lambda.mligo" ; "--format=dev" ] ;
@@ -49,11 +49,11 @@ let%expect_test _ =
     [ ] in file "lambda.mligo", line 1, characters 0-9
 
     Variable definitions:
-    (j#2 -> j) in file "lambda.mligo", line 4, characters 58-63
-    (i#1 -> i) in file "lambda.mligo", line 4, characters 31-63
-    (f#3 -> f) in file "lambda.mligo", line 4, characters 8-9
-    (b#4 -> b) in file "lambda.mligo", line 3, character 0 to line 5, character 7
-    (a#0 -> a) in file "lambda.mligo", line 1, characters 0-9
+    (j#2 -> j) in file "lambda.mligo", line 4, characters 46-47
+    (i#1 -> i) in file "lambda.mligo", line 4, characters 36-37
+    (f#3 -> f) in file "lambda.mligo", line 4, characters 6-7
+    (b#4 -> b) in file "lambda.mligo", line 3, characters 4-5
+    (a#0 -> a) in file "lambda.mligo", line 1, characters 4-5
     Type definitions: |} ] ;
 
   run_ligo_good [ "get-scope" ; gs "match.mligo" ; "--format=dev" ] ;
@@ -74,12 +74,18 @@ let%expect_test _ =
     [ mytype#0 ] in file "match.mligo", line 3, characters 0-9
 
     Variable definitions:
-    (s#11 -> s) in file "match.mligo", line 19, characters 16-21
-    (d#12 -> d) in file "match.mligo", line 17, character 0 to line 20, character 3
-    (d#10 -> d) in file "match.mligo", line 18, characters 17-18
-    (c#9 -> c) in file "match.mligo", line 10, character 0 to line 15, character 3
-    (b#5 -> b) in file "match.mligo", line 5, character 0 to line 8, character 3
-    (a#1 -> a) in file "match.mligo", line 3, characters 0-9
+    (y#4 -> y) in file "match.mligo", line 8, characters 8-9
+    (x#3 -> x) in file "match.mligo", line 7, characters 8-9
+    (tl#8 -> tl) in file "match.mligo", line 15, characters 8-10
+    (s#11 -> s) in file "match.mligo", line 19, characters 10-11
+    (hd#7 -> hd) in file "match.mligo", line 15, characters 4-6
+    (d#12 -> d) in file "match.mligo", line 17, characters 4-5
+    (d#10 -> d) in file "match.mligo", line 18, characters 13-14
+    (c#9 -> c) in file "match.mligo", line 10, characters 4-5
+    (c#6 -> c) in file "match.mligo", line 13, characters 8-9
+    (c#2 -> c) in file "match.mligo", line 6, characters 13-14
+    (b#5 -> b) in file "match.mligo", line 5, characters 4-5
+    (a#1 -> a) in file "match.mligo", line 3, characters 4-5
     Type definitions:
     (mytype#0 -> mytype) in file "match.mligo", line 1, characters 0-40 |} ] ;
 
@@ -96,14 +102,14 @@ let%expect_test _ =
     [ ] in file "rec.mligo", line 1, characters 0-9
 
     Variable definitions:
-    (k#4 -> k) in file "rec.mligo", line 5, characters 12-21
-    (j#3 -> j)
-    (i#2 -> i)
-    (c#5 -> c) in file "rec.mligo", line 4, characters 12-13
-    (c#1 -> c) in file "rec.mligo", line 4, characters 12-13
-    (b#7 -> b) in file "rec.mligo", line 3, character 0 to line 9, character 10
-    (b#6 -> b) in file "rec.mligo", line 8, characters 10-11
-    (a#0 -> a) in file "rec.mligo", line 1, characters 0-9
+    (k#4 -> k) in file "rec.mligo", line 5, characters 8-9
+    (j#3 -> j) in file "rec.mligo", line 4, characters 39-40
+    (i#2 -> i) in file "rec.mligo", line 4, characters 37-38
+    (c#5 -> c) in file "rec.mligo", line 4, characters 10-11
+    (c#1 -> c) in file "rec.mligo", line 4, characters 10-11
+    (b#7 -> b) in file "rec.mligo", line 3, characters 4-5
+    (b#6 -> b) in file "rec.mligo", line 8, characters 6-7
+    (a#0 -> a) in file "rec.mligo", line 1, characters 4-5
     Type definitions: |} ] ;
 
   run_ligo_good [ "get-scope" ; gs "shadowing.mligo" ; "--format=dev" ] ;
@@ -117,10 +123,10 @@ let%expect_test _ =
     [ ] in file "shadowing.mligo", line 1, characters 0-9
 
     Variable definitions:
-    (e#2 -> e) in file "shadowing.mligo", line 6, characters 12-17
-    (d#4 -> d) in file "shadowing.mligo", line 6, character 4 to line 8, character 13
-    (c#1 -> c) in file "shadowing.mligo", line 4, characters 10-15
-    (b#5 -> b) in file "shadowing.mligo", line 3, character 0 to line 10, character 11
-    (a#3 -> a) in file "shadowing.mligo", line 7, characters 12-21
-    (a#0 -> a) in file "shadowing.mligo", line 1, characters 0-9
+    (e#2 -> e) in file "shadowing.mligo", line 6, characters 8-9
+    (d#4 -> d) in file "shadowing.mligo", line 5, characters 6-7
+    (c#1 -> c) in file "shadowing.mligo", line 4, characters 6-7
+    (b#5 -> b) in file "shadowing.mligo", line 3, characters 4-5
+    (a#3 -> a) in file "shadowing.mligo", line 7, characters 8-9
+    (a#0 -> a) in file "shadowing.mligo", line 1, characters 4-5
     Type definitions: |} ] ;
