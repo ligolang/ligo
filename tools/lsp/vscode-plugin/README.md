15fc6f25fd7905589cc0fbdccb51462f86998a35
Kirill Kuvshinov <kkirka@gmail.com>
2020-08-13 09:34:33 +0300

    [LIGO-13] Add a VSCode extension
    Problem: We need to deliver a VSCode extension but the one we use
    currently is located in a separate repo, requires manual patches
    and does not support ReasonLIGO.
    
    Solution: Add our extension that supports PascaLIGO and ReasonLIGO;
    CamLIGO is WIP.

---
+++ tools/lsp/vscode-plugin/README.md
@@ -0,0 +1,36 @@
+# LIGO VSCode Plugin
+
+This plugin is an LSP implementation for the LIGO language family.
+
+Currently, it is highly experimental and may contain bugs. On MacOS and Windows, only syntax highlighting is supported yet.
+
+## Functionality
+Code navigation
+
+- [x] Jump to definition
+- [x] Find references
+- [x] Folding range
+- [ ] Selection range
+- [ ] Jump to type definition
+- [ ] Document symbols
+- [ ] Workspace symbols
+- [ ] Document highlights
+
+Diagnostics
+
+- [x] Parser diagnostics
+- [ ] Extended diagnostics
+
+Code editing
+
+- [ ] Hover suggestions
+- [ ] Code actions (refactor, rename, etc.)
+- [x] Code completion for variable names
+- [ ] Code completion for record fields and constructors
+- [ ] Signature help
+
+Formatting
+
+- [ ] Whole document formatting
+- [ ] On-type formatting
+- [ ] Document range formatting
---
+++ tools/lsp/vscode-plugin/client/src/extension.ts
@@ -0,0 +1,54 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as path from 'path';
+import { ExtensionContext } from 'vscode';
+
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    ServerOptions,
+    TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    let serverOptions: ServerOptions = {
+       command: `${context.extensionPath}/bin/squirrel`
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            { scheme: 'file', language: 'ligo' },
+            { scheme: 'file', language: 'religo' }
+        ],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+	    // fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+        }
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient(
+        'ligoLanguageServer',
+        'LIGO Language Server',
+        serverOptions,
+        clientOptions
+    );
+
+    console.log('Starting the client...')
+    // Start the client. This will also launch the server
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
