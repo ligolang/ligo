c225944db17aa5d92682ab7b4bf26c24a15f90d3
Anton Myasnikov <awkure@protonmail.ch>
2020-09-16 21:27:20 +0300

    [LIGO-44] Extract global extension configuration
    Problem: We want to be able to extract global configuration from
    our extension to be used later for instance resolving external
    binary paths.
    
    Solution: Add `Config` datatype and its JSON instances to work
    with global configuration. And support for configurable ligo binary
    paths within the extension itself.

---
+++ tools/lsp/squirrel/src/Config.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE RecordWildCards #-}
+
+-- | This module contains glpbal extension configuration
+
+module Config
+  ( Config(..)
+  , getConfigFromNotification
+  , getInitialConfig
+  ) where
+
+import Data.Aeson
+import Data.Default
+import qualified Data.Text as T
+import Language.Haskell.LSP.Types
+
+----------------------------------------------------------------------------
+-- Handlers
+----------------------------------------------------------------------------
+
+-- | Given a DidChangeConfigurationNotification message, this function returns the parsed
+-- Config object if possible.
+getConfigFromNotification :: DidChangeConfigurationNotification -> Either T.Text Config
+getConfigFromNotification NotificationMessage { _params = DidChangeConfigurationParams p } =
+  case fromJSON p of
+    Success c -> Right c
+    Error err -> Left $ T.pack err
+
+-- | Given an InitializeRequest message, this function returns the parsed
+-- Config object if possible. Otherwise, it returns the default configuration.
+getInitialConfig :: InitializeRequest -> Either T.Text Config
+getInitialConfig
+  RequestMessage { _params = InitializeParams { _initializationOptions = Nothing }} = Right def
+getInitialConfig
+  RequestMessage { _params = InitializeParams { _initializationOptions = Just opts }} =
+    case fromJSON opts of
+      Success c -> Right c
+      Error err -> Left $ T.pack err
+
+----------------------------------------------------------------------------
+-- Types
+----------------------------------------------------------------------------
+
+-- | Global extension configuration
+data Config =
+  Config
+    { _cLigoBinaryPath :: FilePath -- ^ Path to ligo binary
+    , _cMaxNumberOfProblems :: Int -- ^ Maximum amount of errors displayed
+    } deriving stock (Eq, Show)
+
+instance Default Config where
+  def = Config
+    { _cLigoBinaryPath = "ligo" -- ^ Extract ligo binary from $PATH
+    , _cMaxNumberOfProblems = 100
+    }
+
+----------------------------------------------------------------------------
+-- JSON
+----------------------------------------------------------------------------
+
+instance FromJSON Config where
+  parseJSON = withObject "Config" $ \v -> do
+    s <- v .: "ligoLanguageServer"
+    flip (withObject "Config.settings") s $ \o -> do
+      _cLigoBinaryPath <- o .:? "ligoBinaryPath" .!= _cLigoBinaryPath def
+      _cMaxNumberOfProblems <- o .:? "maxNumberOfProblems" .!= _cMaxNumberOfProblems def
+      pure Config
+        { _cLigoBinaryPath = _cLigoBinaryPath
+        , _cMaxNumberOfProblems = _cMaxNumberOfProblems
+        }
+
+instance ToJSON Config where
+  toJSON Config {..} = object [ "ligoLanguageServer" .= r ]
+    where
+      r = object
+        [ "ligoBinaryPath" .= _cLigoBinaryPath
+        , "maxNumberOfProblems" .= _cMaxNumberOfProblems
+        ]
