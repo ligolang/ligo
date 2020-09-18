9d54285eb810c4c252addc932304ddc461d136a2
Suzanne Soy <2923020-suzanne.duperon@users.noreply.gitlab.com>
2020-09-18 21:12:22 +0000

    Added dependency on json2yaml and jq in nix-shell

--- shell.nix
+++ shell.nix
@@ -6,7 +6,8 @@ let
   emacsAttrs = lib.optionalAttrs emacs
     (import ./tools/instant-editor/emacs.nix { inherit pkgs; });
 in mkShell ({
-  inputsFrom = [ pkgs.ocamlPackages.ligo-tests ];
+  buildInputs = [ pkgs.haskellPackages.json2yaml pkgs.jq ];
+  inputsFrom = [ pkgs.ocamlPackages.ligo-tests pkgs.haskellPackages.json2yaml pkgs.jq ];
 
   UTOP_SITE_LISP = "${ocamlPackages.utop}/share/emacs/site-lisp";
   MERLIN_SITE_LISP = "${ocamlPackages.merlin}/share/emacs/site-lisp";
