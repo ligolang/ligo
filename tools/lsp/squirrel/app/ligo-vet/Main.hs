060a43bdd57656f0afcd839f28cdd97ef3d31b6b
Artem Yurchenko <artem.yurchenko@serokell.io>
2020-11-21 13:39:52 +0300

    add with-scopes option to ligo-vet sexp printing
    Problem: adding scopes might mess up a tree. We need to check it.
    
    Solution: add the appropriate option to ligo-vet and add corresponding
         tests.

--- tools/lsp/squirrel/app/ligo-vet/Main.hs
+++ tools/lsp/squirrel/app/ligo-vet/Main.hs
@@ -1,23 +1,42 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Control.Arrow (first)
 import Control.Monad (unless)
 import Data.Foldable (for_)
-import Duplo.Pretty (pp, render)
+import Duplo.Pretty (Pretty, pp, render)
 import Main.Utf8 (withUtf8)
 import Options.Applicative
-  (Parser, ParserInfo, command, execParser, help, helper, hsubparser, info, long, metavar,
-  progDesc, strOption)
+  (Parser, ParserInfo, command, execParser, help, helper, hsubparser, info, long, metavar, progDesc,
+  short, strOption, switch)
 
-import AST (parse)
+import AST (Fallback, parse, parseWithScopes)
 import ParseTree (Source (Path))
 
-data Command = PrintSexp FilePath
+data Command = PrintSexp PrintSexpOptions
+
+data PrintSexpOptions = PrintSexpOptions
+  { psoContract :: FilePath
+  , psoWithScopes :: Bool
+  }
 
 commandParser :: Parser Command
 commandParser = hsubparser $ mconcat
   [ printSubCommand ]
   where
     printSubCommand = command "print-sexp"
-      (info (PrintSexp <$> contractFileOption)
+      (info printSexp
         (progDesc "Parse a given contract and print a s-exp representing it."))
+
+printSexp :: Parser Command
+printSexp = do
+  psoContract <- contractFileOption
+  psoWithScopes <- withScopesOption
+  pure (PrintSexp PrintSexpOptions{ .. })
+  where
+    withScopesOption = switch $
+      long "with-scopes" <>
+      short 's' <>
+      help "Whether to add fallback scopes to the tree"
 
 -- | Parser of a path to a contract.
 contractFileOption :: Parser FilePath
@@ -29,11 +48,20 @@ contractFileOption = strOption $
 programInfo :: ParserInfo Command
 programInfo = info (helper <*> commandParser) mempty
 
+data SomePretty where
+  SomePretty :: Pretty a => a -> SomePretty
+
+instance Pretty SomePretty where
+  pp (SomePretty a) = pp a
+
 main :: IO ()
 main = withUtf8 $ do
   execParser programInfo >>= \case
-    PrintSexp path -> do
-      (tree, messages) <- parse (Path path)
+    PrintSexp PrintSexpOptions{ .. } -> do
+      let parser = if psoWithScopes
+            then fmap (first SomePretty) . parseWithScopes @Fallback
+            else fmap (first SomePretty) . parse
+      (tree, messages) <- parser (Path psoContract)
       putStrLn (render (pp tree))
       unless (null messages) $ do
         putStrLn "The following errors have been encountered: "
