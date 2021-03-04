c0bc2751ea1a5bc34fc7ade01ce33709a012e967
Tom Jack <tom@tomjack.co>
2021-03-04 14:22:08 +0000

    Drop support for old (pre-Edo) protocols

--- src/environment/protocols.ml
+++ src/environment/protocols.ml
@@ -1,24 +1,18 @@
-type t = Carthage | Dalphanet | Edo
+type t = Edo
 let current = Edo
 
 (* this list is used to print the list of protocols in the CLI help *)
 let protocols_str : string list = [
-  "carthage" ;
-  "dalphanet" ;
-	"edo" ;
+  "edo" ;
 ]
 
 let protocols_to_variant : string -> t option = fun p ->
 	match p with
 	| "current" -> Some current
-	| "carthage" -> Some Carthage
-	| "dalphanet" -> Some Dalphanet
 	| "edo"       -> Some Edo
 	| i when not (List.exists (String.equal i) protocols_str) -> None 
 	| _ -> failwith "internal error: forgot to add the protocol string form to the list ?"
 
 let variant_to_string : t -> string = fun s ->
 	match s with
-	| Carthage -> "carthage"
-	| Dalphanet -> "dalphanet"
 	| Edo -> "edo"
