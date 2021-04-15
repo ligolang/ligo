8f30880836a2f50ffb6db004f8646ab9917df5e6
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2021-04-15 11:34:47 -0300

    [LIGO-172] Split test suite
    Problem: We want the `Parsers` tests for upstream contracts separated
    from the `Parsers` tests for the LSP contracts.
    
    Solution: First we add all the tests to a new `lsp-test` subdirectory.
    After this, we duplicate the `Parsers` test in its own directory and
    change both so that redundancies are removed. Then we create a
    `test-commons` directory and move common parts of both test suites to
    it.

---
+++ tools/lsp/squirrel/test/test-common/Test/Util.hs
@@ -0,0 +1,57 @@
+module Test.Util
+  ( contractsDir
+  , getContractsWithExtension
+  , readContract
+  , readContractWithMessages
+  , readContractWithScopes
+  , supportedExtensions
+  ) where
+
+import Control.Exception.Safe (catch, throwIO)
+import Control.Monad.IO.Class (liftIO)
+import Data.Functor ((<&>))
+import Data.List (isSuffixOf)
+import Language.Haskell.TH.Syntax (liftString)
+import System.Directory (listDirectory)
+import System.Environment (getEnv)
+import System.FilePath ((</>))
+import System.IO.Error (isDoesNotExistError)
+
+import AST (parse)
+import AST.Parser (Source (Path), parseWithScopes)
+import AST.Scope.Common (HasScopeForest, Info')
+import AST.Skeleton (SomeLIGO)
+import Parser (Info, Msg)
+
+contractsDir :: FilePath
+contractsDir =
+  $(
+    let
+      getDir :: IO FilePath
+      getDir = getEnv "CONTRACTS_DIR" `catch` \e ->
+        if isDoesNotExistError e
+        then pure "../../../src/test/contracts"
+        else throwIO e
+    in liftIO getDir >>= liftString
+  )
+
+getContractsWithExtension :: String -> [FilePath] -> FilePath -> IO [FilePath]
+getContractsWithExtension ext ignore dir = listDirectory dir
+                                <&> filter (ext `isSuffixOf`)
+                                <&> map (dir </>)
+                                <&> filter (`notElem` ignore)
+
+readContract :: FilePath -> IO (SomeLIGO Info)
+readContract filepath = fst <$> parse (Path filepath)
+
+readContractWithMessages :: FilePath -> IO (SomeLIGO Info, [Msg])
+readContractWithMessages filepath = parse (Path filepath)
+
+readContractWithScopes
+  :: forall parser. HasScopeForest parser IO
+  => FilePath -> IO (SomeLIGO Info')
+readContractWithScopes filepath
+  = fst <$> parseWithScopes @parser (Path filepath)
+
+supportedExtensions :: [FilePath]
+supportedExtensions = [".ligo", ".mligo", ".religo"]
