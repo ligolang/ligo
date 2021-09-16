790b0952cc741614ee29cecf098c74ab0f91d046
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2021-09-16 14:05:00 -0300

    [LIGO-263] Add keywords to completion
    Problem: We want to make VSCode aware of keywords for better
    completions.
    
    Solution: Add support for keywords by refactoring Completion a bit.
    Particularly, we want to distinguish ordinary completions from keyword
    ones. We create sets containing all keywords and filter only the ones
    we're interested in (they are a subsequence of what the user typed).
    Change our tests to also include keywords.
    
    On the way, refactor our test driver for Completion a bit so it's not
    quadratic anymore.

--- tools/lsp/squirrel/test/integration-test/Test/Integrational/Capabilities/Completion.hs
+++ tools/lsp/squirrel/test/integration-test/Test/Integrational/Capabilities/Completion.hs
@@ -7,5 +7,5 @@ import AST.Scope (Standard)
 import Test.Common.Capabilities.Completion
 import Test.Tasty (TestTree)
 
-test_completion :: TestTree
+test_completion :: IO TestTree
 test_completion = completionDriver @Standard caseInfos
