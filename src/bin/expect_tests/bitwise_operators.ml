13244e7c3f441ccad489f8c9bd4e5a47eab0a772
Rémi Lesenechal <lesenechal.remi@gmail.com>
2021-10-13 18:19:19 +0000

    errors: fix PP of bool type

--- src/bin/expect_tests/bitwise_operators.ml
+++ src/bin/expect_tests/bitwise_operators.ml
@@ -70,27 +70,27 @@ let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "14 land 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat) or (int, nat), but got an argument of type int, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat) or (int, nat), but got an argument of type int, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "14n lor 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type nat, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type nat, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "14 lor 2n" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type int, nat. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type int, nat. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "14n lxor 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type nat, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type nat, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "14 lxor 2n" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type int, nat. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type int, nat. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "cameligo" ; "4 lsr 0n" ] ;
   [%expect{|
@@ -182,27 +182,27 @@ let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "14 land 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat) or (int, nat), but got an argument of type int, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat) or (int, nat), but got an argument of type int, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "14n lor 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type nat, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type nat, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "14 lor 2n" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type int, nat. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type int, nat. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "14n lxor 2" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type nat, int. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type nat, int. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "14 lxor 2n" ] ;
   [%expect{|
     Invalid arguments.
-    Expected an argument of type (sum[False -> unit , True -> unit], sum[False -> unit , True -> unit]) or (nat, nat), but got an argument of type int, nat. |}]
+    Expected an argument of type (bool, bool) or (nat, nat), but got an argument of type int, nat. |}]
 let%expect_test _ =
   run_ligo_bad [ "compile" ; "expression" ; "reasonligo" ; "4 lsr 0n" ] ;
   [%expect{|
