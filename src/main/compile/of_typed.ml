ac77eb02b27e17c916b6b03d6d93ff7a61f9553e
Pierre-Emmanuel Wulfman <pierre.emmanuel.wulfman@gmail.com>
2021-11-23 11:02:54 +0000

    Resolve "Using #import produce contract that doesn't typecheck"

--- src/main/compile/of_typed.ml
+++ src/main/compile/of_typed.ml
@@ -5,16 +5,15 @@ open Main_errors
 
 module SMap = Map.Make(String)
 
-let compile_with_modules ~raise ?(module_env = SMap.empty) : Ast_typed.module_fully_typed -> Mini_c.program * AST.type_expression SMap.t = fun p ->
-  trace ~raise spilling_tracer @@ compile_module ~module_env:module_env p
+let compile_with_modules ~raise : Ast_typed.module_fully_typed -> Mini_c.program = fun p ->
+  trace ~raise spilling_tracer @@ compile_module p
 
-let compile ~raise ?(module_env = SMap.empty) : Ast_typed.module_fully_typed -> Mini_c.program = fun p ->
-  let p = Self_ast_typed.monomorphise_module p in
-  let mini_c,_ = compile_with_modules ~raise ~module_env:module_env p in
+let compile ~raise : Ast_typed.module_fully_typed -> Mini_c.program = fun p ->
+  let mini_c = compile_with_modules ~raise p in
   mini_c
 
-let compile_expression ~raise ?(module_env = SMap.empty) : expression -> Mini_c.expression = fun e ->
-  trace ~raise spilling_tracer @@ compile_expression ~module_env:module_env e
+let compile_expression ~raise : expression -> Mini_c.expression = fun e ->
+  trace ~raise spilling_tracer @@ compile_expression e
 
 let compile_type ~raise : type_expression -> Mini_c.type_expression = fun e ->
   trace ~raise spilling_tracer @@ compile_type e
