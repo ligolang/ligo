f5f348965021150b94b2f52d83cc5c9d988ba073
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2022-02-11 21:07:48 -0300

    Revert "[LIGO-217] Add modules to syntax highlighting tool"
    This reverts commit 0df136d5206cd32f9b79dfb962874a911be4a57e.

--- tools/ligo-syntax-highlighting/CameLIGO.ml
+++ tools/ligo-syntax-highlighting/CameLIGO.ml
@@ -10,8 +10,7 @@ module Name = struct
   let numeric_literals          = "numericliterals"
   let operators                 = "operators"
   let identifier_constructor    = "identifierconstructor"
-  let module_access             = "moduleaccess"
-  let module_declaration        = "moduledeclaration"
+  let module_                   = "module"
   let attribute                 = "attribute"
 end
 
@@ -81,7 +80,6 @@ let syntax_highlighting =
         ("(", ")");
         ("\"", "\"");
         ("'", "'");
-        ("struct", "end");
       ];
       surrounding_pairs = [
         ("{", "}");
@@ -89,7 +87,6 @@ let syntax_highlighting =
         ("(", ")");
         ("\"", "\"");
         ("'", "'");
-        ("struct", "end");
       ];
       syntax_table = [
         ("\n", "> b");
@@ -109,8 +106,7 @@ let syntax_highlighting =
       Name.numeric_literals;
       Name.operators;
       Name.identifier_constructor;
-      Name.module_access;
-      Name.module_declaration;
+      Name.module_
     ];
     repository = [
       Helpers.attribute;
@@ -158,21 +154,11 @@ let syntax_highlighting =
         }
       };
       {
-        name = Name.module_access;
+        name = Name.module_;
         kind = Match {
           match_     = [
-            (Regexp.module_access_match1, Some Structure);
-            (Regexp.module_access_match2, Some Identifier)
-          ];
-          match_name = None
-        }
-      };
-      {
-        name = Name.module_declaration;
-        kind = Match {
-          match_ = [
-            (Regexp.module_declaration_match1, Some Keyword);
-            (Regexp.module_declaration_match2, Some Structure);
+            (Regexp.module_match1, Some Structure);
+            (Regexp.module_match2, Some Identifier)
           ];
           match_name = None
         }
--- tools/ligo-syntax-highlighting/ReasonLIGO.ml
+++ tools/ligo-syntax-highlighting/ReasonLIGO.ml
@@ -11,8 +11,7 @@ module Name = struct
   let numeric_literals          = "numericliterals"
   let operators                 = "operators"
   let identifier_constructor    = "identifierconstructor"
-  let module_access             = "moduleaccess"
-  let module_declaration        = "moduledeclaration"
+  let module_                   = "module"
   let attribute                 = "attribute"
 end
 
@@ -110,8 +109,7 @@ let syntax_highlighting =
       Name.numeric_literals;
       Name.operators;
       Name.identifier_constructor;
-      Name.module_access;
-      Name.module_declaration;
+      Name.module_
     ];
     repository = [
       Helpers.attribute;
@@ -167,21 +165,11 @@ let syntax_highlighting =
         }
       };
       {
-        name = Name.module_access;
+        name = Name.module_;
         kind = Match {
           match_     = [
-            (Regexp.module_access_match1, Some Structure);
-            (Regexp.module_access_match2, Some Identifier)
-          ];
-          match_name = None
-        }
-      };
-      {
-        name = Name.module_declaration;
-        kind = Match {
-          match_ = [
-            (Regexp.module_declaration_match1, Some Keyword);
-            (Regexp.module_declaration_match2, Some Structure);
+            (Regexp.module_match1, Some Structure);
+            (Regexp.module_match2, Some Identifier)
           ];
           match_name = None
         }
