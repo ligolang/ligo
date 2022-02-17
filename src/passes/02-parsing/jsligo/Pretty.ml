e99ca50589c78e87a7c44e14ba4546fdd690131c
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2022-02-17 16:23:18 +0100

    Fixes issue https://gitlab.com/ligolang/ligo/-/issues/1357

--- src/passes/02-parsing/jsligo/Pretty.ml
+++ src/passes/02-parsing/jsligo/Pretty.ml
@@ -364,17 +364,29 @@ and pp_module_access : type a.(a -> document) -> a module_access reg -> document
   let {module_name; field; _} = value in
   group (pp_ident module_name ^^ string "." ^^ break 0 ^^ f field)
 
- and pp_cartesian (node: CST.cartesian) =
+and pp_cartesian (node: CST.cartesian) =
   let pp_type_exprs = pp_nsepseq "," pp_type_expr
   in group (
     pp_attributes node.attributes ^^ hardline ^^
     pp_brackets pp_type_exprs node.inside)
 
- and pp_sum_type (node : sum_type reg) =
+and pp_sum_type (node : sum_type reg) =
   let {variants; attributes; _} = node.value in
-  let variants = pp_nsepseq "|" pp_variant variants.value in
-  if List.is_empty attributes then variants
-  else group (pp_attributes attributes ^/^ variants)
+  let head, tail = variants.value in
+  let head = pp_variant head
+  and padding_flat =
+    if attributes = [] then string "| " else empty
+  and padding_non_flat =
+    if attributes = [] then string "| " else blank 2 in
+  let head =
+    if List.is_empty tail then head
+    else ifflat (padding_flat ^^ head) (padding_non_flat ^^ head)
+  and tail = List.map ~f:snd tail
+  and app variant =
+    group (break 1 ^^ string "| " ^^ pp_variant variant) in
+  let thread = head ^^ concat_map app tail in
+  if attributes = [] then thread
+  else group (pp_attributes attributes ^/^ thread)
 
 and pp_variant (node : variant reg) =
   let {tuple; attributes; _} = node.value in
