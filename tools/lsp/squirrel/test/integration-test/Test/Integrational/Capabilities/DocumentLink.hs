d3bcca69ff960cea4748e3c8221d4f3aad91fd37
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2022-02-17 10:59:19 -0300

    [LIGO-412] Add FromCompiler tests where possible
    Problem: We want to check that FromCompiler tests work as expected.
    
    Solution: Add FromCompiler to integration tests, in addition to Standard
    ones.
    
    n.b.: A lot of tests are actually failing, they seem to be caused by
    LIGO-93 (in rename and find tests mostly), LIGO-208 (in various tests),
    and LIGO-446 (in a few PascaLIGO tests).

--- tools/lsp/squirrel/test/integration-test/Test/Integrational/Capabilities/DocumentLink.hs
+++ tools/lsp/squirrel/test/integration-test/Test/Integrational/Capabilities/DocumentLink.hs
@@ -3,13 +3,17 @@ module Test.Integrational.Capabilities.DocumentLink
   , unit_document_link_c
   ) where
 
-import AST.Scope (Standard)
+import AST.Scope (FromCompiler, Standard)
 
 import Test.Common.Capabilities.DocumentLink
 import Test.HUnit (Assertion)
 
 unit_document_link_b :: Assertion
-unit_document_link_b = documentLinkBDriver @Standard
+unit_document_link_b = do
+  documentLinkBDriver @Standard
+  documentLinkBDriver @FromCompiler
 
 unit_document_link_c :: Assertion
-unit_document_link_c = documentLinkCDriver @Standard
+unit_document_link_c = do
+  documentLinkCDriver @Standard
+  documentLinkCDriver @FromCompiler
