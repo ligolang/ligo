ed33fee7f4c24911097f10ca75fb53dcc74273b2
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-03-01 15:52:18 -0300

    Update available protocols list

--- src/environment/protocols.ml
+++ src/environment/protocols.ml
@@ -3,7 +3,7 @@ let current = Hangzhou
 
 (* this list is used to print the list of protocols in the CLI help *)
 let protocols_str : string list = [
-  "edo" ; "hangzhou"
+  "hangzhou" ; "ithaca"
 ]
 
 let protocols_to_variant : string -> t option = fun p ->
