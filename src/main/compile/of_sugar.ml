cb7add58102eb3ed0a88b945d45736c2f58ead32
Rémi <lesenechal.remi@gmail.com>
2022-03-15 19:12:50 +0100

    dead values in src/main/compile

--- src/main/compile/of_sugar.ml
+++ src/main/compile/of_sugar.ml
@@ -8,13 +8,3 @@ let compile ~raise (p : module_) : Ast_core.module_  =
 let compile_expression ~raise (e : expression) : Ast_core.expression  =
   let e = compile_expression e in
   Self_ast_core.all_expression ~raise e
-
-
-let list_declarations (m : module_) : expression_variable list =
-  List.fold_left
-    ~f:(fun prev el ->
-      let open Location in
-      match el.wrap_content with
-      | Declaration_constant {binder;_} -> binder.var::prev
-      | _ -> prev)
-    ~init:[] m
