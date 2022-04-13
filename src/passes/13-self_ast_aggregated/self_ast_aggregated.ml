6db8af00a0b17593f235e8768c99d58a3bbeade2
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-04-13 08:58:31 +0000

    Testing framework: fix recursive functions with multiple parameters

--- src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
+++ src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
@@ -3,6 +3,7 @@ module Helpers = Helpers
 
 let expression_obj ~raise = Obj_ligo.check_obj_ligo ~raise
 let expression_mono = Monomorphisation.mono_polymorphic_expr
+let expression_uncurry ~raise = Uncurry.uncurry_expression ~raise
 
 let all_expression ~raise e =
   let e = expression_obj ~raise e in
