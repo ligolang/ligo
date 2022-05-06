c556c5f709dcd7de10506894021e9db2cf3eee64
Melwyn Saldanha <melwyn95@gmail.com>
2022-05-06 19:04:17 +0530

    Add check for if all type variables are resolved in E_constructor

--- src/passes/10-checking/checking.ml
+++ src/passes/10-checking/checking.ml
@@ -346,6 +346,9 @@ and type_expression' ~raise ~add_warning ~options : context -> ?tv_opt:O.type_ex
     in
     let c_arg = type_expression' ~raise ~add_warning ~options (app_context, context) element in
     let table = Inference.infer_type_application ~raise ~loc:element.location avs Inference.TMap.empty c_arg_t c_arg.type_expression in
+    let () = if Option.is_none tv_opt then trace_option ~raise (not_annotated e.location) @@
+      if (List.for_all avs ~f:(fun v -> O.Helpers.TMap.mem v table)) then Some () else None
+    in
     let c_t = Ast_typed.Helpers.psubst_type table c_arg_t in
     let sum_t = Ast_typed.Helpers.psubst_type table sum_t in
     let () = assert_type_expression_eq ~raise c_arg.location (c_t, c_arg.type_expression) in
