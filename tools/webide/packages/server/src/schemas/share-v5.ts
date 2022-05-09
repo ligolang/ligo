8cd1de659164f1b1119aae36c29803750312c671
Melwyn Saldanha <melwyn95@gmail.com>
2022-05-09 13:53:54 +0530

    Upade share schema version

--- tools/webide/packages/server/src/schemas/share-v5.ts
+++ tools/webide/packages/server/src/schemas/share-v5.ts
@@ -1,11 +1,11 @@
 import joi from '@hapi/joi';
 
 import { Migration } from './migration';
-import { SchemaMigrationV3, SchemaV3 } from './share-v3';
+import { SchemaMigrationV4, SchemaV4 } from './share-v4';
 
-export type Version = 'v4';
+export type Version = 'v5';
 
-export interface SchemaV4 {
+export interface SchemaV5 {
   version: Version;
   state: {
     editor: {
@@ -16,24 +16,29 @@ export interface SchemaV4 {
     };
     compile: {
       entrypoint: string;
+      protocol: string;
     };
     dryRun: {
       entrypoint: string;
       parameters: string;
       storage: string;
+      protocol: string;
     };
     deploy: {
       entrypoint: string;
       storage: string;
       network: string;
       signer: string;
+      protocol: string;
     };
     evaluateValue: {
       entrypoint: string;
+      protocol: string;
     };
     evaluateFunction: {
       entrypoint: string;
       parameters: string;
+      protocol: string;
     };
     generateDeployScript: {
       tool: string;
@@ -41,12 +46,13 @@ export interface SchemaV4 {
       storage: string;
       originationAccount: string;
       burnCap: number;
+      protocol: string;
     };
   };
 }
 
-export class SchemaMigrationV4 extends Migration {
-  readonly VERSION: Version = 'v4';
+export class SchemaMigrationV5 extends Migration {
+  readonly VERSION: Version = 'v5';
 
   protected readonly schema = joi.object({
     version: joi.string().required().allow(this.VERSION),
@@ -61,24 +67,29 @@ export class SchemaMigrationV4 extends Migration {
         .required(),
       compile: joi.object({
         entrypoint: joi.string().allow(''),
+        protocol: joi.string().required(),
       }),
       dryRun: joi.object({
         entrypoint: joi.string().allow(''),
         parameters: joi.any().allow(''),
         storage: joi.any().allow(''),
+        protocol: joi.string().required(),
       }),
       deploy: joi.object({
         entrypoint: joi.string().allow(''),
         storage: joi.any().allow(''),
         network: joi.string().allow(''),
         signer: joi.string().allow(''),
+        protocol: joi.string().required(),
       }),
       evaluateValue: joi.object({
         entrypoint: joi.string().allow(''),
+        protocol: joi.string().required(),
       }),
       evaluateFunction: joi.object({
         entrypoint: joi.string().allow(''),
         parameters: joi.any().allow(''),
+        protocol: joi.string().required(),
       }),
       generateDeployScript: joi.object({
         tool: joi.string().allow(''),
@@ -86,23 +97,42 @@ export class SchemaMigrationV4 extends Migration {
         storage: joi.any().allow(''),
         originationAccount: joi.string().allow(''),
         burnCap: joi.number().allow(''),
+        protocol: joi.string().required(),
       }),
     }),
   });
 
-  protected readonly previous = new SchemaMigrationV3();
+  protected readonly previous = new SchemaMigrationV4();
 
-  protected migrate(data: SchemaV3): SchemaV4 {
+  protected migrate(data: SchemaV4): SchemaV5 {
     return {
       ...data,
       version: this.VERSION,
       state: {
         ...data.state,
+        compile: {
+          ...data.state.compile,
+          protocol: '',
+        },
+        dryRun: {
+          ...data.state.dryRun,
+          protocol: '',
+        },
         deploy: {
-          entrypoint: '',
-          storage: '',
-          network: '',
-          signer: '',
+          ...data.state.deploy,
+          protocol: '',
+        },
+        evaluateValue: {
+          ...data.state.evaluateValue,
+          protocol: '',
+        },
+        evaluateFunction: {
+          ...data.state.evaluateFunction,
+          protocol: '',
+        },
+        generateDeployScript: {
+          ...data.state.generateDeployScript,
+          protocol: '',
         },
       },
     };
