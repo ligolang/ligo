f37a9edc82380ed39d5c9f44a810f92041a89b83
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-05-20 09:29:21 -0300

    Add explicit cases and also in subst_external_term

--- src/passes/13-self_ast_aggregated/monomorphisation.ml
+++ src/passes/13-self_ast_aggregated/monomorphisation.ml
@@ -122,7 +122,9 @@ let apply_table_expr table (e : AST.expression) =
                   | E_assign { binder = { var ; ascr ; attributes } ; access_path ; expression } ->
                      let ascr = Option.map ~f:apply_table_type ascr in
                      return @@ E_assign { binder = { var ; ascr ; attributes } ; access_path ; expression }
-                  | _ -> return e.expression_content) () e in
+                  | E_literal _ | E_constant _ | E_variable _ | E_application _ | E_type_abstraction _
+                  | E_let_in _ | E_type_in _ | E_raw_code _ | E_constructor _ | E_record _
+                  | E_record_accessor _ | E_record_update _ -> return e.expression_content) () e in
   e
 
 let rec subst_external_type et t (u : AST.type_expression) =
@@ -158,6 +160,9 @@ let subst_external_term et t (e : AST.expression) =
                   match e.expression_content with
                   | E_type_inst { forall ; type_ } ->
                      return @@ E_type_inst { forall ; type_ = subst_external_type et t type_ }
+                  | E_lambda { binder = { var ; ascr ; attributes } ; result } ->
+                     let ascr = Option.map ~f:(subst_external_type et t) ascr in
+                     return @@ E_lambda { binder = { var ; ascr ; attributes } ; result }
                   | E_recursive { fun_name ; fun_type ; lambda } ->
                      let fun_type =  subst_external_type et t fun_type in
                      return @@ E_recursive { fun_name ; fun_type ; lambda }
@@ -166,7 +171,12 @@ let subst_external_term et t (e : AST.expression) =
                   | E_matching { matchee ; cases = Match_record { fields ; body ; tv } } ->
                      let fields = AST.LMap.map (fun binder -> AST.{ binder with ascr = Option.map ~f:(subst_external_type et t) binder.ascr }) fields in
                      return @@ E_matching { matchee ; cases = Match_record { fields ; body ; tv = subst_external_type et t tv } }
-                  | _ -> return e.expression_content) () e in
+                  | E_assign { binder = { var ; ascr ; attributes } ; access_path ; expression } ->
+                     let ascr = Option.map ~f:(subst_external_type et t) ascr in
+                     return @@ E_assign { binder = { var ; ascr ; attributes } ; access_path ; expression }
+                  | E_literal _ | E_constant _ | E_variable _ | E_application _ | E_type_abstraction _
+                  | E_let_in _ | E_type_in _ | E_raw_code _ | E_constructor _ | E_record _
+                  | E_record_accessor _ | E_record_update _ -> return e.expression_content) () e in
   e
 
 (* A term might have remaining external typer usages around. If the
