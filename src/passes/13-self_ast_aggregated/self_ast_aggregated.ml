488e3a3c5f01e59d1031041a496ff32395e15408
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-05-20 08:46:30 -0300

    Revert swaping of passes

--- src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
+++ src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
@@ -6,7 +6,7 @@ let expression_obj ~raise e = Obj_ligo.check_obj_ligo ~raise e
 let all_expression ~raise ~(options : Compiler_options.middle_end) e =
   let e = Helpers.map_expression (Polymorphic_replace.expression ~protocol:options.protocol_version) e in
   let e = if not options.test then Obj_ligo.check_obj_ligo ~raise e else e in
-  let e = Monomorphisation.mono_polymorphic_expr e in
   let e = Purify_assignations.expression e in
+  let e = Monomorphisation.mono_polymorphic_expr e in
   let e = Uncurry.uncurry_expression e in
   e
