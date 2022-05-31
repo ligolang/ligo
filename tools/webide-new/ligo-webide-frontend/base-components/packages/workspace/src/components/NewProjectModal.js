0466cc01c42a55e1ff612fbe8a01d6fd0a1c0f0d
Aleksandr Pakulev <sashasashasasha151@gmail.com>
2022-06-01 03:37:28 +0400

    [LIGO-518] Minimise NewProjectModal
    Problem: NewProjectModal in EthereumStudio is a complex modal
    with various fields and selectors. It also have different styles
    for web and desktop app. But the only thing we need from it is just
    name and template.
    
    Solution: remove all things which extend NewProjectModal. And also
    change templates.

--- tools/webide-new/ligo-webide-frontend/base-components/packages/workspace/src/components/NewProjectModal.js
+++ tools/webide-new/ligo-webide-frontend/base-components/packages/workspace/src/components/NewProjectModal.js
@@ -2,22 +2,12 @@ import React, { PureComponent } from 'react'
 
 import {
   Modal,
-  ButtonOptions,
-  FormGroup,
-  Label,
-  InputGroup,
-  InputGroupAddon,
-  Input,
-  Button,
   DebouncedFormGroup,
-  DropdownInput,
+  DropdownInput
 } from '@obsidians/ui-components'
 
-import platform from '@obsidians/platform'
 import fileOps from '@obsidians/file-ops'
-import Auth from '@obsidians/auth'
 import notification from '@obsidians/notification'
-import Terminal from '@obsidians/terminal'
 
 import ProjectManager from '../ProjectManager'
 import actions from '../actions'
@@ -27,160 +17,72 @@ export default class NewProjectModal extends PureComponent {
     super(props)
 
     this.state = {
-      remote: platform.isWeb,
       name: '',
-      invalid: false,
-      projectRoot: '',
       template: props.defaultTemplate,
-      group: props.defaultGroup,
-      creating: false,
-      showTerminal: false,
+      creating: false
     }
 
     this.modal = React.createRef()
-    this.terminal = React.createRef()
-    this.path = fileOps.current.path
-    this.fs = fileOps.current.fs
+    this.path = fileOps.pathHelper
 
     actions.newProjectModal = this
   }
 
-  openModal (remote) {
-    const { defaultTemplate, defaultGroup } = this.props
+  openModal () {
+    const { defaultTemplate } = this.props
     this.setState({
-      remote,
       template: defaultTemplate,
-      group: defaultGroup,
-      creating: false,
-      showTerminal: false,
+      creating: false
     })
     this.forceUpdate()
     this.modal.current.openModal()
     return new Promise(resolve => { this.onConfirm = resolve })
   }
 
-  chooseProjectPath = async () => {
-    try {
-      const projectRoot = await fileOps.current.chooseFolder()
-      this.setState({ projectRoot })
-    } catch (e) {
-
-    }
-  }
-
   onCreateProject = async () => {
     this.setState({ creating: true })
 
-    const { remote, name, template, group } = this.state
+    const { name, template } = this.state
 
-    let projectRoot
-    if (!remote) {
-      if (!this.state.projectRoot) {
-        projectRoot = this.path.join(fileOps.current.workspace, name)
-      } else if (!this.path.isAbsolute(this.state.projectRoot)) {
-        projectRoot = this.path.join(fileOps.current.workspace, this.state.projectRoot)
-      } else {
-        projectRoot = this.state.projectRoot
-      }
-    }
-
-    const created = await this.createProject({ projectRoot, name, template, group })
+    const created = await this.createProject(name, template)
 
     if (created) {
       this.modal.current.closeModal()
       this.onConfirm(created)
-      this.setState({ name: '', projectRoot: '', template: this.props.defaultTemplate, creating: false, showTerminal: false })
+      this.setState({ name: '', template: this.props.defaultTemplate, creating: false })
     } else {
       this.setState({ creating: false })
     }
   }
 
-  async createProject ({ notify = true, ...options }, stage = '') {
+  async createProject (name, template) {
     try {
-      const Manager = this.state.remote ? ProjectManager.Remote : ProjectManager.Local
-      const created = await Manager.createProject(options, stage)
-      if (notify) {
-        notification.success('Successful', `New project <b>${options.name}</b> is created.`)
-      }
+      const Manager = ProjectManager.Local
+      const created = await Manager.createProject(name, template)
+      notification.success('Successful', `New project <b>${name}</b> is created.`)
       return created
     } catch (e) {
-      // if (notify) {
       notification.error('Cannot Create the Project', e.message)
-      // }
-      return false
+      return undefined
     }
-  }
-
-  renderLocation = () => {
-    if (platform.isDesktop && Auth.username) {
-      return (
-        <div>
-          <ButtonOptions
-            className='mb-3'
-            options={[
-              { key: 'local', text: 'Local', icon: 'fas fa-desktop mr-1' },
-              { key: 'cloud', text: 'Cloud', icon: 'fas fa-cloud mr-1' },
-            ]}
-            selected={this.state.remote ? 'cloud' : 'local'}
-            onSelect={key => this.setState({ remote: key === 'cloud' })}
-          />
-        </div>
-      )
-    }
-    return null
-  }
-
-  renderProjectPath = () => {
-    if (this.state.remote) {
-      return null
-    }
-
-    let placeholder = 'Project path'
-    if (!this.state.projectRoot) {
-      placeholder = this.path.join(fileOps.current.workspace, this.state.name || '')
-    }
-
-    return (
-      <FormGroup>
-        <Label>Project location</Label>
-        <InputGroup>
-          <Input
-            placeholder={placeholder}
-            value={this.state.projectRoot}
-            onChange={e => this.setState({ projectRoot: e.target.value })}
-          />
-          <InputGroupAddon addonType='append'>
-            <Button color='secondary' onClick={this.chooseProjectPath}>
-              Choose...
-            </Button>
-          </InputGroupAddon>
-        </InputGroup>
-      </FormGroup>
-    )
   }
 
   renderTemplate () {
-    const { noTemplate, templates } = this.props
-    const { remote, template } = this.state
-    if (noTemplate) {
-      return null
-    }
+    const { templates } = this.props
+    const { template } = this.state
     return (
       <DropdownInput
         label='Template'
-        options={templates.filter(t => !remote || !t.local)}
+        options={templates}
         placeholder='(Please select a template)'
         value={template}
-        onChange={(template, group) => this.setState({ template, group })}
+        onChange={template => this.setState({ template })}
       />
     )
   }
 
-  renderOtherOptions = () => null
-
   render () {
-    const { projectNameProps, templates } = this.props
-    const { name, invalid, creating, showTerminal } = this.state
+    const { name, creating } = this.state
 
     return (
       <Modal
@@ -189,28 +91,27 @@ export default class NewProjectModal extends PureComponent {
         textConfirm='Create Project'
         onConfirm={this.onCreateProject}
         pending={creating && 'Creating...'}
-        confirmDisabled={!name || invalid}
+        confirmDisabled={!name}
       >
-        {this.renderLocation()}
-        {this.renderProjectPath()}
         <DebouncedFormGroup
           label='Project name'
           value={name}
-          onChange={(name, invalid) => this.setState({ name, invalid })}
-          {...projectNameProps}
+          onChange={name => this.setState({ name })}
         />
-        {this.renderTemplate()}
-        {this.renderOtherOptions()}
-        <div style={{ display: showTerminal ? 'block' : 'none'}}>
-          <Terminal
-            ref={this.terminal}
-            active={showTerminal}
-            height='200px'
-            logId='create-project'
-            className='rounded overflow-hidden'
-          />
-        </div>
+        {this.renderTemplate(true)}
       </Modal>
     )
   }
+}
+
+const templates = [
+  { id: 'empty', display: 'Empty Project' },
+  { id: 'increment', display: 'Increment' },
+  { id: 'id', display: 'ID' },
+  { id: 'hashlock', display: 'Hashlock Contract' }
+]
+
+NewProjectModal.defaultProps = {
+  defaultTemplate: 'increment',
+  templates
 }
