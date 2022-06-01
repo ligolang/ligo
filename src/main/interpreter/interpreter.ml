ee9528c8fb6cb75298ad99171dbb09dab558da3d
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-06-01 09:29:51 -0300

    Add C_TEST_READ_CONTRACT_FROM_FILE

--- src/main/interpreter/interpreter.ml
+++ src/main/interpreter/interpreter.ml
@@ -857,7 +857,7 @@ let rec apply_operator ~raise ~add_warning ~steps ~(options : Compiler_options.t
          C_SET_LITERAL | C_LIST_LITERAL | C_MAP | C_MAP_LITERAL | C_MAP_GET | C_MAP_GET_FORCE |
          C_BIG_MAP | C_BIG_MAP_LITERAL | C_BIG_MAP_GET_AND_UPDATE | C_CALL | C_CONTRACT |
          C_CONTRACT_OPT | C_CONTRACT_WITH_ERROR | C_CONTRACT_ENTRYPOINT |
-         C_CONTRACT_ENTRYPOINT_OPT | C_SET_DELEGATE |
+         C_CONTRACT_ENTRYPOINT_OPT | C_SET_DELEGATE | C_TEST_READ_CONTRACT_FROM_FILE |
          C_CREATE_CONTRACT | C_OPEN_CHEST | C_VIEW | C_GLOBAL_CONSTANT) , _ ) ->
       fail @@ Errors.generic_error loc "Unbound primitive."
   )
