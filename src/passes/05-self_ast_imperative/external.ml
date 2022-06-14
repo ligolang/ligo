a012eccba7920b3c7e9ea1a1492820d6b56f17bc
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-06-14 09:51:40 -0300

    Remove %external handling

--- src/passes/05-self_ast_imperative/external.ml
+++ src/passes/05-self_ast_imperative/external.ml
@@ -1,15 +1,5 @@
 open Ast_imperative
 
-(* This function transforms an application expression `l e1 ... en` into the pair `([ e1 ; ... ; en ] , l)` *)
-let destruct_applications (e : expression) =
-  let rec destruct_applications acc (lamb : expression) =
-    match lamb.expression_content with
-    | E_application {lamb;args} ->
-       destruct_applications (args :: acc) lamb
-    | _ ->
-       (lamb, acc) in
-  destruct_applications [] e
-
 let destruct_args (m : expression list) : (string * expression list) option =
   match m with
   | { expression_content = E_literal (Literal_string prim) ; _ } :: args ->
@@ -18,41 +8,6 @@ let destruct_args (m : expression list) : (string * expression list) option =
 
 let replace : expression -> expression = fun e ->
   match e.expression_content with
-  | E_application _ -> (
-     let e_, args = destruct_applications e in
-     match e_.expression_content with
-     | E_raw_code { language ; code = { expression_content = E_literal (Literal_string code) ; _ } }
-          when String.equal language "external" -> (
-       let code = (Simple_utils.Ligo_string.extract code) in
-       match read_constant' code with
-       | None -> failwith @@ "Constant cannot be externalized: " ^ code
-       | Some cons ->
-          let expression_content = E_constant { cons_name = Const cons;
-                                                arguments = args } in
-          { e with expression_content }
-     )
-     | _ -> e
-  )
-  | E_raw_code { language ; code = { expression_content = E_literal (Literal_string code) ; _ } }
-       when String.equal language "external" -> (
-    let code = (Simple_utils.Ligo_string.extract code) in
-    match read_constant' code with
-    | None -> failwith @@ "Constant cannot be externalized: " ^ code
-    | Some cons ->
-       let expression_content = E_constant { cons_name = Const cons;
-                                             arguments = [] } in
-       { e with expression_content }
-  )
-  | E_raw_code { language ; code = { expression_content = E_literal (Literal_string code) ; _ } }
-       when String.equal language "external" -> (
-    let code = (Simple_utils.Ligo_string.extract code) in
-    match read_constant' code with
-    | None -> failwith @@ "Constant cannot be externalized: " ^ code
-    | Some cons ->
-       let expression_content = E_constant { cons_name = Const cons;
-                                             arguments = [] } in
-       { e with expression_content }
-  )
   | E_raw_code { language ; code = { expression_content = E_tuple m ; _ } }
        when String.equal language "ext" -> (
     match destruct_args m with
