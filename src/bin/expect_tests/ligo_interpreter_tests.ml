d11ea13fa7afc8b318818137aebd4a0fab2b0617
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-06-15 09:28:34 -0300

    Add test case

--- src/bin/expect_tests/ligo_interpreter_tests.ml
+++ src/bin/expect_tests/ligo_interpreter_tests.ml
@@ -622,6 +622,12 @@ let%expect_test _ =
     Everything at the top-level was executed.
     - test exited with value (). |}]
 
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; test "test_transfer_entrypoint.ligo" ] ;
+  [%expect {|
+    Everything at the top-level was executed.
+    - test_entrypoint exited with value "Passed". |}]
+
 (* do not remove that :) *)
 let () = Sys.chdir pwd
 
