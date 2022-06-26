f5e7e386a909011287303e00184b62656836af5b
sashasashasasha151 <sashasashasasha151@gmail.com>
2022-06-27 03:53:26 +0400

    [LIGO-643] Add links
    Problem: we do not have any links on ligo
    pages
    
    Solution: add useful links to the top right menu

--- tools/webide-new/ligo-webide-frontend/base-components/packages/navbar/src/User.js
+++ tools/webide-new/ligo-webide-frontend/base-components/packages/navbar/src/User.js
@@ -34,11 +34,28 @@ class User extends Component {
   }
 
   renderDropdownMenus = profile => {
-    let dropdownItems = [
+    const links = [
+      { name: 'Install', link: 'https://ligolang.org/docs/intro/installation/', icon: 'fa-tools', key: 'install' },
+      { name: 'Docs', link: 'https://ligolang.org/docs/intro/introduction/', icon: 'fa-book', key: 'docs' },
+      { name: 'Tutorials', link: 'https://ligolang.org/docs/tutorials/getting-started/getting-started/', icon: 'fa-graduation-cap', key: 'tutorials' },
+      { name: 'Blog', link: 'https://forum.tezosagora.org/tag/ligo', icon: 'fa-comment', key: 'blog' },
+      { name: 'Ask Questions', link: 'https://ligolang.org/contact/', icon: 'fa-question', key: 'ask-questions' },
+      { name: 'Changelog', link: 'https://ligolang.org/docs/next/intro/changelog/', icon: 'fa-list-ul', key: 'changelog' },
+      { name: 'Cheat Sheet', link: 'https://ligolang.org/docs/api/cheat-sheet', icon: 'fa-hand-holding', key: 'cheat-sheet' }
+    ]
+
+    const dropdownItems = [
       <DropdownItem key='divider-2' divider />,
       <DropdownItem key='my-projects' onClick={() => this.props.history.push(`/local`)}>
         <i className='fas fa-th-list w-3 mr-2' />My Projects
-        </DropdownItem>
+      </DropdownItem>,
+      ...links.map(link => {
+        return (
+          <DropdownItem key={link.key} onClick={() => window.open(link.link, '_blank')}>
+            <i className={`fas ${link.icon} w-3 mr-2`} />{link.name}
+          </DropdownItem>
+        )
+      })
     ]
 
     const isDropdownOpen = this.state.isDropdownOpen
