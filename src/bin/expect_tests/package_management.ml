725f65bbf95ba317f07d99b03c4f33bab6493fca
Melwyn Saldanha <melwyn95@gmail.com>
2022-07-19 15:54:23 +0530

    Add tests for package management inferred --project-root

--- src/bin/expect_tests/package_management.ml
+++ src/bin/expect_tests/package_management.ml
@@ -9,15 +9,63 @@ let%expect_test _ =
     - test exited with value KT1Riu7zn7S1PCTu197y2i29TGheSLzfeaZ6(None). |}]
 
 let%expect_test _ = 
-  run_ligo_good [ "info"; "measure-contract" ; "using_scope_pkg_project/contract.mligo" ; "--project-root" ; "using_scope_pkg_project" ] ;
+  run_ligo_good [ "info"; "measure-contract" ; "using_scope_pkg_project/src/a/b/c/contract.mligo" ; "--project-root" ; "using_scope_pkg_project" ] ;
   [%expect{|
     95 bytes |}]
 
 let pwd = Sys.getcwd ()
 let () = Sys.chdir "using_scope_pkg_project"
 let%expect_test _ =
-  run_ligo_good [ "run"; "test" ; "contract.test.mligo" ; "--project-root" ; "." ] ;
+  run_ligo_good [ "run"; "test" ; "src/a/b/c/contract.test.mligo" ; "--project-root" ; "." ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
+
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "src/a/b/c/contract.test.mligo" ] ;
   [%expect{|
     Everything at the top-level was executed.
     - test_originate exited with value (). |}]
 let () = Sys.chdir pwd
+
+let pwd = Sys.getcwd ()
+let () = Sys.chdir "using_scope_pkg_project/src/a/b/c"
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "contract.test.mligo" ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
+let () = Sys.chdir pwd
+
+let pwd = Sys.getcwd ()
+let () = Sys.chdir "using_scope_pkg_project/src/a/b"
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "c/contract.test.mligo" ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
+let () = Sys.chdir pwd
+
+let pwd = Sys.getcwd ()
+let () = Sys.chdir "using_scope_pkg_project/src/a"
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "b/c/contract.test.mligo" ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
+let () = Sys.chdir pwd
+
+let pwd = Sys.getcwd ()
+let () = Sys.chdir "using_scope_pkg_project/src"
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "a/b/c/contract.test.mligo" ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
+let () = Sys.chdir pwd
+
+let%expect_test _ =
+  run_ligo_good [ "run"; "test" ; "using_scope_pkg_project/src/a/b/c/contract.test.mligo" ; "--project-root" ; "using_scope_pkg_project" ] ;
+  [%expect{|
+    Everything at the top-level was executed.
+    - test_originate exited with value (). |}]
