26ea60d73756f2aa4ee34cdbaad3e3cbae76a0eb
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-07-21 16:52:40 +0200

    Remove self_typing from Self_ast_typed

--- src/passes/11-self_ast_typed/contract_passes.ml
+++ src/passes/11-self_ast_typed/contract_passes.ml
@@ -7,52 +7,6 @@ type contract_pass_data = {
   main_name : expression_variable ;
 }
 
-let annotation_or_label annot label = Option.value ~default:(String.uncapitalize label) @@ Ast_typed.Helpers.remove_empty_annotation annot
-
-let check_entrypoint_annotation_format ~raise ep (exp: expression) =
-  match String.split ~on:'%' ep with
-    | [ "" ; ep'] -> ep'
-    | _ -> raise.error @@ Errors.bad_format_entrypoint_ann ep exp.location
-
-let self_typing ~raise : contract_pass_data -> expression -> bool * contract_pass_data * expression = fun dat e ->
-  let bad_self_err () = Errors.bad_self_type
-    e.type_expression
-    {e.type_expression with
-      type_content =
-        T_constant {
-          language=Stage_common.Backends.michelson;
-          injection=Stage_common.Constant.Contract;
-          parameters=[dat.contract_type.parameter]
-        }
-    }
-    e.location
-  in
-  match e.expression_content , e.type_expression with
-  | (E_constant {cons_name=C_SELF ; arguments=[entrypoint_exp]} , {type_content = T_constant {language=_;injection=Stage_common.Constant.Contract;parameters=[t]} ; _}) ->
-    let entrypoint =
-      match entrypoint_exp.expression_content with
-      | E_literal (Literal_string ep) -> check_entrypoint_annotation_format ~raise (Ligo_string.extract ep) entrypoint_exp
-      | _ -> raise.error @@ Errors.entrypoint_annotation_not_literal entrypoint_exp.location
-    in
-    let entrypoint_t =
-      match dat.contract_type.parameter.type_content with
-      | (T_sum _ as t) when String.equal "default" (String.uncapitalize entrypoint) -> {dat.contract_type.parameter with type_content = t}
-      | T_sum cmap ->
-        let content = LMap.to_kv_list cmap.content in
-        let content = List.map ~f:(fun (Label entrypoint, {michelson_annotation;associated_type;_}) ->
-                          (annotation_or_label michelson_annotation entrypoint, associated_type)) content in
-        let associated_type = trace_option ~raise (Errors.unmatched_entrypoint entrypoint_exp.location) @@
-          List.Assoc.find content ~equal:String.equal entrypoint
-        in
-        associated_type
-      | t -> {dat.contract_type.parameter with type_content = t}
-    in
-    let () =
-      trace_option ~raise (bad_self_err ()) @@
-      Ast_typed.assert_type_expression_eq (entrypoint_t , t) in
-    (true, dat, e)
-  | _ -> (true,dat,e)
-
 module VVarSet = Caml.Set.Make(ValueVar)
 module MVarMap = Simple_utils.Map.Make(ModuleVar)
 
--- src/passes/11-self_ast_typed/self_ast_typed.ml
+++ src/passes/11-self_ast_typed/self_ast_typed.ml
@@ -17,7 +17,6 @@ let all_expression_passes ~raise ~warn_unused_rec = [
 
 let contract_passes ~raise = [
   (* REMITODO: Move old self_mini_c.ml "self in lambda" check *)
-  Contract_passes.self_typing ~raise ;
   No_nested_big_map.self_typing ~raise ;
 ]
 
