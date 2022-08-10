8962312005c3ac300107ae3b5e0b2cbdc62ff3fa
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2022-08-10 15:04:47 -0300

    [LIGO-688] Refactor TextMate and VSCode generators
    Problem: We change Emacs and Vim to take a list of syntaxes, but Emacs
    and VS Code still expect individual arguments, resulting in a lot of
    duplicated code. Moreover, these two generators are almost identical,
    with a lot of duplication.
    
    Solution: Extract common functionality into a new function and fold over
    the list of syntaxes, stopping at the first `Error`, if any. This should
    simulate a `traverse` in Haskell.

--- tools/ligo-syntax-highlighting/LigoSyntaxHighlighting.ml
+++ tools/ligo-syntax-highlighting/LigoSyntaxHighlighting.ml
@@ -38,36 +38,36 @@ let vim_syntax_highlighting dir syntaxes =
   Fun.flip List.iter syntaxes (fun (name, syntax) ->
     let vim_syntax = vim_syntax (name, syntax) in
     write_file (name ^ ".vim") "syntax" vim_syntax);
-  let () = print_endline "Success (Vim)" in
   `Ok ()
 
-let vscode_syntax_highlighting: string -> string -> string -> string -> SyntaxHighlighting.Core.t -> unit Term.ret = fun dir syntax_file language_file syntax textmate ->
-  let jsons = SyntaxHighlighting.Textmate.to_jsons syntax textmate in
-  match jsons with
-    Ok (syntax_highlighting_json, language_conf_json) ->
-      let s = Yojson.Safe.pretty_to_string syntax_highlighting_json in
-      let () = output_file dir syntax_file s in
-      let s = Yojson.Safe.pretty_to_string language_conf_json in
-      let () = output_file dir language_file s in
-      print_endline ("Success (Visual Studio Code): " ^ syntax);
-      `Ok ()
-  | Error SyntaxHighlighting.Core.Referenced_rule_does_not_exist s -> `Error (false, Format.sprintf "Referenced rule '%s' does not exist." s)
-  | Error Meta_name_some_but_empty s -> `Error (false, Format.sprintf  "%s.name has no value, but is expected to." s)
-  | Error Begin_cant_be_empty s -> `Error (false, Format.sprintf  "%s.begin_ can't be empty." s)
-  | Error End_cant_be_empty s -> `Error (false, Format.sprintf  "%s.end_ can't be empty" s)
+let textmate_syntax_highlighting_impl (generate_configuration: bool) (dir: string) (syntaxes: (string * SyntaxHighlighting.Core.t) list): unit Term.ret =
+  let go (syntax, textmate) = function
+    | Ok () -> (
+      match SyntaxHighlighting.Textmate.to_jsons syntax textmate with
+      | Ok (syntax_highlighting_json, language_conf_json) ->
+          let s = Yojson.Safe.pretty_to_string syntax_highlighting_json in
+          let syntax_file = syntax ^ ".tmLanguage.json" in
+          output_file dir syntax_file s;
+          (if generate_configuration then
+            let s = Yojson.Safe.pretty_to_string language_conf_json in
+            let language_file = syntax ^ ".configuration.json" in
+            output_file dir language_file s);
+          Ok ()
+      | Error SyntaxHighlighting.Core.Referenced_rule_does_not_exist s -> Error (Format.sprintf "Referenced rule '%s' does not exist." s)
+      | Error Meta_name_some_but_empty s -> Error (Format.sprintf "%s.name has no value, but is expected to." s)
+      | Error Begin_cant_be_empty s -> Error (Format.sprintf "%s.begin_ can't be empty." s)
+      | Error End_cant_be_empty s -> Error (Format.sprintf "%s.end_ can't be empty" s))
+    | Error s -> Error s
+  in
+  match List.fold_left (Fun.flip go) (Ok ()) syntaxes with
+  | Ok () -> `Ok ()
+  | Error s -> `Error (false, s)
 
-let textmate_syntax_highlighting: string -> string -> string -> SyntaxHighlighting.Core.t -> unit Term.ret = fun dir file syntax textmate ->
-  let jsons = SyntaxHighlighting.Textmate.to_jsons syntax textmate in
-  match jsons with
-    Ok (syntax_highlighting_json, _) ->
-      let s = Yojson.Safe.pretty_to_string syntax_highlighting_json in
-      let () = output_file dir file s in
-      print_endline ("Success (TextMate): " ^ syntax);
-      `Ok ()
-  | Error SyntaxHighlighting.Core.Referenced_rule_does_not_exist s -> `Error (false, Format.sprintf "Referenced rule '%s' does not exist." s)
-  | Error Meta_name_some_but_empty s -> `Error (false, Format.sprintf  "%s.name has no value, but is expected to." s)
-  | Error Begin_cant_be_empty s -> `Error (false, Format.sprintf  "%s.begin_ can't be empty." s)
-  | Error End_cant_be_empty s -> `Error (false, Format.sprintf  "%s.end_ can't be empty" s)
+let vscode_syntax_highlighting: string -> (string * SyntaxHighlighting.Core.t) list -> unit Term.ret =
+  textmate_syntax_highlighting_impl true
+
+let textmate_syntax_highlighting: string -> (string * SyntaxHighlighting.Core.t) list -> unit Term.ret =
+  textmate_syntax_highlighting_impl false
 
 let emacs_syntax_highlighting dir syntaxes =
   let buffer = Buffer.create 1000 in
@@ -84,7 +84,6 @@ let emacs_syntax_highlighting dir syntaxes =
   Print.print_footer fmt;
   let emacs_output = Buffer.contents buffer in
   let () = output_file dir "ligo-mode.el" emacs_output in
-  let () = print_endline "Success (Emacs)" in
   `Ok ()
 
 let ( let* ) o f : _ Term.ret  =
@@ -95,30 +94,29 @@ let ( let* ) o f : _ Term.ret  =
 
 let output: string -> string -> string -> string -> _ Term.ret = fun vscode_directory vim_directory emacs_directory textmate_directory ->
   if not (Sys.is_directory vscode_directory) then
-    `Error (false, "Not a valid directory to output VSCode files")
+    `Error (false, "Not a valid directory to output Visual Studio Code files")
   else if not (Sys.is_directory vim_directory) then
-    `Error (false, "Not a valid directory to output VIM files")
+    `Error (false, "Not a valid directory to output Vim files")
   else if not (Sys.is_directory emacs_directory) then
-    `Error (false, "Not a valid directory to output EMacs files")
+    `Error (false, "Not a valid directory to output Emacs files")
   else if not (Sys.is_directory textmate_directory) then
     `Error (false, "Not a valid directory to output TextMate files")
   else (
-    let syntaxes = [
+    let syntaxes_without_jsligo = [
       ("ligo", PascaLIGO.syntax_highlighting);
       ("mligo", CameLIGO.syntax_highlighting);
       ("religo", ReasonLIGO.syntax_highlighting);
     ] in
+    let syntaxes = ("jsligo", JsLIGO.syntax_highlighting) :: syntaxes_without_jsligo in
 
-    let* _ = vscode_syntax_highlighting vscode_directory "ligo.tmLanguage.json" "ligo.configuration.json" "ligo" PascaLIGO.syntax_highlighting in
-    let* _ = textmate_syntax_highlighting textmate_directory "ligo.tmLanguage.json" "ligo" PascaLIGO.syntax_highlighting in
-    let* _ = vscode_syntax_highlighting vscode_directory "mligo.tmLanguage.json" "mligo.configuration.json" "mligo" CameLIGO.syntax_highlighting in
-    let* _ = textmate_syntax_highlighting textmate_directory "mligo.tmLanguage.json" "mligo" CameLIGO.syntax_highlighting in
-    let* _ = vscode_syntax_highlighting vscode_directory "religo.tmLanguage.json" "religo.configuration.json" "religo" ReasonLIGO.syntax_highlighting in
-    let* _ = textmate_syntax_highlighting textmate_directory "religo.tmLanguage.json" "religo" ReasonLIGO.syntax_highlighting in
-    let* _ = vscode_syntax_highlighting vscode_directory "jsligo.tmLanguage.json" "jsligo.configuration.json" "jsligo" JsLIGO.syntax_highlighting in
-    let* _ = textmate_syntax_highlighting textmate_directory "jsligo.tmLanguage.json" "jsligo" JsLIGO.syntax_highlighting in
-    let* _ = vim_syntax_highlighting vim_directory syntaxes in
-    let* _ = emacs_syntax_highlighting emacs_directory syntaxes in
+    let* _ = vscode_syntax_highlighting vscode_directory syntaxes in
+    print_endline "Success (Visual Studio Code)";
+    let* _ = textmate_syntax_highlighting textmate_directory syntaxes in
+    print_endline "Success (TextMate)";
+    let* _ = vim_syntax_highlighting vim_directory syntaxes_without_jsligo in
+    print_endline "Success (Vim)";
+    let* _ = emacs_syntax_highlighting emacs_directory syntaxes_without_jsligo in
+    print_endline "Success (Emacs)";
     let () = print_endline "Successfully generated syntaxes" in
     `Ok ()
   )
