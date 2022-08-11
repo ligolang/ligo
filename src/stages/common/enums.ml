672bce6f906fbda25450044217f915e9bf0eed73
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-08-11 09:38:04 -0300

    Add C_TEST_TRY_WITH operation

--- src/stages/common/enums.ml
+++ src/stages/common/enums.ml
@@ -217,6 +217,7 @@ type constant' =
   | C_TEST_SIGN [@only_interpreter]
   | C_TEST_GET_ENTRYPOINT [@only_interpreter]
   | C_TEST_LAST_EVENTS [@only_interpreter]
+  | C_TEST_TRY_WITH [@only_interpreter]
   (* New with EDO*)
   | C_SAPLING_VERIFY_UPDATE
   | C_SAPLING_EMPTY_STATE
