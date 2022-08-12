be655eef499e8f4ee0f4dfad4a12e4c38db761ad
Melwyn Saldanha <melwyn95@gmail.com>
2022-08-12 15:49:09 +0530

    dune promote

--- src/bin/expect_tests/help_tests.ml
+++ src/bin/expect_tests/help_tests.ml
@@ -19,10 +19,10 @@ let%expect_test _ =
       changelog  print the ligo changelog
       print      print intermediary program representation.
                  Warning: Intended for development of LIGO and can break at any time
-      install    install LIGO packages declared in package.json
-      publish    publish the LIGO package declared in package.json
-      add-user   create a new user for the LIGO package registry
-      login      login to the LIGO package registry
+      install    install LIGO dependencies declared in package.json
+      publish    [BETA] publish the LIGO package declared in package.json
+      add-user   [BETA] create a new user for the LIGO package registry
+      login      [BETA] login to the LIGO package registry
       daemon     launch LIGO dameon
       version    print version information
       help       explain a given subcommand (perhaps recursively)
