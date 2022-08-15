8ca012a94b3706fe6723bfc6ff9db0d79ef51818
Leonid Vasilev <leonid.vasilev@serokell.io>
2022-08-15 16:05:10 +0300

    [LIGO-637] Create 2 children for addresses
    Problem: now if we have an address with entrypoint then it would
    be displayed in variables pane like `tz1abcabcabc12314abc%foo`
    (where `tz1abcabcabc12314abc` is address and `foo` is an entrypoint).
    For better UX we can create 2 children for these addresses
    
    Solution: create 2 subvars for `VContract` values
    (for some reason `ligo-debugger` treats addresses like contracts)
    
    N.B.: tested it on a contract from
    https://gitlab.com/morley-framework/morley-debugger/-/issues/70.
    Before running it we should remove `--disable-michelson-typechecking`
    flag in `Call`

--- tools/debugger/ligo-debugger/src/Language/LIGO/DAP/Variables.hs
+++ tools/debugger/ligo-debugger/src/Language/LIGO/DAP/Variables.hs
@@ -11,15 +11,16 @@ import Data.HashMap.Strict qualified as HM
 import Data.Map qualified as M
 import Data.Text qualified as T
 import Fmt (pretty)
+import Morley.Debugger.Core (DebugPrintMode (DpmEvaluated, DpmNormal), debugBuild)
+import Morley.Debugger.Protocol.DAP (Variable)
+import Morley.Debugger.Protocol.DAP qualified as DAP
+import Morley.Michelson.Typed (SomeConstrainedValue (SomeValue), Value, Value' (..))
+
 import Language.LIGO.Debugger.CLI.Types
   (LigoExposedStackEntry (LigoExposedStackEntry, leseDeclaration, leseType),
   LigoStackEntry (LigoStackEntry), LigoType (LTApp, LTRecord, LTUnresolved),
   LigoTypeApp (LigoTypeApp, ltaArguments))
 import Language.LIGO.Debugger.Snapshots (StackItem (StackItem))
-import Morley.Debugger.Core (DebugPrintMode (DpmEvaluated, DpmNormal), debugBuild)
-import Morley.Debugger.Protocol.DAP (Variable)
-import Morley.Debugger.Protocol.DAP qualified as DAP
-import Morley.Michelson.Typed (SomeConstrainedValue (SomeValue), Value, Value' (..))
 
 -- | For a given stack generate its representation as a tree of 'DAP.Variable's.
 --
@@ -56,21 +57,25 @@ insertVars vars = do
   _2 %= M.insert nextIdx vars
   return nextIdx
 
+createVariable :: String -> String -> LigoType -> Maybe String -> Maybe String -> Variable
+createVariable name varText typ menuContext evaluateName = DAP.defaultVariable
+  { DAP.nameVariable = name
+  , DAP.valueVariable = varText
+  , DAP.typeVariable = pretty typ
+  , DAP.__vscodeVariableMenuContextVariable = menuContext
+  , DAP.evaluateNameVariable = evaluateName
+  }
+
 buildVariable :: forall t. LigoType -> Value t -> String -> VariableBuilder Variable
 buildVariable typ v name = do
   let
     varText = pretty $ debugBuild DpmNormal v
     evaluatedText = pretty $ debugBuild DpmEvaluated v
-    var = DAP.defaultVariable
-      { DAP.nameVariable = name
-      , DAP.valueVariable = varText
-      , DAP.typeVariable = pretty typ
-      , DAP.__vscodeVariableMenuContextVariable = case v of
-          VAddress  {} -> Just "address"
-          VContract {} -> Just "contract"
-          _            -> Nothing
-      , DAP.evaluateNameVariable = Just evaluatedText
-      }
+    menuContext = case v of
+      VAddress  {} -> Just "address"
+      VContract {} -> Just "contract"
+      _            -> Nothing
+    var = createVariable name varText typ menuContext (Just evaluatedText)
 
   subVars <- buildSubVars typ v
 
@@ -109,5 +114,9 @@ buildSubVars typ = \case
     forM (toPairs m) \(k, v) -> do
       let name = pretty $ debugBuild DpmNormal k
       buildVariable (getInnerTypeFromRecord name typ) v (T.unpack name)
+  VContract address entrypoint -> do
+    let addr = createVariable "address" (pretty address) LTUnresolved Nothing Nothing
+    let ep = createVariable "entrypoint" (pretty entrypoint) LTUnresolved Nothing Nothing
+    pure [addr, ep]
   -- Other value types do not have nested structure
   _ -> return []
