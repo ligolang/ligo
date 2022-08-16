93888790d86e726bc5e8b89281fb31c42a1fefed
Laurent Canis <laurent@marigold.dev>
2022-08-16 13:30:17 -0400

    navbar and footer + fix static pages

--- gitlab-pages/website/docusaurus.config.js
+++ gitlab-pages/website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'LIGO', 
+  title: 'LIGO',
   tagline: 'LIGO is a friendly smart contract language for Tezos',
   url: 'https://ligolang.org',
   baseUrl: '/',
@@ -30,6 +30,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          path: '../docs',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -40,12 +41,12 @@ const config = {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
-        },
+        }
       }),
     ],
   ],
@@ -54,24 +55,26 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'LIGO Logo',
+          src: 'img/logo/logo.svg',
+          srcDark: 'img/logo/logo-night.svg'
         },
         items: [
+          { type: 'docsVersionDropdown', position: 'left' },
+          { href: 'https://ide.ligolang.org/', label: 'Try Online', position: 'left', target: '_self' },
+          { to: 'docs/intro/installation', label: 'Install', position: 'left' },
+          { to: 'docs/intro/introduction', label: 'Docs', position: 'left' },
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            to: 'docs/tutorials/getting-started',
+            label: 'Tutorials',
+            position: 'left'
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          { href: 'https://academy.ligolang.org/', label: 'Academy', position: 'left', target: '_self' },
+          { href: 'https://forum.tezosagora.org/tag/ligo', label: 'Blog', position: 'left' },
+          { to: '/contact', label: 'Ask Questions', position: 'left' },
+          { to: 'docs/faq/intro', label: 'FAQ', position: 'left' },
+          { to: 'docs/intro/changelog', label: 'Changelog', position: 'left' }
         ],
       },
       footer: {
@@ -81,25 +84,41 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: 'Install',
+                to: 'docs/intro/installation',
               },
+              {
+                label: 'CLI Commands',
+                to: 'docs/api/cli-commands',
+              },
+              {
+                label: 'Cheat Sheet',
+                to: 'docs/api/cheat-sheet',
+              }
             ],
           },
           {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: 'Tezos Stack Exchange',
+                href: 'https://tezos.stackexchange.com/questions/tagged/ligo',
               },
               {
                 label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                href: 'https://discord.gg/9rhYaEt',
+              },
+              {
+                label: 'Telegram',
+                href: 'https://t.me/LigoLang',
+              },
+              {
+                label: 'Riot',
+                href: 'https://riot.im/app/#/room/#ligo-public:matrix.org',
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/LigoLang',
               },
             ],
           },
@@ -107,23 +126,56 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'Tutorials',
+                to: 'docs/tutorials/getting-started',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Blog',
+                to: 'https://forum.tezosagora.org/tag/ligo',
+              },
+              {
+                label: 'GitLab',
+                href: 'https://gitlab.com/ligolang/ligo',
+              },
+              {
+                label: 'Contribute',
+                to: 'docs/contributors/origin',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} LIGO. All rights reserved.`,
       },
+      image: 'img/logo/logo.png',
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: 'BH4D9OD16A',
+
+        // Public API key: it is safe to commit it
+        apiKey: 'b773aa611ab70b8e44a657eae5539176',
+
+        indexName: 'ligolang',
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+
+      },
     }),
+  plugins: [
+    [
+      './src/plugins/syntax', {},
+    ]
+  ]
 };
 
 module.exports = config;
