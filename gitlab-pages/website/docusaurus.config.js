36818e26b345ef5957cd83ea5dbcf629fe7c14d8
Melwyn Saldanha <melwyn95@gmail.com>
2022-08-18 11:29:13 +0530

    Fix blockquote padding & editUrl

--- gitlab-pages/website/docusaurus.config.js
+++ gitlab-pages/website/docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://gitlab.com/ligolang/ligo/-/tree/dev/gitlab-pages/website',
         },
         blog: {
           showReadingTime: true,
