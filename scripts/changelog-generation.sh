0493ce94d2400432ea276eda22d4ca7215310c16
Laurent Canis <laurent@marigold.dev>
2022-08-18 09:32:04 -0400

    remove nix expression to generate changelog

---
+++ scripts/changelog-generation.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -p mustache-go jq  -i bash
+
+# shellcheck shell=bash
+
+cd "$(dirname "${BASH_SOURCE[0]}"/..)"
+
+./scripts/changelog-json.sh > changelog.json
+
+mustache ./changelog.json ./scripts/changelog.md.mustache > changelog.md
+mustache ./changelog.json ./scripts/changelog.txt.mustache > changelog.txt
+
+jq '.changelog[0]' changelog.json > release-notes.json
+
+mustache ./changelog.json ./scripts/release-notes.md.mustache > release-notes.md
+mustache ./changelog.json ./scripts/release-notes.txt.mustache > release-notes.txt
