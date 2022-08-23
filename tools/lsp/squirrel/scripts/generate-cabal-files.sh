f7980853cf7b4944ee15cc51dac06bd17aa20ae0
stepan.prudnikov@serokell.io <prustephan98@gmail.com>
2022-08-23 14:18:19 +0200

    [LIGO-712] Add validation step

--- tools/lsp/squirrel/scripts/generate-cabal-files.sh
+++ tools/lsp/squirrel/scripts/generate-cabal-files.sh
@@ -15,5 +15,6 @@ set -e
 mkdir -p stack2cabal
 stack install stack2cabal --resolver snapshot-stack2cabal.yaml --local-bin-path stack2cabal
 ./stack2cabal/stack2cabal
+
 rm -rf stack2cabal
 echo "cabal.project, cabal.project.freeze and *.cabal files are successfully generated"
