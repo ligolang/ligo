abc936789c9d62667b3949272e278dc243bf9f36
Preetham Gujjula <gitcommit@mail.preetham.io>
2022-08-24 01:25:44 -0700

    [LIGO-625] Implement multi-file compilation
    Problem: We can only compile single-file projects.
    
    Solution: Change the API to allow compiling multi-file projects. Also
    add a test for multi-file compilation. Also add API documentation using
    Swagger.

--- tools/webide-new/ligo-webide-backend/src/Lib.hs
+++ tools/webide-new/ligo-webide-backend/src/Lib.hs
@@ -3,44 +3,56 @@ module Lib
   , mkApp
   , CompileRequest (..)
   , Config (..)
+  , Source (..)
   )
 where
 
+import Control.Monad (forM_)
 import Control.Monad.Except (ExceptT)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ReaderT, asks, runReaderT)
 import Control.Monad.Trans (lift)
 import Data.Aeson
-  (FromJSON, Result(..), ToJSON, Value, defaultOptions, fieldLabelModifier, fromJSON,
-  genericParseJSON, genericToJSON, parseJSON, toJSON)
-import Data.ByteString.Lazy.Char8 qualified as BSL
+  (FromJSON, ToJSON, defaultOptions, fieldLabelModifier, genericParseJSON, genericToJSON, parseJSON,
+  toJSON)
 import Data.Char (toLower)
+import Data.Proxy (Proxy(Proxy))
+import Data.Swagger.ParamSchema (ToParamSchema)
+import Data.Swagger.Schema (ToSchema)
 import Data.Text (Text)
 import Data.Text qualified as Text
 import Data.Text.IO qualified as Text
-import GHC.Generics
+import GHC.Generics (Generic)
 import Katip (Environment(..), KatipT, initLogEnv, runKatipT)
 import Network.Wai (Middleware)
 import Network.Wai.Handler.Warp (run)
 import Network.Wai.Middleware.Cors (cors, corsRequestHeaders, simpleCorsResourcePolicy)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Servant
+  (Application, Handler(..), JSON, Post, ReqBody, Server, ServerError, hoistServer, serve,
+  (:<|>)((:<|>)), (:>))
+import Servant.Swagger (toSwagger)
+import Servant.Swagger.UI (SwaggerSchemaUI, swaggerSchemaUIServer)
+import System.Directory (createDirectoryIfMissing)
 import System.Exit (ExitCode(ExitFailure, ExitSuccess))
-import System.IO (hClose)
-import System.IO.Temp
-import System.Process
+import System.FilePath (takeDirectory, (</>))
+import System.IO.Temp (withSystemTempDirectory)
+import System.Process (readProcessWithExitCode)
 
-type API = "compile" :> ReqBody '[JSON] Value :> Post '[JSON] Text
+newtype Source = Source {unSource :: Text}
+  deriving stock (Eq, Show, Ord, Generic)
 
-data Config = Config
-  { cLigoPath :: FilePath
-  , cPort :: Int
-  , cVerbose :: Bool
-  }
+instance FromJSON Source where
+  parseJSON = genericParseJSON defaultOptions
+
+instance ToJSON Source where
+  toJSON = genericToJSON defaultOptions
+
+instance ToSchema Source
 
 data CompileRequest = CompileRequest
-  { rFileExtension :: Text
-  , rSource :: Text
+  { rSources :: [(FilePath, Source)]
+  , rMain :: FilePath
   } deriving stock (Eq, Show, Ord, Generic)
 
 prepareField :: Int -> String -> String
@@ -57,6 +69,29 @@ instance FromJSON CompileRequest where
 instance ToJSON CompileRequest where
   toJSON = genericToJSON defaultOptions {fieldLabelModifier = prepareField 1}
 
+instance ToSchema CompileRequest
+
+newtype Build = Build Text deriving stock (Show, Generic)
+
+instance ToJSON Build where
+  toJSON = genericToJSON defaultOptions
+
+instance ToSchema Build
+
+instance ToParamSchema Build
+
+type API = "compile" :> ReqBody '[JSON] CompileRequest :> Post '[JSON] Build
+
+type SwaggeredAPI =
+  SwaggerSchemaUI "swagger-ui" "swagger.json"
+    :<|> API
+
+data Config = Config
+  { cLigoPath :: FilePath
+  , cPort :: Int
+  , cVerbose :: Bool
+  }
+
 type WebIDEM = KatipT (ReaderT Config (ExceptT ServerError IO))
 
 startApp :: Config -> IO ()
@@ -64,7 +99,7 @@ startApp config = run (cPort config) (mkApp config)
 
 mkApp :: Config -> Application
 mkApp config =
-  maybeLogRequests . corsWithContentType $ serve (Proxy @API) server
+  maybeLogRequests . corsWithContentType $ serve (Proxy @SwaggeredAPI) server
   where
     maybeLogRequests :: Middleware
     maybeLogRequests =
@@ -79,27 +114,34 @@ mkApp config =
         policy = simpleCorsResourcePolicy
           {corsRequestHeaders = ["Content-Type"]}
 
-    server :: ServerT API Handler
-    server = hoistServer (Proxy @API) hoist compile
+    server :: Server SwaggeredAPI
+    server =
+      swaggerSchemaUIServer (toSwagger (Proxy @API))
+        :<|> hoistServer (Proxy @API) hoist compile
 
     hoist :: WebIDEM a -> Handler a
     hoist x = Handler $ do
       logEnv <- liftIO $ initLogEnv "ligo-webide" (Environment "devel")
       runReaderT (runKatipT logEnv x) config
 
-compile :: Value -> WebIDEM Text
-compile input =
-  case fromJSON input of
-    Error err -> lift $ throwError $
-      err400 {errBody = BSL.pack $ "malformed request body: " ++ err}
-    Success request ->
-      let filename = "input." ++ Text.unpack (rFileExtension request)
-       in withSystemTempFile filename $ \fp handle -> do
-            liftIO $ hClose handle
-            liftIO $ Text.writeFile fp (rSource request)
-            ligoPath <- lift (asks cLigoPath)
-            (ec, out, err) <- liftIO $
-              readProcessWithExitCode ligoPath ["compile", "contract", fp] ""
-            case ec of
-              ExitSuccess -> pure (Text.pack out)
-              ExitFailure _ -> pure (Text.pack err)
+compile :: CompileRequest -> WebIDEM Build
+compile request =
+  let (filepaths, sources) = unzip (rSources request)
+   in withSystemTempDirectory "" $ \dirPath -> do
+        let fullFilepaths = map (dirPath </>) filepaths
+        let fullMainPath = dirPath </> rMain request
+
+        liftIO . forM_ (zip fullFilepaths sources) $ \(fp, src) -> do
+          createDirectoryIfMissing True (takeDirectory fp)
+          Text.writeFile fp (unSource src)
+
+        ligoPath <- lift (asks cLigoPath)
+        (ec, out, err) <- liftIO $
+          readProcessWithExitCode
+            ligoPath
+            ["compile", "contract", fullMainPath]
+            ""
+
+        case ec of
+          ExitSuccess -> pure (Build $ Text.pack out)
+          ExitFailure _ -> pure (Build $ Text.pack err)
