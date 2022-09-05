6a2c5138205a0aef545c48ebbf615d71ad671835
Melwyn Saldanha <melwyn95@gmail.com>
2022-09-05 22:25:22 +0530

    wip: handle M_struct

--- src/main/scopes/new_scopes.ml
+++ src/main/scopes/new_scopes.ml
@@ -27,35 +27,35 @@ module Free = struct
           let references = loc :: v.references in
           true, Variable { v with references } :: defs
         | def::defs ->
-          let updated, defs = update_variable_reference ev defs in  
+          let updated, defs = update_variable_reference ev defs in
           updated, def :: defs
 
   let rec update_module_variable_references : AST.module_variable list -> AST.expression_variable option -> def list -> bool * def list
     = fun mvs ev defs ->
         match mvs, defs with
           _, [] -> false, defs
-        | [], defs -> 
+        | [], defs ->
           begin match ev with
             Some ev -> update_variable_reference ev defs
           | None -> true, defs
           end
-        (* | [mv], Module m::defs when MVar.is_name mv m.name -> 
+        (* | [mv], Module m::defs when MVar.is_name mv m.name ->
             let loc = MVar.get_location mv in
             let references = loc :: m.references in
             Module { m with references } :: defs *)
-        | mv::mvs, Module ({ mod_case = Def d ; _ } as m)::defs when MVar.is_name mv m.name -> 
+        | mv::mvs, Module ({ mod_case = Def d ; _ } as m)::defs when MVar.is_name mv m.name ->
             let loc = MVar.get_location mv in
             let references = loc :: m.references in
             let updated, d  = update_module_variable_references mvs ev d in
             let mod_case = Def d in
             updated, Module { m with mod_case ; references } :: defs
-        | mv::mvs, Module ({ mod_case = Alias a ; _ } as m)::defs when MVar.is_name mv m.name -> 
+        | mv::mvs, Module ({ mod_case = Alias a ; _ } as m)::defs when MVar.is_name mv m.name ->
             let loc = MVar.get_location mv in
             let references = loc :: m.references in
             let updated, defs = resolve_alias a mvs ev defs in
             updated, Module { m with references } :: defs
         | mvs, def::defs ->
-            let updated, defs = update_module_variable_references mvs ev defs in 
+            let updated, defs = update_module_variable_references mvs ev defs in
             updated, def :: defs
     and resolve_alias : string list -> AST.module_variable list -> AST.expression_variable option -> def list -> bool * def list
       = fun aliases mvs ev defs ->
@@ -68,10 +68,10 @@ module Free = struct
                   let updated, d = resolve_alias aliases mvs ev d in
                   let mod_case = Def d in
                   updated, Module { m with mod_case } :: defs
-              | Module ({ name ; mod_case = Alias a ; _ }) as def::defs when String.(name = alias) -> 
+              | Module ({ name ; mod_case = Alias a ; _ }) as def::defs when String.(name = alias) ->
                 let updated, defs = resolve_alias (a @ aliases) mvs ev defs in
                 updated, def :: defs
-              | def::defs -> 
+              | def::defs ->
                   let updated, defs = aux defs in
                   updated, def :: defs
               in
@@ -102,7 +102,7 @@ module Free = struct
         | E_variable ev -> [], [Variable ev]
         | E_module_accessor m -> [], [ModuleAccess (m.module_path, m.element)]
         | E_constant { arguments ; _ } ->
-          List.fold_left arguments ~init:([], []) 
+          List.fold_left arguments ~init:([], [])
             ~f:(fun (defs, refs) e ->
                   let d, r = expression e in
                   defs @ d, refs @ r)
@@ -113,7 +113,7 @@ module Free = struct
         | E_lambda { binder ; result ; output_type = _ } ->
           (* TODO: handle input_type *)
           (* TODO: handle output_type *)
-          let def = 
+          let def =
             let binder_name = get_binder_name binder.var in
             let binder_loc =  VVar.get_location binder.var in
             make_v_def binder_name Unresolved binder_loc (result.location)
@@ -125,13 +125,13 @@ module Free = struct
         | E_type_abstraction { result ; _ } -> expression result
         | E_constructor { element ; _ } -> expression element
         | E_record_accessor { record ; _ } -> expression record
-        | E_ascription { anno_expr ; _ } -> expression anno_expr 
-        | E_record_update { record ; update ; _ } -> 
+        | E_ascription { anno_expr ; _ } -> expression anno_expr
+        | E_record_update { record ; update ; _ } ->
           let defs, refs =  expression record in
           let defs', refs' = expression update in
           defs @ defs', refs @ refs'
         | E_record e_lable_map ->
-          let defs, refs = AST.LMap.fold (fun _ e (defs, refs) -> 
+          let defs, refs = AST.LMap.fold (fun _ e (defs, refs) ->
             let defs', refs' = expression e in
             defs @ defs', refs @ refs'
           ) e_lable_map ([], []) in
@@ -141,7 +141,7 @@ module Free = struct
           let defs, refs = expression e in
           defs, refs @ refs'
         | E_let_in { let_binder ; rhs ; let_result ; _ } ->
-          let def = 
+          let def =
             let binder_name = get_binder_name let_binder.var in
             let binder_loc =  VVar.get_location let_binder.var in
             make_v_def binder_name Unresolved binder_loc (rhs.location)
@@ -167,11 +167,7 @@ module Free = struct
           let defs, refs_result = update_references refs_result defs in
           defs @ defs_result, refs_result
         | E_type_in { type_binder ; rhs ; let_result } ->
-          let def =
-            let binder_name = get_type_binder_name type_binder in
-            let binder_loc =  TVar.get_location type_binder in
-            make_t_def binder_name binder_loc rhs
-          in
+          let def = type_expression type_binder rhs in
           let defs, refs = expression let_result in
           [def] @ defs, refs
         | E_matching { matchee ; cases } ->
@@ -182,7 +178,7 @@ module Free = struct
                 fun defs (p : _ AST.pattern) ->
                   match p.wrap_content with
                     P_var binder ->
-                      let def = 
+                      let def =
                         let binder_name = get_binder_name binder.var in
                         let binder_loc =  VVar.get_location binder.var in
                         make_v_def binder_name Unresolved binder_loc (body.location)
@@ -197,12 +193,31 @@ module Free = struct
           in
           defs_matchee @ defs_cases, refs_matchee @ refs_cases
         | E_mod_in { module_binder ; rhs ; let_result } -> [], []
+    and type_expression : TVar.t -> AST.type_expression -> def
+      = fun tv t ->
+          let def =
+            let binder_name = get_type_binder_name tv in
+            let binder_loc =  TVar.get_location tv in
+            make_t_def binder_name binder_loc t
+          in
+          def
     and module_expression : MVar.t -> AST.module_expr -> def list * reference list
       = fun top m ->
           match m.wrap_content with
-            M_struct _ -> [], []
-          | M_variable mv -> 
-            let def, reference = 
+            M_struct decls ->
+              let defs, refs = List.fold_left decls ~init:([], [])
+                ~f:(fun (defs, refs) decl ->
+                  let defs', refs' = declaration decl in
+                  let defs, refs' = update_references refs' defs in
+                  defs @ defs', refs @ refs')
+              in
+              let range = MVar.get_location top in
+              let body_range = m.location in
+              let name = get_mod_binder_name top in
+              let def = make_m_def ~range ~body_range name defs in
+              [def] @ defs, refs
+          | M_variable mv ->
+            let def, reference =
               let name = get_mod_binder_name top in
               let range =  MVar.get_location top in
               let body_range = MVar.get_location mv in
@@ -212,9 +227,9 @@ module Free = struct
               def, reference
             in
             [def], [reference]
-          | M_module_path path -> 
+          | M_module_path path ->
             let mvs = List.Ne.to_list path in
-            let def, reference = 
+            let def, reference =
               let name = get_mod_binder_name top in
               let range =  MVar.get_location top in
               let body_range = m.location in
@@ -224,28 +239,29 @@ module Free = struct
               def, reference
             in
             [def], [reference]
+    and declaration_expression : VVar.t -> AST.expression -> def list * reference list
+      = fun ev e ->
+          let defs, refs = expression e in
+          let name = get_binder_name ev in
+          let range = VVar.get_location ev in
+          let body_range = e.location in
+          let def = make_v_def name Unresolved range body_range in
+          [def] @ defs, refs
+    and declaration : AST.declaration -> def list * reference list
+      = fun decl ->
+        match decl.wrap_content with
+          Declaration_constant { attr        = { hidden ; _ } ; _ }
+        | Declaration_module   { module_attr = { hidden ; _ } ; _ }
+        | Declaration_type     { type_attr   = { hidden ; _ } ; _ } when hidden -> [], []
+        | Declaration_constant { binder      = { var ; ascr=_ ; _ } ; expr ; _ } ->
+          (* TODO: use ascr *)
+          declaration_expression var expr
+        | Declaration_type     { type_binder ; type_expr ; _ } ->
+          let def = type_expression type_binder type_expr in
+          [def], []
+        | Declaration_module   { module_binder ; module_ ; _ } ->
+          module_expression module_binder module_
 end
-
-let declaration : AST.declaration -> def list * reference list
-  = fun decl ->
-      match decl.wrap_content with
-        Declaration_constant { attr        = { hidden ; _ } ; _ }
-      | Declaration_module   { module_attr = { hidden ; _ } ; _ }
-      | Declaration_type     { type_attr   = { hidden ; _ } ; _ } when hidden -> [], []
-      | Declaration_constant { binder      = { var ; ascr=_ ; _ } ; expr ; _ } ->
-        (* add def for var *)
-        (* calculate function for expr -> def list & reference list *)
-        [], []
-      | Declaration_type     { type_binder ; type_expr ; _ } ->
-        (* add def for type_binder *)
-        (* calculate function for type_expr -> [] & reference list (free type variables) *)
-        [], []
-      | Declaration_module   { module_binder ; module_ ; _ } ->
-        (* add def for module_binder *)
-        (* calculate function for module_ -> def list & free references *)
-        (* should return [Module {def list}], free references *)
-        [], []
-
 
 let scopes : with_types:bool -> options:Compiler_options.middle_end -> AST.module_ -> (def list * scopes)
   = fun ~with_types ~options prg ->
