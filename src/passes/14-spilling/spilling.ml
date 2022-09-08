a09ddf0d8e2d1d21da62e9d35a2bcf25d00e16ed
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-09-08 17:27:33 +0000

    Testing framework: reuse layout from spilling pass

--- src/passes/14-spilling/spilling.ml
+++ src/passes/14-spilling/spilling.ml
@@ -4,6 +4,7 @@ module Append_tree = Simple_utils.Tree.Append
 module Compiler   = Compiler
 module Decompiler = Decompiler
 module Errors = Errors
+module Layout = Layout
 
 let compile_expression = Compiler.compile_program
 let compile_type       = Compiler.compile_type
