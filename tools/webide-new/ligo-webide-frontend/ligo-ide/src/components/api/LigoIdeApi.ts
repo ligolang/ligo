bb2bfe6685025e520dc2a0a6f7879849b1882270
sashasashasasha151 <sashasashasasha151@gmail.com>
2022-09-09 22:24:34 +0400

    [LIGO-662] Frontend protocol flag
    Problem: tezos sometimes change protocols which affect ligo
    
    Solution: add global option for protocol selection and save
    it to the redux. Use this field implicitly in api calls

--- tools/webide-new/ligo-webide-frontend/ligo-ide/src/components/api/LigoIdeApi.ts
+++ tools/webide-new/ligo-webide-frontend/ligo-ide/src/components/api/LigoIdeApi.ts
@@ -1,9 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { AxiosInstance, AxiosResponse } from "axios";
+import redux from "~/base-components/redux";
 
-export interface CompileContractArgsApi {
-  sources: [string, { unSource: string }][];
+export type ProtocolArg = {
+  protocol?: string;
+};
+
+export type CompileContractArgsApi = ProtocolArg & {
+  sources: [string, string][];
   main: string;
-}
+};
 
 interface LigoIdeApiInterface {
   compileContract(args: CompileContractArgsApi): Promise<string>;
@@ -11,7 +17,11 @@ interface LigoIdeApiInterface {
 
 export default function LigoIdeApi(axiosInst: AxiosInstance): LigoIdeApiInterface {
   return {
-    compileContract: async (args: CompileContractArgsApi) => {
+    compileContract: async (argument: CompileContractArgsApi) => {
+      const args = argument;
+      const { protocol } = redux.getState();
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      args.protocol = protocol.name;
       return axiosInst.post("/compile", args).then((resp: AxiosResponse<string>) => resp.data);
     },
   };
---
+++ tools/webide-new/ligo-webide-frontend/ligo-ide/src/ligo-components/eth-compiler/bottombar/ProtocolSelector.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { useSelector, useDispatch } from "react-redux";
+import {
+  UncontrolledButtonDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "~/base-components/ui-components";
+
+function ProtocolSelector() {
+  // @ts-ignore
+  const protocol: { showName: string; name: string } = useSelector((state) => state.protocol);
+
+  const dispatch = useDispatch();
+
+  const protocols = [
+    { showName: "Jakarta (mainnet)", name: "jakarta" },
+    { showName: "Kathmandu (testnet)", name: "kathmandu" },
+  ];
+
+  const menuHeader = (
+    <>
+      <i className="fas fa-clipboard-list mr-1" />
+      Protocols
+    </>
+  );
+  return (
+    <UncontrolledButtonDropdown direction="up">
+      <DropdownToggle
+        size="sm"
+        color="default"
+        className="rounded-0 text-muted px-2 text-nowrap overflow-hidden text-overflow-dots"
+        style={{ maxWidth: 300 }}
+      >
+        <span key="icon" className="mr-1">
+          <i className="fas fa-clipboard-list" />
+        </span>
+        {`Protocol ${protocol.showName}`}
+      </DropdownToggle>
+      <DropdownMenu right className="dropdown-menu-sm">
+        <DropdownItem header>{menuHeader}</DropdownItem>
+        {protocols.map((pr) => (
+          <DropdownItem
+            key={`image-version-${pr.name}`}
+            active={protocol.name === pr.name}
+            onClick={() => dispatch({ type: "SET_PROTOCOL", payload: pr })}
+          >
+            {pr.showName}
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </UncontrolledButtonDropdown>
+  );
+}
+
+export default ProtocolSelector;
