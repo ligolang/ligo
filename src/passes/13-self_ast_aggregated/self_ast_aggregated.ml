422f9b0efc1eba3d7bf4b1e91ec23348851b4685
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2022-09-14 11:55:55 -0300

    refactor: destruct tuple version in separate case

--- src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
+++ src/passes/13-self_ast_aggregated/self_ast_aggregated.ml
@@ -15,17 +15,72 @@ let eta_reduce : Ast_aggregated.expression -> Ast_aggregated.expression option =
      end
   | _ -> None
 
-let rec get_abstractions acc e =
+let get_abstraction_tuple e =
   let open Ast_aggregated in
   match e.expression_content with
   | E_lambda { binder ; result ; _ } ->
-     get_abstractions (acc @ [Ligo_prim.Binder.get_var binder]) result
-  | E_matching { matchee = { expression_content = E_variable u ; _ } ; cases = Match_record { fields ; body ; _ } }
-       when List.equal Ligo_prim.Value_var.equal [u] acc && Ligo_prim.Record.is_tuple fields ->
-     let acc = Ligo_prim.Record.tuple_of_record fields in
+    let var = Ligo_prim.Binder.get_var binder in
+    begin match result.expression_content with
+    | E_matching { matchee = { expression_content = E_variable u ; _ } ; cases = Match_record { fields ; body ; _ } }
+      when Ligo_prim.Value_var.equal u var && Ligo_prim.Record.is_tuple fields ->
+      let acc = Ligo_prim.Record.tuple_of_record fields in
+      let acc = List.map ~f:snd acc in
+      let acc = List.map ~f:(Ligo_prim.Binder.get_var) acc in
+      Some (acc, body)
+    | _ -> None end
+  | _ -> None
+
+let get_application_tuple e =
+  let open Ast_aggregated in
+  match e.expression_content with
+  | E_application { lamb ; args = { expression_content = E_record p ; _ } } ->
+     let acc = Ligo_prim.Record.tuple_of_record p in
      let acc = List.map ~f:snd acc in
-     let acc = List.map ~f:(Ligo_prim.Binder.get_var) acc in
-     get_abstractions acc body
+     Some (acc, lamb)
+  | _ -> None
+
+(* This function destructs an expression of the form:
+     (fun (x1, ..., xn) -> EXPR)(A1, ..., An) *)
+let get_app_and_abs_tuple e =
+  let open Option in
+  let* arguments, lamb = get_application_tuple e in
+  let* vars, expr = get_abstraction_tuple lamb in
+  Some (arguments, vars, expr)
+
+let rec get_abstractions acc e =
+  let open Ast_aggregated in
+  match acc, e.expression_content with
+  | _, E_lambda { binder ; result ; _ } ->
+    get_abstractions (acc @ [Ligo_prim.Binder.get_var binder]) result
+  | [], _ -> None
+  | _, _ -> Some (acc, e)
+
+let rec get_applications acc e =
+  let open Ast_aggregated in
+  match acc, e.expression_content with
+  | _, E_application { lamb ; args } ->
+     get_applications (args :: acc) lamb
+  | [], _ -> None
+  | _, _ -> Some (acc, e)
+
+(* This function destructs an expression of the form:
+     (fun x1 -> ... -> fun xn -> EXPR) A1 ... An *)
+let get_apps_and_abs e =
+  let open Option in
+  let* arguments, lamb = get_applications [] e in
+  let* vars, expr = get_abstractions [] lamb in
+  Some (arguments, vars, expr)
+
+let get_abstract_and_app e =
+  match get_app_and_abs_tuple e with
+  | Some v -> Some v
+  | None -> get_apps_and_abs e
+
+(* Given a list [x1;...;xn] of vars, and an expression e, it checks
+   whether e = CONST(x1, ..., xn) *)
+let get_const acc e =
+  let open Ast_aggregated in
+  match e.expression_content with
   | E_constant { cons_name ; arguments = tuple ; _ } ->
      let rec f b e v =
        match e.expression_content with
@@ -42,17 +97,6 @@ let rec get_abstractions acc e =
      end
   | _ -> None
 
-let rec get_applications ?(first = true) acc e =
-  let open Ast_aggregated in
-  match e.expression_content with
-  | E_application { lamb ; args = { expression_content = E_record p ; _ } } when first ->
-     let acc = Ligo_prim.Record.tuple_of_record p in
-     let acc = List.map ~f:snd acc in
-     get_applications acc lamb
-  | E_application { lamb ; args } ->
-     get_applications ~first:false (args :: acc) lamb
-  | _ -> acc, e
-
 let inline_let : bool ref -> Ast_aggregated.expression -> Ast_aggregated.expression =
   fun changed e ->
   match e.expression_content with
@@ -61,15 +105,19 @@ let inline_let : bool ref -> Ast_aggregated.expression -> Ast_aggregated.express
      (changed := true ; e2')
   | E_application _ ->
     (* This case tries to reduce
-         `(fun x1 -> ... -> fun xn -> CONST(x1, ..., xn)) A1 ... An` to `CONST(A1, ... AN)`
+         `(fun x1 -> ... -> fun xn -> CONST(x1, ..., xn)) A1 ... An` to `CONST(A1, ... AN)` or
+         `(fun (x1, ..., xn) -> CONST(x1, ..., xn))(A1, ..., An)` to `CONST(A1, ... AN)`
        It is needed to handle special constants in the stdlib
      *)
      begin
-       let arguments, expr = get_applications [] e in
-       match get_abstractions [] expr with
-       | Some (cons_name, len) when len = List.length arguments ->
-          (changed := true; { e with expression_content = E_constant { cons_name ; arguments } })
-       | Some _ | None -> e
+       match get_abstract_and_app e with
+       | Some (arguments, vars, const) ->
+         begin match get_const vars const with
+           | Some (cons_name, len) when len = List.length arguments ->
+             (changed := true; { e with expression_content = E_constant { cons_name ; arguments } })
+           | Some _ | None -> e
+         end
+       | None -> e
      end
   | _ -> e
 
