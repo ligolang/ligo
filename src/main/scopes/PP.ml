2de65e031e75438b180a4757c2c3c588d63580d0
Melwyn Saldanha <melwyn95@gmail.com>
2022-09-21 10:22:47 +0530

    Format: apply ocamlformat on `src/main/scopes`

--- src/main/scopes/PP.ml
+++ src/main/scopes/PP.ml
@@ -2,125 +2,220 @@ module PP_helpers = Simple_utils.PP_helpers
 open Types
 
 let scopes : Format.formatter -> scopes -> unit =
-  fun f s ->
-    let s = List.sort s ~compare:(fun (l1,_) (l2,_) -> Location.compare l1 l2) in
-    let pp_scope f (scope : scope) =
-      let loc, defs = scope in
-      let pp_bindings f defs = List.iter defs ~f:(fun def -> Format.fprintf f "%s " (get_def_uid def)) in
-      Format.fprintf f "[ %a ] %a" pp_bindings defs Location.pp loc
+ fun f s ->
+  let s =
+    List.sort s ~compare:(fun (l1, _) (l2, _) -> Location.compare l1 l2)
+  in
+  let pp_scope f (scope : scope) =
+    let loc, defs = scope in
+    let pp_bindings f defs =
+      List.iter defs ~f:(fun def -> Format.fprintf f "%s " (get_def_uid def))
     in
-    let pp_scopes f = List.iter ~f: (Format.fprintf f "@[<v>%a@ @]" pp_scope) in
-    Format.fprintf f "@[<v>Scopes:@ %a@]" pp_scopes s
+    Format.fprintf f "[ %a ] %a" pp_bindings defs Location.pp loc
+  in
+  let pp_scopes f = List.iter ~f:(Format.fprintf f "@[<v>%a@ @]" pp_scope) in
+  Format.fprintf f "@[<v>Scopes:@ %a@]" pp_scopes s
 
-let rec definitions : Format.formatter -> def list -> unit
-  = fun f defs ->
-    let defs = List.sort defs ~compare:(fun d1 d2 -> String.compare (get_def_uid d1) (get_def_uid d2)) in
-    let refs ppf locs =
-      let locs = LSet.elements locs in
-      match locs with
-        [] -> Format.fprintf ppf "references: []"
-      | locs ->
-        let locs = List.sort locs ~compare:Location.compare in
-        Format.fprintf ppf "@[<hv 2>references:@ %a@]" PP_helpers.(list_sep Location.pp (tag " ,@ ")) locs
-    in
-    let pp_content ppf =
-      function
-        Variable v -> (
-          let typ ppf t = match t with
-            | Core t -> Format.fprintf ppf "core: %a" Ast_core.PP.type_expression t
-            | Resolved t -> Format.fprintf ppf "resolved: %a" Ast_typed.PP.type_expression t
-            | Unresolved -> Format.fprintf ppf "unresolved"
-          in
-          Format.fprintf ppf "|%a|@ %a" typ v.t refs v.references
-        )
-      | Type t -> Format.fprintf ppf ": %a" Ast_core.PP.type_expression t.content
-      | Module { mod_case = Alias a ; references ; _ } ->
-        Format.fprintf ppf "Alias: %s @ %a @ " (String.concat ~sep:"." a) refs references
-      | Module { mod_case = Def d ; references ; _ } ->
-        Format.fprintf ppf "Members: %a @ %a @ " definitions d refs references
-    in
-    let variables, types_modules = List.partition_tf ~f:(function Type _ | Module _ -> false | Variable _ -> true) defs in
-    let types, modules = List.partition_tf ~f:(function Type _ -> true | _ -> false) types_modules in
-    let pp_def f = List.iter ~f: (fun def -> Format.fprintf f "(%s -> %s) @ Range: %a @ Body Range: %a @ Content: %a@ "
-      (get_def_uid def) (get_def_name def) Location.pp (get_range def) Location.pp (get_body_range def) pp_content def) in
-    Format.fprintf f "@[<v>Variable definitions:@ %aType definitions:@ %aModule definitions:@ %a@]" pp_def variables pp_def types pp_def modules
+
+let rec definitions : Format.formatter -> def list -> unit =
+ fun f defs ->
+  let defs =
+    List.sort defs ~compare:(fun d1 d2 ->
+        String.compare (get_def_uid d1) (get_def_uid d2))
+  in
+  let refs ppf locs =
+    let locs = LSet.elements locs in
+    match locs with
+    | [] -> Format.fprintf ppf "references: []"
+    | locs ->
+      let locs = List.sort locs ~compare:Location.compare in
+      Format.fprintf
+        ppf
+        "@[<hv 2>references:@ %a@]"
+        PP_helpers.(list_sep Location.pp (tag " ,@ "))
+        locs
+  in
+  let pp_content ppf = function
+    | Variable v ->
+      let typ ppf t =
+        match t with
+        | Core t -> Format.fprintf ppf "core: %a" Ast_core.PP.type_expression t
+        | Resolved t ->
+          Format.fprintf ppf "resolved: %a" Ast_typed.PP.type_expression t
+        | Unresolved -> Format.fprintf ppf "unresolved"
+      in
+      Format.fprintf ppf "|%a|@ %a" typ v.t refs v.references
+    | Type t -> Format.fprintf ppf ": %a" Ast_core.PP.type_expression t.content
+    | Module { mod_case = Alias a; references; _ } ->
+      Format.fprintf
+        ppf
+        "Alias: %s @ %a @ "
+        (String.concat ~sep:"." a)
+        refs
+        references
+    | Module { mod_case = Def d; references; _ } ->
+      Format.fprintf ppf "Members: %a @ %a @ " definitions d refs references
+  in
+  let variables, types_modules =
+    List.partition_tf
+      ~f:(function
+        | Type _ | Module _ -> false
+        | Variable _ -> true)
+      defs
+  in
+  let types, modules =
+    List.partition_tf
+      ~f:(function
+        | Type _ -> true
+        | _ -> false)
+      types_modules
+  in
+  let pp_def f =
+    List.iter ~f:(fun def ->
+        Format.fprintf
+          f
+          "(%s -> %s) @ Range: %a @ Body Range: %a @ Content: %a@ "
+          (get_def_uid def)
+          (get_def_name def)
+          Location.pp
+          (get_range def)
+          Location.pp
+          (get_body_range def)
+          pp_content
+          def)
+  in
+  Format.fprintf
+    f
+    "@[<v>Variable definitions:@ %aType definitions:@ %aModule definitions:@ \
+     %a@]"
+    pp_def
+    variables
+    pp_def
+    types
+    pp_def
+    modules
+
 
 let rec def_to_yojson : def -> string * Yojson.Safe.t =
-  fun def ->
-    let type_case_to_yojson =
-      function
-        Core t     -> `Assoc [ "core",       Ast_core.type_expression_to_yojson t ]
-      | Resolved t -> `Assoc [ "resolved",   Ast_typed.type_expression_to_yojson t ]
-      | Unresolved -> `Assoc [ "unresolved", `Null ]
-    in
-    let defintion ~name ~range ~body_range ~t ~references =
-      let references = LSet.elements references in
-      `Assoc [
-        ("name", `String name);
-        ("range", Location.to_yojson range);
-        ("body_range", Location.to_yojson body_range);
-        ("t", type_case_to_yojson t);
-        ("references", `List (List.map ~f:Location.to_yojson references));
+ fun def ->
+  let type_case_to_yojson = function
+    | Core t -> `Assoc [ "core", Ast_core.type_expression_to_yojson t ]
+    | Resolved t -> `Assoc [ "resolved", Ast_typed.type_expression_to_yojson t ]
+    | Unresolved -> `Assoc [ "unresolved", `Null ]
+  in
+  let defintion ~name ~range ~body_range ~t ~references =
+    let references = LSet.elements references in
+    `Assoc
+      [ "name", `String name
+      ; "range", Location.to_yojson range
+      ; "body_range", Location.to_yojson body_range
+      ; "t", type_case_to_yojson t
+      ; "references", `List (List.map ~f:Location.to_yojson references)
       ]
-    in
-    let type_definition ~name ~range ~body_range ~content =
-      `Assoc [
-        ("name", `String name);
-        ("range", Location.to_yojson range);
-        ("body_range", Location.to_yojson body_range);
-        ("content", Ast_core.type_expression_to_yojson content );
+  in
+  let type_definition ~name ~range ~body_range ~content =
+    `Assoc
+      [ "name", `String name
+      ; "range", Location.to_yojson range
+      ; "body_range", Location.to_yojson body_range
+      ; "content", Ast_core.type_expression_to_yojson content
       ]
-    in
-    let aux =
-      function
-        Variable { name ; range ; body_range ; t ; references ; uid ; def_type = _ } ->
-        uid, defintion  ~name ~range ~body_range ~t ~references
-      | Type { name ; range ; body_range ; content ; uid ; def_type = _ } ->
-        uid, type_definition ~name ~range ~body_range ~content
-      | Module { name ; range ; body_range ; mod_case = Def d ; references ; uid ; def_type = _ } ->
-        uid, `Assoc [
-          ("definition", defintion ~name ~range ~body_range ~references ~t:Unresolved);
-          ("members", defs_json d);
-        ]
-      | Module { name ; range ; body_range ; mod_case = Alias a ; references ; uid ; def_type = _ } ->
-        let alias = `List (List.map a ~f:(fun s -> `String s)) in
-        uid, `Assoc [
-          ("definition", defintion ~name ~range ~body_range ~references ~t:Unresolved);
-          ("alias", alias);
-        ]
-    in
-    aux def
+  in
+  let aux = function
+    | Variable { name; range; body_range; t; references; uid; def_type = _ } ->
+      uid, defintion ~name ~range ~body_range ~t ~references
+    | Type { name; range; body_range; content; uid; def_type = _ } ->
+      uid, type_definition ~name ~range ~body_range ~content
+    | Module
+        { name
+        ; range
+        ; body_range
+        ; mod_case = Def d
+        ; references
+        ; uid
+        ; def_type = _
+        } ->
+      ( uid
+      , `Assoc
+          [ ( "definition"
+            , defintion ~name ~range ~body_range ~references ~t:Unresolved )
+          ; "members", defs_json d
+          ] )
+    | Module
+        { name
+        ; range
+        ; body_range
+        ; mod_case = Alias a
+        ; references
+        ; uid
+        ; def_type = _
+        } ->
+      let alias = `List (List.map a ~f:(fun s -> `String s)) in
+      ( uid
+      , `Assoc
+          [ ( "definition"
+            , defintion ~name ~range ~body_range ~references ~t:Unresolved )
+          ; "alias", alias
+          ] )
+  in
+  aux def
+
+
 and defs_json (defs : def list) : Yojson.Safe.t =
   let get_defs defs =
-    let variables, types_modules = List.partition_tf defs ~f:(function Variable _ -> true | Type _ | Module _ -> false) in
-    let types, modules = List.partition_tf types_modules ~f:(function Type _ -> true | Variable _ | Module _ -> false) in
-    let modules, module_aliases = List.partition_tf modules
-      ~f:(function Module { mod_case = Def _ ; _ } -> true | Variable _ | Type _ | Module { mod_case = Alias _ ; _ } -> false) in
-    [
-      ("variables",      `Assoc (List.map ~f:(fun def -> def_to_yojson def) variables))  ;
-      ("types",          `Assoc (List.map ~f:(fun def -> def_to_yojson def) types)) ;
-      ("modules",        `Assoc (List.map ~f:(fun def -> def_to_yojson def) modules)) ;
-      ("module_aliases", `Assoc (List.map ~f:(fun def -> def_to_yojson def) module_aliases)) ;
+    let variables, types_modules =
+      List.partition_tf defs ~f:(function
+          | Variable _ -> true
+          | Type _ | Module _ -> false)
+    in
+    let types, modules =
+      List.partition_tf types_modules ~f:(function
+          | Type _ -> true
+          | Variable _ | Module _ -> false)
+    in
+    let modules, module_aliases =
+      List.partition_tf modules ~f:(function
+          | Module { mod_case = Def _; _ } -> true
+          | Variable _ | Type _ | Module { mod_case = Alias _; _ } -> false)
+    in
+    [ "variables", `Assoc (List.map ~f:(fun def -> def_to_yojson def) variables)
+    ; "types", `Assoc (List.map ~f:(fun def -> def_to_yojson def) types)
+    ; "modules", `Assoc (List.map ~f:(fun def -> def_to_yojson def) modules)
+    ; ( "module_aliases"
+      , `Assoc (List.map ~f:(fun def -> def_to_yojson def) module_aliases) )
     ]
   in
   `Assoc (get_defs defs)
 
-let scopes_json (scopes : scopes) : Yojson.Safe.t = `List (
-  List.map
-    ~f:(fun scope ->
-      let loc, defs = scope in
-      let variables, types_modules = List.partition_tf defs ~f:(function Type _ | Module _ -> false | Variable _ -> true) in
-      let types, modules = List.partition_tf types_modules ~f:(function Type _ -> true | Module _ | Variable _ -> false) in
-      let vs = List.map ~f:(fun def -> `String (get_def_uid def)) variables in
-      let ts = List.map ~f:(fun def -> `String (get_def_uid def)) types in
-      let ms = List.map ~f:(fun def -> `String (get_def_uid def)) modules in
-      (`Assoc [
-        ("range", Location.to_yojson loc) ;
-        ("expression_environment", `List vs) ;
-        ("type_environment", `List ts) ;
-        ("module_environment", `List ms) ;
-      ]))
-    scopes
-  )
 
-let to_json (d,s) = `Assoc [("definitions", (defs_json d)) ; ("scopes", (scopes_json s))]
+let scopes_json (scopes : scopes) : Yojson.Safe.t =
+  `List
+    (List.map
+       ~f:(fun scope ->
+         let loc, defs = scope in
+         let variables, types_modules =
+           List.partition_tf defs ~f:(function
+               | Type _ | Module _ -> false
+               | Variable _ -> true)
+         in
+         let types, modules =
+           List.partition_tf types_modules ~f:(function
+               | Type _ -> true
+               | Module _ | Variable _ -> false)
+         in
+         let vs =
+           List.map ~f:(fun def -> `String (get_def_uid def)) variables
+         in
+         let ts = List.map ~f:(fun def -> `String (get_def_uid def)) types in
+         let ms = List.map ~f:(fun def -> `String (get_def_uid def)) modules in
+         `Assoc
+           [ "range", Location.to_yojson loc
+           ; "expression_environment", `List vs
+           ; "type_environment", `List ts
+           ; "module_environment", `List ms
+           ])
+       scopes)
+
+
+let to_json (d, s) =
+  `Assoc [ "definitions", defs_json d; "scopes", scopes_json s ]
--- src/main/scopes/types.ml
+++ src/main/scopes/types.ml
@@ -1,185 +1,250 @@
 open Ligo_prim
 
 let generated_flag = "#?generated"
-let get_binder_name : Value_var.t -> string = fun v ->
-  if Value_var.is_generated v
-  then generated_flag
-  else Value_var.to_name_exn v
 
-let get_type_binder_name : Type_var.t -> string = fun v ->
-  if Type_var.is_generated v
-  then generated_flag
-  else Type_var.to_name_exn v
-let get_mod_binder_name : Module_var.t -> string = fun v ->
-  if Module_var.is_generated v
-  then generated_flag
-  else Module_var.to_name_exn v
+let get_binder_name : Value_var.t -> string =
+ fun v ->
+  if Value_var.is_generated v then generated_flag else Value_var.to_name_exn v
+
+
+let get_type_binder_name : Type_var.t -> string =
+ fun v ->
+  if Type_var.is_generated v then generated_flag else Type_var.to_name_exn v
+
+
+let get_mod_binder_name : Module_var.t -> string =
+ fun v ->
+  if Module_var.is_generated v then generated_flag else Module_var.to_name_exn v
+
 
 let counter = ref 0
 let reset_counter () = counter := 0
 
 let make_def_id name =
   let c, () = !counter, incr counter in
-  name ^ "#" ^ (string_of_int c)
+  name ^ "#" ^ string_of_int c
+
 
 module Location = Simple_utils.Location
-module List     = Simple_utils.List
-module LSet     = Caml.Set.Make(Location)
+module List = Simple_utils.List
+module LSet = Caml.Set.Make (Location)
 
 type type_case =
-  Core of Ast_core.type_expression
-| Resolved of Ast_typed.type_expression
-| Unresolved
+  | Core of Ast_core.type_expression
+  | Resolved of Ast_typed.type_expression
+  | Unresolved
 
-type def_type = Local | Global
+type def_type =
+  | Local
+  | Global
 
-type vdef = {
-  name  : string ;
-  uid : string ;
-  range : Location.t ;
-  body_range : Location.t ;
-  t : type_case ;
-  references : LSet.t ;
-  def_type : def_type ;
-}
+type vdef =
+  { name : string
+  ; uid : string
+  ; range : Location.t
+  ; body_range : Location.t
+  ; t : type_case
+  ; references : LSet.t
+  ; def_type : def_type
+  }
 
-type tdef = {
-  name  : string ;
-  uid : string ;
-  range : Location.t ;
-  body_range : Location.t ;
-  content : Ast_core.type_expression ;
-  def_type : def_type ;
-}
+type tdef =
+  { name : string
+  ; uid : string
+  ; range : Location.t
+  ; body_range : Location.t
+  ; content : Ast_core.type_expression
+  ; def_type : def_type
+  }
 
-type mod_case = Def of def list | Alias of string list
+type mod_case =
+  | Def of def list
+  | Alias of string list
 
-and mdef = {
-  name : string ;
-  uid: string;
-  range : Location.t ;
-  body_range : Location.t ;
-  references : LSet.t ;
-  mod_case : mod_case ;
-  def_type : def_type ;
-}
+and mdef =
+  { name : string
+  ; uid : string
+  ; range : Location.t
+  ; body_range : Location.t
+  ; references : LSet.t
+  ; mod_case : mod_case
+  ; def_type : def_type
+  }
 
-and def = Variable of vdef | Type of tdef | Module of mdef
+and def =
+  | Variable of vdef
+  | Type of tdef
+  | Module of mdef
 
 let def_compare a b =
-  match a , b with
-  | Variable x , Variable y -> String.compare x.name y.name
-  | Type x , Type y -> String.compare x.name y.name
-  | Module x , Module y -> String.compare x.name y.name
+  match a, b with
+  | Variable x, Variable y -> String.compare x.name y.name
+  | Type x, Type y -> String.compare x.name y.name
+  | Module x, Module y -> String.compare x.name y.name
   | Variable _, (Type _ | Module _) -> -1
   | (Type _ | Module _), Variable _ -> 1
   | Type _, Module _ -> 1
   | Module _, Type _ -> -1
+
+
 let def_equal a b = 0 = def_compare a b
 
 let get_def_name = function
-  | Variable    d -> d.name
-  | Type        d -> d.name
-  | Module      d -> d.name
+  | Variable d -> d.name
+  | Type d -> d.name
+  | Module d -> d.name
+
 
 let get_def_uid = function
-  | Variable    d -> d.uid
-  | Type        d -> d.uid
-  | Module      d -> d.uid
+  | Variable d -> d.uid
+  | Type d -> d.uid
+  | Module d -> d.uid
+
 
 let get_range = function
-  | Type        t -> t.range
-  | Variable    v -> v.range
-  | Module      m -> m.range
+  | Type t -> t.range
+  | Variable v -> v.range
+  | Module m -> m.range
+
 
 let get_body_range = function
-  | Type        t -> t.body_range
-  | Variable    v -> v.body_range
-  | Module      m -> m.body_range
+  | Type t -> t.body_range
+  | Variable v -> v.body_range
+  | Module m -> m.body_range
+
 
 let get_def_type = function
-  | Type        t -> t.def_type
-  | Variable    v -> v.def_type
-  | Module      m -> m.def_type
+  | Type t -> t.def_type
+  | Variable v -> v.def_type
+  | Module m -> m.def_type
 
-let make_v_def : string -> type_case -> def_type -> Location.t -> Location.t -> def =
-  fun name t def_type range body_range ->
-    let uid = make_def_id name in
-    Variable { name ; range ; body_range ; t ; uid ; references = LSet.empty ; def_type }
 
-let make_t_def : string -> def_type -> Location.t -> Ast_core.type_expression -> def =
-  fun name def_type loc te ->
-    let uid = make_def_id name in
-    Type { name ; range = loc ; body_range = te.location ; uid ; content = te ; def_type }
+let make_v_def
+    : string -> type_case -> def_type -> Location.t -> Location.t -> def
+  =
+ fun name t def_type range body_range ->
+  let uid = make_def_id name in
+  Variable
+    { name; range; body_range; t; uid; references = LSet.empty; def_type }
 
-let make_m_def : range:Location.t -> body_range:Location.t -> string -> def_type -> def list -> def =
-  fun ~range ~body_range name def_type members ->
-    let uid = make_def_id name in
-    let mod_case = Def members in
-    Module { name ; range ; body_range ; mod_case ; uid ; references = LSet.empty ; def_type }
 
-let make_m_alias_def : range:Location.t -> body_range:Location.t -> string -> def_type -> string list -> def =
-  fun ~range ~body_range name def_type alias ->
-    let uid = make_def_id name in
-    let mod_case = Alias alias in
-    Module { name ; range ; body_range ; mod_case ; uid ; references = LSet.empty ; def_type }
+let make_t_def
+    : string -> def_type -> Location.t -> Ast_core.type_expression -> def
+  =
+ fun name def_type loc te ->
+  let uid = make_def_id name in
+  Type
+    { name; range = loc; body_range = te.location; uid; content = te; def_type }
 
-let rec filter_local_defs : def list -> def list
-  = fun defs ->
-      match defs with
-        [] -> []
-      | def::defs when Caml.(get_def_type def = Local) -> filter_local_defs defs
-      | def::defs -> def :: filter_local_defs defs
 
-type scope = Location.t * (def list)
+let make_m_def
+    :  range:Location.t -> body_range:Location.t -> string -> def_type
+    -> def list -> def
+  =
+ fun ~range ~body_range name def_type members ->
+  let uid = make_def_id name in
+  let mod_case = Def members in
+  Module
+    { name
+    ; range
+    ; body_range
+    ; mod_case
+    ; uid
+    ; references = LSet.empty
+    ; def_type
+    }
+
+
+let make_m_alias_def
+    :  range:Location.t -> body_range:Location.t -> string -> def_type
+    -> string list -> def
+  =
+ fun ~range ~body_range name def_type alias ->
+  let uid = make_def_id name in
+  let mod_case = Alias alias in
+  Module
+    { name
+    ; range
+    ; body_range
+    ; mod_case
+    ; uid
+    ; references = LSet.empty
+    ; def_type
+    }
+
+
+let rec filter_local_defs : def list -> def list =
+ fun defs ->
+  match defs with
+  | [] -> []
+  | def :: defs when Caml.(get_def_type def = Local) -> filter_local_defs defs
+  | def :: defs -> def :: filter_local_defs defs
+
+
+type scope = Location.t * def list
 type scopes = scope list
 
-let add_defs_to_scope : def list -> scope -> scope
-  = fun defs scope ->
-      let loc, scope_defs = scope in
-      loc, scope_defs @ defs
+let add_defs_to_scope : def list -> scope -> scope =
+ fun defs scope ->
+  let loc, scope_defs = scope in
+  loc, scope_defs @ defs
 
-let add_defs_to_scopes : def list -> scopes -> scopes
-  = fun defs scopes ->
-      List.map scopes ~f:(add_defs_to_scope defs)
 
-let merge_same_scopes : scopes -> scopes
-  = fun scopes ->
-      let rec aux scopes acc =
-        match scopes with
-          [] -> acc
-        | (loc,scope)::scopes ->
-            let same, different = List.partition_tf scopes ~f:(fun (_,s) -> List.equal def_equal s scope) in
-            let merged_scope_loc = List.fold_left same ~init:loc ~f:(fun loc (loc',_) -> Location.cover loc loc') in
-            let merged_scope = merged_scope_loc, scope in
-            aux different (merged_scope :: acc)
+let add_defs_to_scopes : def list -> scopes -> scopes =
+ fun defs scopes -> List.map scopes ~f:(add_defs_to_scope defs)
+
+
+let merge_same_scopes : scopes -> scopes =
+ fun scopes ->
+  let rec aux scopes acc =
+    match scopes with
+    | [] -> acc
+    | (loc, scope) :: scopes ->
+      let same, different =
+        List.partition_tf scopes ~f:(fun (_, s) -> List.equal def_equal s scope)
       in
-      aux scopes []
+      let merged_scope_loc =
+        List.fold_left same ~init:loc ~f:(fun loc (loc', _) ->
+            Location.cover loc loc')
+      in
+      let merged_scope = merged_scope_loc, scope in
+      aux different (merged_scope :: acc)
+  in
+  aux scopes []
+
 
 let rec flatten_defs defs =
   match defs with
-    [] -> []
-  | (Module { mod_case = Def d ; _ } as def) :: defs ->
-    [def] @ (flatten_defs (shadow_defs d)) @ flatten_defs defs
-  | def::defs -> def :: flatten_defs defs
+  | [] -> []
+  | (Module { mod_case = Def d; _ } as def) :: defs ->
+    [ def ] @ flatten_defs (shadow_defs d) @ flatten_defs defs
+  | def :: defs -> def :: flatten_defs defs
 
-and shadow_defs : def list -> def list
-  = fun defs ->
-      match defs with
-        [] -> []
-      | def::defs ->
-          let shadow_def def' = not @@ def_equal def def' in
-          def :: shadow_defs (List.filter defs ~f:shadow_def)
 
-let fix_shadowing_in_scope : scope -> scope
-  = fun (loc, defs) ->
-      let defs = shadow_defs defs in
-      let defs = flatten_defs defs in
-      loc, defs
+and shadow_defs : def list -> def list =
+ fun defs ->
+  match defs with
+  | [] -> []
+  | def :: defs ->
+    let shadow_def def' = not @@ def_equal def def' in
+    def :: shadow_defs (List.filter defs ~f:shadow_def)
 
-let fix_shadowing_in_scopes : scopes -> scopes
-  = fun scopes ->
-      List.map scopes ~f:fix_shadowing_in_scope
 
-module Bindings_map = Simple_utils.Map.Make ( struct type t = Ast_typed.expression_variable let compare = Value_var.compare end )
+let fix_shadowing_in_scope : scope -> scope =
+ fun (loc, defs) ->
+  let defs = shadow_defs defs in
+  let defs = flatten_defs defs in
+  loc, defs
+
+
+let fix_shadowing_in_scopes : scopes -> scopes =
+ fun scopes -> List.map scopes ~f:fix_shadowing_in_scope
+
+
+module Bindings_map = Simple_utils.Map.Make (struct
+  type t = Ast_typed.expression_variable
+
+  let compare = Value_var.compare
+end)
+
 type bindings_map = Ast_typed.type_expression Bindings_map.t
