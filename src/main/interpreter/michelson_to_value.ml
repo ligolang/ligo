905e9e8b7ce9ecea9af5a4cd73374d8b1b967a7f
Rémi Lesenechal <lesenechal.remi@gmail.com>
2022-09-21 08:43:29 +0000

    Testing framework: fixing decompilation of event payloads

--- src/main/interpreter/michelson_to_value.ml
+++ src/main/interpreter/michelson_to_value.ml
@@ -304,7 +304,15 @@ let rec decompile_to_untyped_value ~raise ~bigmaps
     Ligo_interpreter.Combinators.v_pair (addr, va)
   | ty, v -> raise.error (untranspilable ty v)
 
-
+(*
+decompile_value [v] [t] will morph value [v] into another value using type [t].
+LIGO types do not always find their equivalent in Michelson and some information can be lost:
+pairs/record ; lambda lacking types ; ... (?)
+e.g.
+  v == (1,2,3)
+  t == { a : int ; b : int ; c : int }
+  result == { a = 1 ; b = 2 ; c = 3}
+*)
 let rec decompile_value
   ~raise
   ~(bigmaps : bigmap list)
