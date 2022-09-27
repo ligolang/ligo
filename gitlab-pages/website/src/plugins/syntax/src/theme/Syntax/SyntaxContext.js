44a8cae1f96507dd47fbdac1139d36d6ada5eaf3
JulesGuesnon <jules.guesnon@hetic.net>
2022-09-27 11:04:30 +0200

    feat: Added lang to query param

--- gitlab-pages/website/src/plugins/syntax/src/theme/Syntax/SyntaxContext.js
+++ gitlab-pages/website/src/plugins/syntax/src/theme/Syntax/SyntaxContext.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const SyntaxContext = React.createContext({syntax: "jsligo", setSyntax: () => {} });
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+
+const valid = ["jsligo", "cameligo", "reasonligo", "pascaligo"];
+
+const ctx = {
+  syntax: (() => {
+    const lang = (params.lang || "").toLowerCase();
+
+    if (valid.includes(lang)) return lang;
+
+    return "jsligo";
+  })(),
+  setSyntax: () => {},
+};
+
+const SyntaxContext = React.createContext(ctx);
 
 export default SyntaxContext;
--- gitlab-pages/website/src/theme/Root.js
+++ gitlab-pages/website/src/theme/Root.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import SyntaxContext from '@theme/Syntax/SyntaxContext';
+import React, { useState } from "react";
+import SyntaxContext from "@theme/Syntax/SyntaxContext";
 
+let defaultSyntax = (() => {
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
 
-let defaultSyntax = 'jsligo';
-if (typeof window !== "undefined" && 'localStorage' in window) {
-  defaultSyntax = localStorage.getItem('syntax') || defaultSyntax
-}
+  const valid = ["jsligo", "cameligo", "reasonligo", "pascaligo"];
+
+  const lang = (params.lang || "").toLowerCase();
+
+  if (valid.includes(lang)) return lang;
+
+  return "jsligo";
+})();
 
 // Default implementation, that you can customize
-export default function Root({children}) {
-    const [syntax, setSyntax] = useState(defaultSyntax);
+export default function Root({ children }) {
+  const [syntax, setSyntax] = useState(defaultSyntax);
 
-    return (
-        <SyntaxContext.Provider value={{syntax, setSyntax}}>
-            {children}
-        </SyntaxContext.Provider>
-    );
+  return (
+    <SyntaxContext.Provider value={{ syntax, setSyntax }}>
+      {children}
+    </SyntaxContext.Provider>
+  );
 }
- 
