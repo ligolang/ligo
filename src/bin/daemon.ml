4ee01a2fa10a047ce24a48ab26b6c59b67d7c6c2
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-05 07:48:36 +0530

    Ligo daemon: output `\x00` null char on stdout & stderr

--- src/bin/daemon.ml
+++ src/bin/daemon.ml
@@ -1,26 +1,36 @@
-let main () =
+let output_null_char () =
+  let () = Out_channel.output_char stdout '\x00' in
+  let () = Out_channel.output_char stderr '\x00' in
+  let () = Out_channel.flush stdout in
+  Out_channel.flush stderr
+
+let main ~ligo_bin_path () =
   let () = Sys.catch_break true in
   let stdin = In_channel.stdin in
   let exception Break in
   try
     let () = while true do
-    let x = Stdio.In_channel.input_line stdin in
-    let x = match x with
-      Some s ->
-        let args = Result.ok_or_failwith (Parse_argv.parse s) in
-        let args = Array.of_list args in
-        let () = match args with
-          [| "exit" |] -> raise Break
-        | args ->
-          let cmd = ("", Array.concat [[| "ligo" |] ; args]) in
-          let _ = Cli_helpers.run_command cmd in
-          ()
-        in
-        ""
-    | None -> "Error: unable to read from stdin." in
-    print_endline x;
-    done in Ok("", "")
+      let e = match Stdio.In_channel.input_line stdin with
+        Some s ->
+          let args = Result.ok_or_failwith (Parse_argv.parse s) in
+          let args = Array.of_list args in
+          let () = match args with
+            [| "exit" |] -> raise Break
+          | args ->
+            let cmd = ("", Array.concat [[| ligo_bin_path |] ; args]) in
+            let _ = Cli_helpers.run_command cmd in
+            ()
+          in
+          ""
+      | None -> "Error: unable to read from stdin." in
+      let () = print_string e in
+      output_null_char ()
+    done in Ok("\x00", "\x00")
   with 
     Break
-  | Sys.Break -> Ok("", "") 
-  | _ -> Error ("","Unexpected error")
+  | Sys.Break -> 
+    output_null_char ();
+    Ok("\x00", "\x00") 
+  | _ -> 
+    output_null_char ();
+    Error ("\x00","Unexpected error\x00")
