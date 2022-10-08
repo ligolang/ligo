492d5a7ce225c7fc764d65c1e2cd3026472e364e
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-08 17:18:51 +0530

    Handle pass 11-self-ast-typed

--- src/passes/11-self_ast_typed/self_ast_typed.ml
+++ src/passes/11-self_ast_typed/self_ast_typed.ml
@@ -7,13 +7,17 @@ let all_program_passes ~raise ~warn_unused_rec = [
   Muchused.muchused_map_program ~raise;
   Helpers.map_program @@ Recursion.check_tail_expression ~raise ;
   Helpers.map_program @@ Recursion.remove_rec_expression ~raise ~warn_unused_rec ;
-  Helpers.map_program @@ Pattern_matching_simpl.peephole_expression ;
+  
+  (* TODO: move this to aggregated selfpass *)
+  (* Helpers.map_program @@ Pattern_matching_simpl.peephole_expression ; *)
 ]
 
 let all_expression_passes ~raise ~warn_unused_rec = [
   Helpers.map_expression @@ Recursion.check_tail_expression ~raise ;
   Helpers.map_expression @@ Recursion.remove_rec_expression ~raise ~warn_unused_rec ;
-  Pattern_matching_simpl.peephole_expression ;
+  
+  (* TODO: move this to aggregated selfpass *)
+  (* Pattern_matching_simpl.peephole_expression ; *)
 ]
 
 let contract_passes ~raise = [
