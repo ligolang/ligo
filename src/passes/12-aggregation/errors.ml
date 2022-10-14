6585ae1f76c31f508ce90b5e9eb1d2375dcd36d1
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-14 15:21:57 +0530

    Fix errors in 12-aggregation

--- src/passes/12-aggregation/errors.ml
+++ src/passes/12-aggregation/errors.ml
@@ -21,6 +21,19 @@ let error_ppformat : display_format:string display_format ->
       Format.fprintf f "@[<hv>%a@.Redundant pattern matching@]" Snippet.pp loc
   )
 
+let error_json : aggregation_error -> Ligo_prim.Error.t =
+  fun e ->
+  let open Ligo_prim.Error in
+  match e with
+  | `Aggregation_corner_case desc ->
+    let message = Format.sprintf "@[<hv>An aggregation corner case occurred:@.%s@]" desc in
+    let content = make_content ~message () in
+    make ~stage ~content
+  | `Aggregation_redundant_pattern location ->
+    let message = "Redundant pattern matching" in
+    let content = make_content ~message ~location () in
+    make ~stage ~content
+
 let error_jsonformat : aggregation_error -> Yojson.Safe.t = fun a ->
   let json_error ~stage ~content =
     `Assoc [
