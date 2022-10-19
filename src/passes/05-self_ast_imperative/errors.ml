8748cefadbcfa728dc5170e7b9e6b8808ccaac46
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-19 11:13:21 +0530

    Fix error in 05-self_ast_imperative

--- src/passes/05-self_ast_imperative/errors.ml
+++ src/passes/05-self_ast_imperative/errors.ml
@@ -88,6 +88,73 @@ let error_ppformat : display_format:string display_format ->
             Snippet.pp l
   )
 
+let rec error_json : self_ast_imperative_error -> Error.t =
+  fun e ->
+    let open Error in
+    match e with
+    | `Self_ast_imperative_non_linear_record e ->
+      let message  = Format.sprintf "Duplicated record field@.Hint: Change the name." in
+      let location = e.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_reserved_name (str,location) ->
+      let message = Format.sprintf "Reserved name %S.@.Hint: Change the name." str in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_non_linear_pattern t ->
+      let message  = Format.sprintf "Repeated variable in pattern.@.Hint: Change the name." in
+      let location = t.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_non_linear_type_decl t ->
+      let message  = Format.sprintf "Repeated type variable in type.@.Hint: Change the name." in
+      let location = t.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_non_linear_row t ->
+      let message  = Format.sprintf "Duplicated field or variant name.@.Hint: Change the name." in
+      let location = t.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_duplicate_parameter exp ->
+      let message  = Format.sprintf "Duplicated variable name in function parameter.@.Hint: Change the name." in
+      let location = exp.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_long_constructor (c,e) ->
+      let message  = Format.sprintf " Ill-formed data constructor \"%s\".@.Data constructors have a maximum length of 32 characters, which is a limitation imposed by annotations in Tezos." c in
+      let location = e.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_bad_timestamp (t,e) ->
+      let message  = Format.sprintf "Ill-formed timestamp \"%s\".@.At this point, a string with a RFC3339 notation or the number of seconds since Epoch is expected." t in
+      let location = e.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_bad_format_literal e ->
+      let message  = Format.asprintf "Ill-formed literal \"%a\".@.In the case of an address, a string is expected prefixed by either tz1, tz2, tz3 or KT1 and followed by a Base58 encoded hash and terminated by a 4-byte checksum.@.In the case of a key_hash, signature, or key a Base58 encoded hash is expected." Ast_imperative.PP.expression e in
+      let location = e.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_bad_conversion_bytes e ->
+      let message  = Format.sprintf "Ill-formed bytes literal.@.Example of a valid bytes literal: \"ff7a7aff\"." in
+      let location = e.location in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_vars_captured vars ->
+      let message = "Invalid capture of non-constant variable" in
+      let location = List.hd vars |> Option.map ~f:(fun (l,_) -> l) in
+      let content = make_content ~message ?location () in
+      make ~stage ~content
+    | `Self_ast_imperative_const_assigned (location, var) ->
+      let message  = Format.asprintf "Invalid assignment to constant variable \"%a\"" Value_var.pp var in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+    | `Self_ast_imperative_no_shadowing location ->
+      let message  = "Cannot redeclare block-scoped variable." in
+      let content  = make_content ~message ~location () in
+      make ~stage ~content
+
 let error_jsonformat : self_ast_imperative_error -> json = fun a ->
   let json_error ~stage ~content =
     `Assoc [
