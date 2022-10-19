390ac214e37fd8d6d3d963b162800084861bc42c
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-19 14:01:13 +0530

    Fix errors in 04-tree_abstraction pascaligo

--- src/passes/04-tree_abstraction/pascaligo/errors.ml
+++ src/passes/04-tree_abstraction/pascaligo/errors.ml
@@ -111,6 +111,95 @@ let error_ppformat : display_format:string display_format ->
         Snippet.pp_lift @@ loc
   )
 
+let error_json : abs_error -> Ligo_prim.Error.t =
+  fun e ->
+    let open Ligo_prim.Error in
+    match e with
+    | `Concrete_pascaligo_wrong_lvalue reg ->
+      let message = "Effectful updates must be performed on identified objects that are not accessed through a module." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unsupported_type_ann_on_patterns reg ->
+      let message = "Type annotations on this kind of patterns are not supported yet." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unsuported_pattern_in_function reg ->
+      let message = "These kind of patterns are not supported in function parameters." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unexpected_wildcard reg ->
+      let message = "Wildcards ('_') are not supported yet." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_expected_field_name reg ->
+      let message = "Expected a field name." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_expected_field_or_access reg ->
+      let message = "Expected a field name or an accessor." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_wrong_functional_lens reg ->
+      let message = "Functional lenses can't be used in record expressions." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_ignored_attribute location ->
+      let message = "Attribute being ignored." in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_expected_variable location ->
+      let message = "Expected a declaration name." in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_wrong_functional_updator reg ->
+      let message = "Functional update only work on records." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unknown_constant (s,location) ->
+      let message = Format.sprintf "Unknown constant: %s." s in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unsupported_pattern_type pl ->
+      let message = Format.sprintf "Invalid case pattern.@.Can't match on values." in
+      let location = Location.File (Raw.pattern_to_region pl) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unsupported_string_singleton te ->
+      let message = Format.sprintf "Invalid type. @.It's not possible to assign a string to a type." in
+      let location = Location.File (Raw.type_expr_to_region te) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_michelson_type_wrong (texpr,name) ->
+      let message = Format.sprintf "Invalid \"%s\" type.@.At this point, an annotation, in the form of a string, is expected for the preceding type." name in
+      let location = Location.File (Raw.type_expr_to_region texpr) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_michelson_type_wrong_arity (location,name) ->
+      let message = Format.sprintf "Invalid \"%s\" type.@.An even number of 2 or more arguments is expected, where each odd item is a type annotated by the following string." name in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_untyped_recursive_fun location ->
+      let message = "Invalid function declaration.@.Recursive functions are required to have a type annotation (for now)." in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_block_start_with_attribute block ->
+      let message = "Invalid attribute declaration.@.Attributes have to follow the declaration it is attached to." in
+      let location = Location.File block.region in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_pascaligo_unsupported_top_level_destructuring reg ->
+      let message = "Unsupported destructuring at top-level." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
 
 let error_jsonformat : abs_error -> Yojson.Safe.t = fun a ->
   let json_error ~stage ~content =
