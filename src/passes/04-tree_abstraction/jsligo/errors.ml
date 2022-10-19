9a5138c4c4adb489b7ab63aa2c84253c9681dd7d
Melwyn Saldanha <melwyn95@gmail.com>
2022-10-19 14:33:56 +0530

    Fix errors in 04-tree_abstraction jsligo

--- src/passes/04-tree_abstraction/jsligo/errors.ml
+++ src/passes/04-tree_abstraction/jsligo/errors.ml
@@ -188,6 +188,174 @@ let error_ppformat : display_format:string display_format ->
       Snippet.pp_lift r
   )
 
+let error_json : abs_error -> Ligo_prim.Error.t =
+  fun e ->
+    let open Ligo_prim.Error in
+    match e with
+    | `Concrete_jsligo_unknown_constant (s,location) ->
+      let message = Format.sprintf "Unknown constant: %s." s in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unknown_constructor (s,location) ->
+      let message = Format.sprintf "Unknown constructor in module: %s." s in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_untyped_recursive_fun reg ->
+      let message = "Invalid function declaration.@.Recursive functions are required to have a type annotation (for now)." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unsupported_pattern_type pl ->
+      let message = Format.sprintf 
+        "Invalid pattern matching.\
+        @.  If this is pattern matching over Booleans, then \"true\" or \"false\" is expected.\
+        @.  If this is pattern matching on a list, then one of the following is expected:\
+        @.    * an empty list pattern \"[]\";\
+        @.    * a cons list pattern \"[head, ...tail]\".\
+        @.  If this is pattern matching over variants, then a constructor of a variant is expected.\
+        @.\
+        @.  Other forms of pattern matching are not (yet) supported." in
+      let location = Location.File (Raw.pattern_to_region pl) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unsupported_string_singleton te ->
+      let message = "Invalid type. @.It's not possible to assign a string to a type." in
+      let location = Location.File (Raw.type_expr_to_region te) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_recursion_on_non_function location ->
+      let message = "Invalid let declaration.@.Only functions can be recursive." in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_michelson_type_wrong (texpr,name) ->
+      let message = Format.sprintf "Invalid \"%s\" type.@.At this point, an annotation, in the form of a string, is expected for the preceding type." name in
+      let location = Location.File (Raw.type_expr_to_region texpr) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_michelson_type_wrong_arity (location,name) ->
+      let message = Format.sprintf "Invalid \"%s\" type.@.An even number of 2 or more arguments is expected, where each odd item is a type annotated by the following string." name in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_funarg_tuple_type_mismatch (region, pattern, texpr) ->
+      let p = Parsing.pretty_print_pattern pattern |> Buffer.contents in
+      let t = Parsing.pretty_print_type_expr texpr |> Buffer.contents in
+      let message = Format.sprintf "The tuple \"%s\" does not have the expected type \"%s\"." p t in
+      let location = Location.File region in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_not_in_switch_or_loop reg ->
+      let message = Format.sprintf "Invalid let declaration.@.Only functions can be recursive." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_statement_not_supported_at_toplevel s ->
+      let message = Format.sprintf "Statement not supported at toplevel.@.Only let, const, and type statements are supported at the toplevel." in
+      let location = Location.File (Raw.statement_to_region s) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_not_a_valid_parameter p ->
+      let message = Format.sprintf "Not a valid function parameter." in
+      let location = Location.File (Raw.expr_to_region p) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_rest_not_supported_here p ->
+      let message = Format.sprintf "Rest property not supported here." in
+      let location = Location.File (Raw.property_to_region p) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_property_not_supported p ->
+      let message = Format.sprintf "This kind of property not supported here." in
+      let location = Location.File (Raw.property_to_region p) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_expected_an_expression a ->
+      let message = Format.sprintf "Expected an expression." in
+      let location = Location.File (Raw.array_item_to_region a) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_new_not_supported p ->
+      let message = Format.sprintf "'new' keyword not supported." in
+      let location = Location.File (Raw.expr_to_region p) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_invalid_case (s, e) ->
+      let message = Format.sprintf "Invalid '%s' field value. An anonymous arrow function was expected, eg. `None: () => foo`." s in
+      let location = Location.File (Raw.expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_invalid_constructor e ->
+      let message = Format.sprintf "Invalid constructor. Expected a constructor like: `[\"Foo\"]` or `[\"Foo\", int, string]`." in
+      let location = Location.File (Raw.type_expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unsupported_match_pattern e ->
+      let message = Format.sprintf "Unsupported match pattern." in
+      let location = Location.File (Raw.expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unsupported_match_object_property p ->
+      let message = Format.sprintf "Unsupported pattern match object property." in
+      let location = Location.File (Raw.property_to_region p) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_expected_a_function e ->
+      let message = Format.sprintf "Expected a function." in
+      let location = Location.File (Raw.expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_not_supported_assignment e ->
+      let message = Format.sprintf "Not supported assignment." in
+      let location = Location.File (Raw.expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_array_rest_not_supported e ->
+      let message = Format.sprintf "Rest property not supported here." in
+      let location = Location.File (Raw.array_item_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_expected_a_variable reg ->
+      let message = Format.sprintf "Expected a variable." in
+      let location = Location.File reg in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_expected_a_field_name s ->
+      let message = Format.sprintf "Expected a field name." in
+      let location = Location.File (Raw.selection_to_region s) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_expected_an_int e ->
+      let message = Format.sprintf "Expected an int." in
+      let location = Location.File (Raw.expr_to_region e) in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_invalid_list_pattern_match _l ->
+      let message = Format.sprintf "Invalid list pattern matching." in
+      let content = make_content ~message () in
+      make ~stage ~content
+    | `Concrete_jsligo_no_shared_fields r ->
+      let message = Format.sprintf "All the objects are expected to have a shared field with an unique value." in
+      let location = Location.File r in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_unexpected -> 
+      let message = Format.sprintf "Unexpected error." in
+      let content = make_content ~message () in
+      make ~stage ~content
+    | `Concrete_jsligo_wrong_matchee_disc r ->
+      let message = Format.sprintf "Expected a record field. For example: `field.kind`." in
+      let location = Location.File r in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_case_break_disc r ->
+      let message = Format.sprintf "A discriminated union case needs to end with a `break` or `return` statement. A fallthrough to another case is currently not supported." in
+      let location = Location.File r in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
+    | `Concrete_jsligo_not_implemented r ->
+      let message = Format.sprintf "This has not been implemented yet." in
+      let location = Location.File r in
+      let content = make_content ~message ~location () in
+      make ~stage ~content
 
 let error_jsonformat : abs_error -> Yojson.Safe.t = fun a ->
   let json_error ~stage ~content =
