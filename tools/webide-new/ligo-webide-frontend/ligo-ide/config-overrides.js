29dc11c62b0c4f98cdbbda069ab46b209940dc2c
sashasashasasha151 <sashasashasasha151@gmail.com>
2022-10-27 01:38:59 +0400

    [LIGO-835] Use generated api

--- tools/webide-new/ligo-webide-frontend/ligo-ide/config-overrides.js
+++ tools/webide-new/ligo-webide-frontend/ligo-ide/config-overrides.js
@@ -173,7 +173,7 @@ module.exports = {
     return function (proxy, allowedHost) {
       const config = configFunction({
         '/api': {
-          target: 'http://localhost:8080',
+          target: 'http://127.0.0.1:8080',
           pathRewrite: { '^/api': '' },
         },
       }, allowedHost);
