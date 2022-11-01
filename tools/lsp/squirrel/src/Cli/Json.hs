d1bb8a575a8dcc1ccede26b2df1e9820c5918247
Iurii Kravchenko <iurii.kravchenko@serokell.io>
2022-11-01 21:39:41 +0400

    [Chore] Refactor fromLigoTypeFull

--- tools/lsp/squirrel/src/Cli/Json.hs
+++ tools/lsp/squirrel/src/Cli/Json.hs
@@ -28,12 +28,11 @@ module Cli.Json
 where
 
 import Control.Lens (over, _head)
-import Control.Monad.State (State, evalState, get, gets, modify)
 import Data.Aeson.Types hiding (Error)
 import Data.Aeson.KeyMap (toAscList)
 import Data.Char (isUpper, toLower)
 import Data.Foldable (toList)
-import Data.Function (on, (&))
+import Data.Function ((&))
 import Data.HashMap.Strict qualified as HM
 import Data.List qualified as List
 import Data.List.NonEmpty (NonEmpty (..))
@@ -502,97 +501,84 @@ data FieldKind = FieldSum | FieldProduct
 
 -- | Reconstruct `LIGO` tree out of `LigoTypeFull`.
 fromLigoTypeFull :: LigoTypeFull -> LIGO Info
-fromLigoTypeFull = enclose . \case
+fromLigoTypeFull = \case
   LTFCore lte     -> fromLigoTypeExpression lte
   LTFResolved lte -> fromLigoTypeExpression lte
-  LTFUnresolved   -> mkErr "unresolved type given"
+  LTFUnresolved   -> mkLigoError defaultState "unresolved type given"
+
+fromLigoTypeExpression :: LigoTypeExpression -> LIGO Info
+fromLigoTypeExpression
+  LigoTypeExpression {..} =
+    let st = putElem (fromLigoRangeOrDef _lteLocation) defaultState in
+    fromLigoType st _lteTypeContent
+
+fromLigoType
+  :: Product Info
+  -> LigoTypeContent
+  -> LIGO Info
+fromLigoType st = \case
+  LTCConstant LigoTypeConstant {..} ->
+    -- See: https://gitlab.com/ligolang/ligo/-/issues/1478
+    fromLigoConstant (NE.head _ltcInjection & over _head toLower) _ltcParameters
+
+  LTCVariable variable -> fromLigoPrimitive Nothing $ _ltvName variable
+
+  LTCRecord record ->
+    let record' = fromLigoTable FieldProduct record in
+    make' (st, TRecord record')
+
+  LTCSum sum ->
+    case fromLigoTable FieldSum sum of
+      [] -> mkErr "malformed sum type, please report this as a bug"
+      v : vs -> make' (st, TSum (v :| vs))
+
+  LTCSingleton      _ -> mkErr "unsupported type `Singleton`"      -- TODO not used
+  LTCAbstraction    _ -> mkErr "unsupported type `Abstraction`"    -- TODO not used
+  LTCForAll         _ -> mkErr "unsupported type `ForAll`"         -- TODO not used
+  LTCModuleAccessor _ -> mkErr "unsupported type `ModuleAccessor`" -- TODO not used
+
+  LTCApp LigoTypeApp{..} ->
+    let n = fromLigoPrimitive Nothing (_ltvName _ltaTypeOperator) in
+    let p = fromLigoTypeExpression <$> _ltaArguments in
+    make' (st, TApply n p)
+
+  LTCArrow LigoTypeArrow {..} ->
+    make' (st, TArrow (fromLigoTypeExpression _ltaType1) (fromLigoTypeExpression _ltaType2))
+
   where
 
-    fromLigoPrimitive :: Maybe FieldKind -> Text -> State (Product Info) (LIGO Info)
-    fromLigoPrimitive fieldKind p = do
-      st <- get
-      return case fieldKind of
-        Just FieldSum     -> make' (st, Ctor p)
-        Just FieldProduct -> make' (st, FieldName p)
-        Nothing           -> make' (st, TypeName p)
-
-    fromLigoTypeExpression
-      LigoTypeExpression {..} = do
-        modify . putElem . fromLigoRangeOrDef $ _lteLocation
-        fromLigoType _lteTypeContent
+    fromLigoPrimitive :: Maybe FieldKind -> Text -> LIGO Info
+    fromLigoPrimitive = \case
+      Just FieldSum     -> make' . (st,) . Ctor
+      Just FieldProduct -> make' . (st,) . FieldName
+      Nothing           -> make' . (st,) . TypeName
 
     fromLigoConstant name [] = fromLigoPrimitive Nothing name
-    fromLigoConstant name params = do
-      st <- get
-      n <- fromLigoPrimitive Nothing name
-      p <- sequence $ fromLigoTypeExpression <$> params
-      return $ make' (st, TApply n p)
+    fromLigoConstant name params =
+      let n = fromLigoPrimitive Nothing name in
+      let p = fromLigoTypeExpression <$> params in
+      make' (st, TApply n p)
 
-    fromLigoType
-      :: LigoTypeContent
-      -> State (Product Info) (LIGO Info)
-    fromLigoType = \case
-      LTCConstant LigoTypeConstant {..} ->
-        -- See: https://gitlab.com/ligolang/ligo/-/issues/1478
-        fromLigoConstant (NE.head _ltcInjection & over _head toLower) _ltcParameters
-
-      LTCVariable variable ->
-        fromLigoPrimitive Nothing $ _ltvName variable
-
-      LTCRecord record -> do
-        st <- get
-        record' <- fromLigoTable FieldProduct record
-        return $ make' (st, TRecord record')
-
-      LTCSum sum -> do
-        st <- get
-        sum' <- fromLigoTable FieldSum sum
-        case sum' of
-          [] -> mkErr "malformed sum type, please report this as a bug"
-          v : vs -> pure $ make' (st, TSum (v :| vs))
-
-      LTCSingleton      _ -> mkErr "unsupported type `Singleton`"      -- TODO not used
-      LTCAbstraction    _ -> mkErr "unsupported type `Abstraction`"    -- TODO not used
-      LTCForAll         _ -> mkErr "unsupported type `ForAll`"         -- TODO not used
-      LTCModuleAccessor _ -> mkErr "unsupported type `ModuleAccessor`" -- TODO not used
-
-      LTCApp LigoTypeApp{..} -> do
-        st <- get
-        p <- fromLigoPrimitive Nothing (_ltvName _ltaTypeOperator)
-        return . make' . (st,) $
-          TApply p (enclose . fromLigoTypeExpression <$> _ltaArguments)
-
-      LTCArrow LigoTypeArrow {..} -> do
-        st <- get
-        let mkArrow = TArrow `on` (enclose . fromLigoTypeExpression)
-        return $ make' (st, mkArrow _ltaType1 _ltaType2)
-
-    fromLigoTable fieldKind =
-      traverse (uncurry (fromLigoTableField fieldKind)) . HM.toList . _lttFields
+    fromLigoTable fieldKind x =
+      map (uncurry (fromLigoTableField fieldKind)) $ HM.toList $ _lttFields x
 
     fromLigoTableField
       :: FieldKind
       -> Text
       -> LigoTableField
-      -> State (Product Info) (LIGO Info)
-    fromLigoTableField fieldKind name LigoTableField {..} = do
-      st <- get
-      n <- fromLigoPrimitive (Just fieldKind) name
+      -> LIGO Info
+    fromLigoTableField fieldKind name LigoTableField {..} =
+      let n = fromLigoPrimitive (Just fieldKind) name in
       -- FIXME: Type annotation is optional.
-      let type' = Just $ enclose $ fromLigoTypeExpression _ltfAssociatedType
-      return case fieldKind of
+      let type' = Just $ fromLigoTypeExpression _ltfAssociatedType in
+      case fieldKind of
         FieldSum     -> make' (st, Variant n type')
         FieldProduct -> make' (st, TField  n type')
 
-    mkErr = gets . flip mkLigoError
+    mkErr = mkLigoError st
 
-    enclose
-      :: State (Product Info) (LIGO Info)
-      -> LIGO Info
-    enclose = flip evalState defaultState
-
-    defaultState :: Product Info
-    defaultState = [] :> [] :> point 1 1 :> CodeSource "" :> Nil
+defaultState :: Product Info
+defaultState = [] :> [] :> point 1 1 :> CodeSource "" :> Nil
 
 mkLigoError :: Product Info -> Text -> LIGO Info
 mkLigoError p msg = make' . (p,) $ Error (FromLIGO msg) []
