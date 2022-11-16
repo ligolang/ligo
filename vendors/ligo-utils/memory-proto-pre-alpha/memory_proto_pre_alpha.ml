88019bbcc116f9649b258378131b46de7760eacc
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-11-16 12:19:21 +0530

    Tezos_014_PtKathma_test_helpers -> P014_test_helpers

--- vendors/ligo-utils/memory-proto-pre-alpha/memory_proto_pre_alpha.ml
+++ vendors/ligo-utils/memory-proto-pre-alpha/memory_proto_pre_alpha.ml
@@ -4,7 +4,7 @@ module Alpha_environment = Tezos_protocol_environment_014_PtKathma
 module Raw_protocol = Tezos_raw_protocol_014_PtKathma
 module Parameters = Tezos_protocol_014_PtKathma_parameters
 module Client = Tezos_client_014_PtKathma
-module Test_helpers = Tezos_014_PtKathma_test_helpers
+module Test_helpers = P014_test_helpers
 
 type alpha_error = Alpha_environment.Error_monad.error
 type 'a alpha_tzresult = 'a Alpha_environment.Error_monad.tzresult
