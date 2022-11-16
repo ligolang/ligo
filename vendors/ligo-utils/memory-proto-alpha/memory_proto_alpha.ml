2e869d9c4cd3cc55ff9f237c15fecd6a41d1d18a
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-11-16 12:18:53 +0530

    Tezos_015_PtLimaPt_test_helpers -> T015_test_helpers

--- vendors/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
+++ vendors/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
@@ -4,7 +4,7 @@ module Alpha_environment = Tezos_protocol_environment_015_PtLimaPt
 module Raw_protocol = Tezos_raw_protocol_015_PtLimaPt
 module Parameters = Tezos_protocol_015_PtLimaPt_parameters
 module Client = Tezos_client_015_PtLimaPt
-module Test_helpers = Tezos_015_PtLimaPt_test_helpers
+module Test_helpers = T015_test_helpers
 
 type alpha_error = Alpha_environment.Error_monad.error
 type 'a alpha_tzresult = 'a Alpha_environment.Error_monad.tzresult
