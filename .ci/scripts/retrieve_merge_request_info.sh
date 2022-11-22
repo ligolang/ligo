76ebab75a2aad8fc5d57a898380ab8d546b5f9d9
Laurent Canis <laurent@marigold.dev>
2022-11-22 13:11:15 -0500

    fix changelog script...

--- .ci/scripts/retrieve_merge_request_info.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-HOST=$1
-CI_PROJECT_ID=$2
-CI_MERGE_REQUEST_IID=$3
-PRIVATE_TOKEN=$4
-
-echo `python3 parse_mr_info.py --hostname "${HOST}" --project "${CI_PROJECT_ID}" --mr-id "${CI_MERGE_REQUEST_IID}" --token "${PRIVATE_TOKEN}"`
