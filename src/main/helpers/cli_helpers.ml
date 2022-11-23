8c4bae21e1dc612a9d6af0da62340cda3ab7f947
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-11-23 05:33:34 +0530

    Use Unix.create_process_env on Windows

--- src/main/helpers/cli_helpers.ml
+++ src/main/helpers/cli_helpers.ml
@@ -87,15 +87,109 @@ let does_command_exist (cmd : string) =
   | _ -> Error "unknown error"
 
 
+
+let makeCommand cmd =
+  let open Caml in
+  match Sys.unix with
+  | true  -> cmd
+  | false  ->
+      let pathVars =
+        Ligo_unix.environment ()
+        |> Array.to_list
+        |> List.map
+                (fun e  ->
+                   match Str.split
+                           (Str.regexp (("=")[@reason.raw_literal "="])) e
+                   with
+                   | k::v::_rest -> Some (k, v)
+                   | _ -> None)
+        |> List.filter (function | None  -> false | _ -> true)
+        |> List.filter (fun e  ->
+               match e with
+               | Some (k,_) -> (String.lowercase_ascii k) = "path"
+               | _ -> false)
+        |> List.map (function
+              | Some x -> x
+              | None  -> ("", ""))
+      in
+      let v =
+        List.fold_right
+          (fun e  ->
+             fun acc  ->
+               let (_,v) = e in
+               acc ^
+                 ((match Sys.unix with
+                   | true  -> ((":")[@reason.raw_literal ":"])
+                   | false  -> ((";")[@reason.raw_literal ";"])) ^ v))
+          pathVars "" in
+      let paths =
+        Str.split
+          (Str.regexp
+             (match Sys.unix with
+              | true  -> ((":")[@reason.raw_literal ":"])
+              | false  -> ((";")[@reason.raw_literal ";"]))) v in
+      let npmPaths =
+        List.filter
+          (fun path  ->
+             Sys.file_exists
+               (Filename.concat path
+                  (sprintf (("%s.cmd")[@reason.raw_literal "%s.cmd"]) cmd)))
+          paths in
+      (match npmPaths with
+       | [] ->
+           (fprintf stderr
+              (("No %s bin path found")[@reason.raw_literal
+                                         "No %s bin path found"]) cmd;
+            exit (-1))
+       | h::_ ->
+           Filename.concat h
+             (sprintf (("%s.cmd")[@reason.raw_literal "%s.cmd"]) cmd))
+
+let run ?env  c args =
+  let open Ligo_unix in
+  let env_vars =
+    match env with
+    | ((Some (v))[@explicit_arity ]) -> v
+    | None  -> Ligo_unix.environment () in
+  let pid =
+    Ligo_unix.create_process_env c (Array.append [|c|] args) env_vars stdin stdout
+      stderr in
+  match Ligo_unix.waitpid [] pid with
+  | (_,((WEXITED (c))[@explicit_arity ])) -> c
+  | (_,((WSIGNALED (c))[@explicit_arity ])) -> c
+  | (_,((WSTOPPED (c))[@explicit_arity ])) -> c
+
+let run ?env cmd args =
+  let exitCode = run ?env cmd args in
+  if exitCode <> 0
+  then
+    (printf
+       (("%s failed. Exit code relayed to the shell\n Exiting with (%d)...\n")
+       [@reason.raw_literal
+         "%s failed. Exit code relayed to the shell\\n Exiting with (%d)...\\n"])
+       cmd exitCode)
+
 (* Runs a commands in a separate process *)
 let run_command (cmd : command) =
-  let status =
-    Lwt_process.with_process_none ~stdout:`Keep ~stderr:`Keep cmd (fun p ->
-        Lwt.map
-          (fun status ->
-            match status with
-            | Ligo_unix.WEXITED 0 -> Ok ()
-            | _ -> Error "unknown error")
-          p#status)
-  in
-  Lwt_main.run status
+  if Sys.unix then
+    let status =
+      Lwt_process.with_process_none ~stdout:`Keep ~stderr:`Keep cmd
+        (fun p -> Lwt.map
+                    (fun status ->
+                      match status with
+                        Ligo_unix.WEXITED 0 -> Ok ()
+                      | _ -> Error ("unknown error"))
+                    p#status)
+    in
+    Lwt_main.run status
+  else
+    let (_empty_string, args) = cmd in
+    let bin = args.(0) in
+    let bin_full_path = makeCommand bin in
+    let env = Ligo_unix.environment ()
+              |> Array.filter ~f:(fun kv -> not (Str.string_match (Str.regexp "_=") kv 0))
+              |> Array.append([|"_="|])
+    in
+    let args = Array.sub args ~pos:1 ~len:((Array.length args) - 1) in
+    run ~env "cmd.exe" (Array.append [| "/c"; bin_full_path |] args);
+    Ok ()
