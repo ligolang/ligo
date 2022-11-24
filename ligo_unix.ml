d12c4113490211b70392ee650159a31d5e522315
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2022-11-25 02:13:54 +0530

    Fix match warning in ligo_unix.ml

--- ligo_unix.ml
+++ ligo_unix.ml
@@ -3,8 +3,8 @@ let ls_dir directory = Array.to_list (Sys.readdir directory)
 let home_directory () =
   match Sys.getenv_opt "HOME" with
   | Some home -> home
-  | None when Sys.win32-> (match Sys.getenv_opt "USERPROFILE", Sys.getenv "HOMEDRIVE", Sys.getenv "HOMEPATH" with
-                          | _, home_drive, home_path -> home_drive ^ home_path
+  | None when Sys.win32-> (match Sys.getenv_opt "USERPROFILE", Sys.getenv_opt "HOMEDRIVE", Sys.getenv_opt "HOMEPATH" with
+                          | _, Some home_drive, Some home_path -> home_drive ^ home_path
                           | Some home, _, _ -> home
-                          | _ -> "")
+                          | _, _, _ -> "")
   | None -> ""
