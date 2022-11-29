ef55bc7db183114237daa154c0c14ef8958371ee
Leonid Vasilev <leonid.vasilev@serokell.io>
2022-11-29 17:34:07 +0300

    [LIGO-917] Make tag functions that disable tests
    Problem: at this moment we can separate all tests into 3 groups:
    1. Regular. We don't want to see these tests failed
    2. Reinsuring. They're like the regular but take very long time
    to run. It would be more convenient to run them only in CI
    3. Minor. These tests can fail when the used `ligo` version is only
    partially supported by us.
    
    It would be a good idea to write some `tasty` options for them
    
    Solution: implement 2 tag functions: `reinsuring` and `minor`.
    These options are `False` by default, so, marked tests wouldn't
    be launched by default.

--- tools/debugger/ligo-debugger/test/Driver.hs
+++ tools/debugger/ligo-debugger/test/Driver.hs
@@ -1,1 +1,1 @@
-{-# OPTIONS_GHC -F -pgmF tasty-discover -optF --tree-display -optF --hide-successes -optF --generated-module -optF Driver #-}
+{-# OPTIONS_GHC -F -pgmF tasty-discover -optF --ingredient=Test.Util.Options.ligoIngredient -optF --tree-display -optF --hide-successes -optF --generated-module -optF Driver #-}
