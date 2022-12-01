9d879eda43562894c2bcbda9cfa35b1851df9c98
Melwyn Saldanha <melwyn95@gmail.com>
2022-12-01 17:49:48 +0530

    Optimization for PUSH nat 1 ; INT ; NEG  ↦  PUSH int -1

--- src/bin/expect_tests/literals.ml
+++ src/bin/expect_tests/literals.ml
@@ -51,6 +51,15 @@ let%expect_test _ =
     { DROP ; PUSH int -1 } |}]
 
 let%expect_test _ =
+  run_ligo_good [ "compile"; "expression"; "cameligo"; " -(int 100n)"; "--without-run" ];
+  [%expect {| { PUSH int -100 } |}]
+
+let%expect_test _ =
+  run_ligo_good [ "compile"; "expression"; "cameligo"; "(fun () -> -(int 1n) : unit -> int)" ];
+  [%expect {|
+    { DROP ; PUSH int -1 } |}]
+
+let%expect_test _ =
   run_ligo_bad
     [ "run"; "interpret"; "(\"thisisnotapublickey\":key)"; "--syntax"; "pascaligo" ];
   [%expect
