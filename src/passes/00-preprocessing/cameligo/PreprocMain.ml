e5944b2d4f97c5c36cc4bd8bd7a4e0aa2753baf3
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2022-12-15 11:27:37 +0100

    Renamed 00-preprocessing/shared/PreprocMainGen.ml into PreprocMain.ml

--- src/passes/00-preprocessing/cameligo/PreprocMain.ml
+++ src/passes/00-preprocessing/cameligo/PreprocMain.ml
@@ -1,7 +1,6 @@
 (* Driving the standalone preprocessor for CameLIGO *)
 
-module PreprocMainGen = Preprocessing_shared.PreprocMainGen
-module Config         = Preprocessing_cameligo.Config
-module PreprocMain    = PreprocMainGen.Make (Config)
+module Config      = Preprocessing_cameligo.Config
+module PreprocMain = Preprocessing_shared.PreprocMain.Make (Config)
 
 let () = PreprocMain.run ()
