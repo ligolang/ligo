d99cdeaf6c0e6f95df79173af5d0a05dcac58ef3
Melwyn Saldanha <melwyn95@gmail.com>
2022-12-16 12:02:59 +0530

    Add more tests

--- src/bin/expect_tests/automatic_semicolon_jsligo.ml
+++ src/bin/expect_tests/automatic_semicolon_jsligo.ml
@@ -8,10 +8,34 @@ let%expect_test _ =
     {|
     Everything at the top-level was executed.
     - test1 exited with value 1.
+    - test2 exited with value 2. |}];
+  run_ligo_good [ "run"; "test"; contract "asi_line_comment_before.jsligo" ];
+  [%expect
+    {|
+    Everything at the top-level was executed.
+    - test1 exited with value 1.
+    - test2 exited with value 2. |}];
+  run_ligo_good [ "run"; "test"; contract "asi_line_comment_after.jsligo" ];
+  [%expect
+    {|
+    Everything at the top-level was executed.
+    - test1 exited with value 1.
     - test2 exited with value 2. |}]
 
 let%expect_test _ =
   run_ligo_good [ "run"; "test"; contract "asi_block_comment.jsligo" ];
+  [%expect
+    {|
+    Everything at the top-level was executed.
+    - test1 exited with value 1.
+    - test2 exited with value 2. |}];
+  run_ligo_good [ "run"; "test"; contract "asi_block_comment_before.jsligo" ];
+  [%expect
+    {|
+    Everything at the top-level was executed.
+    - test1 exited with value 1.
+    - test2 exited with value 2. |}];
+  run_ligo_good [ "run"; "test"; contract "asi_block_comment_after.jsligo" ];
   [%expect
     {|
     Everything at the top-level was executed.
