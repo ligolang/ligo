fb579badee7f2f15d8d3167024ce0e6969ef39a9
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2023-01-17 11:41:22 +0000

    Update index.js

--- gitlab-pages/website/src/pages/index.js
+++ gitlab-pages/website/src/pages/index.js
@@ -73,8 +73,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={`${siteConfig.title}`}
+      description="A friendly Smart Contract Language for Tezos"
     >
       <main>
         <div
