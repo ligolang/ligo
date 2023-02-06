fa0b110f83c9e4ee1fc33cc4f0b8a0f26fadda51
Leonid Vasilev <leonid.vasilev@serokell.io>
2023-02-06 23:24:33 +0300

    [#1541] Use `parsePreprocessed` instead of `parse`
    Problem: we noticed that we see locations for top-level functions
    with some preprocessor directives. After some investigations we've seen
    that the problem is wider: we're trying to parse a contract before
    preprocessing stage, so, we have some parse errors there.
    
    Solution: use `parsePreprocessed` instead of `parse`. Change types
    where it is necessary.

--- tools/debugger/ligo-debugger/test/Test/Navigation.hs
+++ tools/debugger/ligo-debugger/test/Test/Navigation.hs
@@ -89,6 +89,23 @@ test_Seq_node_doesn't_have_location =
       runData
       (dumpAllSnapshotsWithStep doStep)
 
+test_top_level_function_with_preprocessor_don't_have_locations :: TestTree
+test_top_level_function_with_preprocessor_don't_have_locations =
+  let
+    runData = ContractRunData
+      { crdProgram = contractsDir </> "contract-with-preprocessor.mligo"
+      , crdEntrypoint = Nothing
+      , crdParam = ()
+      , crdStorage = 0 :: Integer
+      }
+
+    doStep = processLigoStep (CStepIn GExpExt)
+  in goldenTestWithSnapshots
+      "top-level functions with preprocessor don't have expression locations in snapshots"
+      "StepIn"
+      runData
+      (dumpAllSnapshotsWithStep doStep)
+
 test_Next_golden :: TestTree
 test_Next_golden = testGroup "Next" do
   gran <- allLigoStepGranularities
