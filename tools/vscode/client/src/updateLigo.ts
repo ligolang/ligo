84afa0e0e075d3c213de3b67baf1d8318233c9a7
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2023-02-09 16:42:37 -0300

    [#1678] Better detection of update version
    Problem: Looks like we don't properly update when we installed a correct
    version.
    
    Solution: Try to get whether we have a new version or the old version
    and compare it instead of the old version.

--- tools/vscode/client/src/updateLigo.ts
+++ tools/vscode/client/src/updateLigo.ts
@@ -27,18 +27,18 @@ type Release = {
 }
 /* eslint-enable camelcase */
 
-export async function installLigo(ligoPath: string, latest: Release): Promise<void> {
+export async function installLigo(ligoPath: string, latest: Release): Promise<boolean> {
   const asset = latest.assets.links.find((download) => download.name === 'Static Linux binary')
   if (!asset) {
     await vscode.window.showErrorMessage('Could not find a download for a static Linux binary.')
-    return
+    return false
   }
 
   const fileOptions = {
     encoding: 'binary' as BufferEncoding,
   }
 
-  vscode.window.withProgress(
+  return vscode.window.withProgress(
     {
       cancellable: true,
       location: vscode.ProgressLocation.Notification,
@@ -63,7 +63,7 @@ export async function installLigo(ligoPath: string, latest: Release): Promise<vo
       .then((res) => {
         if (cancelToken.isCancellationRequested) {
           vscode.window.showInformationMessage('LIGO installation cancelled.')
-          return
+          return false
         }
 
         // FIXME: Sometimes the installation may fail. This will happen if ligo
@@ -72,18 +72,20 @@ export async function installLigo(ligoPath: string, latest: Release): Promise<vo
         fs.writeFile(ligoPath, Buffer.from(res.data), fileOptions, (err) => {
           if (err) {
             vscode.window.showErrorMessage(`Could not install LIGO: ${err.message}`)
+            return false
           } else {
             vscode.window.showInformationMessage(`LIGO installed at: ${path.resolve(ligoPath)}`)
+            return true
           }
         })
       })
       .catch((err) => {
         if (axios.default.isCancel(err)) {
           vscode.window.showInformationMessage('LIGO download cancelled.')
-          return
         }
 
         vscode.window.showErrorMessage(`Could not download LIGO: ${err.message}`)
+        return false
       }),
   )
 }
@@ -124,7 +126,7 @@ function openLigoReleases(): Thenable<boolean> {
 async function promptLigoUpdate(
   ligoPath: string,
   installedVersionIdentifier: string | number,
-): Promise<void> {
+): Promise<string | number> {
   const latestRelease = await getLatestLigoRelease()
   if (!latestRelease) {
     return
@@ -134,13 +136,13 @@ async function promptLigoUpdate(
     // Semantic version
     case 'string':
       if (semver.gte(installedVersionIdentifier, latestRelease.tag_name)) {
-        return
+        return installedVersionIdentifier
       }
       break
     // Date of some rolling release
     case 'number':
       if (new Date(installedVersionIdentifier) >= latestRelease.released_at) {
-        return
+        return installedVersionIdentifier
       }
       break
     default:
@@ -156,7 +158,9 @@ async function promptLigoUpdate(
 
   switch (answer) {
     case 'Static Linux Binary':
-      installLigo(ligoPath, latestRelease)
+      if (await installLigo(ligoPath, latestRelease)) {
+        return latestRelease.tag_name
+      }
       break
     case 'Open Downloads':
       openLigoReleases()
@@ -165,6 +169,8 @@ async function promptLigoUpdate(
     default:
       break
   }
+
+  return installedVersionIdentifier
 }
 
 export default async function updateLigo(): Promise<void> {
@@ -236,31 +242,59 @@ async function updateLigoImpl(config: vscode.WorkspaceConfiguration): Promise<vo
     return
   }
 
-  const semverTest = semver.valid(semver.coerce(data))
-  if (semverTest) {
-    promptLigoUpdate(ligoPath, semverTest)
-
-    if (semver.lt(semverTest, semver.coerce('0.61.0'))) {
-      vscode.window.showErrorMessage(
-        'You need LIGO version 0.61.0 or greater so that `ligo lsp` may work. Closing the language server. Please update and try again.'
-      )
-      throw new Error("Unsupported version")
-    }
-
-    return
+  async function unsupportedVersion<T>(): Promise<T> {
+    await vscode.window.showErrorMessage(
+      'You need LIGO version 0.61.0 or greater so that `ligo lsp` may work. Closing the language server. Please update and try again.'
+    )
+    throw new Error("Unsupported version")
   }
 
-  const commitTest = /Rolling release\nCommit SHA: [0-9a-f]{40}\nCommit Date: (.+)/
-  const commitDate = commitTest.exec(data)
-  if (commitDate && commitDate.length === 2) {
-    const date: number = Date.parse(commitDate[1])
-    if (Number.isNaN(date)) {
-      // Parse failed; not a date.
-      return
+  async function validateSemver(version: string) {
+    const semverTest = semver.valid(semver.coerce(version))
+    if (semverTest) {
+      const newVersion = await promptLigoUpdate(ligoPath, semverTest)
+      switch (typeof newVersion) {
+        case 'string':
+          // TODO: Replace with actual LIGO version with LIGO LSP.
+          if (semver.lt(newVersion, semver.coerce('0.61.0'))) {
+            return await unsupportedVersion()
+          }
+          break
+        case 'number':
+          validateRollingRelease(version)
+          break
+      }
     }
-
-    promptLigoUpdate(ligoPath, date)
-  } else {
-    vscode.window.showErrorMessage(`Could not identify the installed LIGO version: ${data}`)
   }
+
+  async function validateRollingRelease(version: string) {
+    const commitTest = /Rolling release\nCommit SHA: [0-9a-f]{40}\nCommit Date: (.+)/
+    const commitDate = commitTest.exec(version)
+    if (commitDate && commitDate.length === 2) {
+      const date: number = Date.parse(commitDate[1])
+      if (Number.isNaN(date)) {
+        // Parse failed; not a date.
+        return
+      }
+
+      const newVersion = promptLigoUpdate(ligoPath, date)
+      switch (typeof newVersion) {
+        case 'string':
+          validateSemver(version)
+          break;
+        case 'number':
+          // TODO: Replace with actual LIGO release date with LIGO LSP.
+          // Note: month is 0-indexed
+          if (newVersion < Date.UTC(2023, 2 - 1, 14)) {
+            return await unsupportedVersion()
+          }
+          break
+      }
+    } else {
+      vscode.window.showErrorMessage(`Could not identify the installed LIGO version: ${version}`)
+    }
+  }
+
+  await validateSemver(data)
+  await validateRollingRelease(data)
 }
