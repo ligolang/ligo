4c35dea87a3ad8f126e9a31fd55482c946f17417
Anton Sorokin <anton.sorokin@serokell.io>
2023-03-22 18:07:13 +0200

    [Chore] Untyper: handle case when T_constant has no params
    Problem:
    `T_constant` is currently transformed to `T_app` by untyper
    This can create a strange term: T_app with empty arguments if
    `T_constant` had no parameters.
    
    Solution:
    Create a `T_variable` instead if a list of parameters was not empty

--- src/passes/10-checking/untyper.ml
+++ src/passes/10-checking/untyper.ml
@@ -28,7 +28,10 @@ let rec untype_type_expression (t : O.type_expression) : I.type_expression =
     let arguments = List.map ~f:self parameters in
     let type_operator = Type_var.of_input_var ~loc (Literal_types.to_string injection) in
     return
-    @@ I.T_app { type_operator = Module_access.make_el @@ type_operator; arguments }
+    @@
+    (match arguments with
+    | [] -> I.T_variable type_operator
+    | _ -> I.T_app { type_operator = Module_access.make_el @@ type_operator; arguments })
   | O.T_singleton l -> return @@ I.T_singleton l
   | O.T_abstraction x ->
     let x = Abstraction.map self x in
