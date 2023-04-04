b5bab1e43d0127e92515fbeb0307af97e6aa8507
Rémi <lesenechal.remi@gmail.com>
2023-04-04 17:23:31 +0000

    Deprecate katmandu

--- src/environment/protocols.ml
+++ src/environment/protocols.ml
@@ -1,20 +1,16 @@
-type t =
-  | Kathmandu
-  | Lima
-[@@deriving eq, compare]
+type t = Lima [@@deriving eq, compare]
 
 let current = Lima
 let in_use = Lima (* Protocol we depend on *)
 
 (* this list is used to print the list of protocols in the CLI help *)
-let protocols_str : string list = [ "kathmandu"; "lima" ]
+let protocols_str : string list = [ "lima" ]
 
 let protocols_to_variant : string -> t option =
  fun p ->
   match p with
   | "current" -> Some current
   | "lima" -> Some Lima
-  | "kathmandu" -> Some Kathmandu
   | i when not (List.exists ~f:(String.equal i) protocols_str) -> None
   | _ -> failwith "internal error: forgot to add the protocol string form to the list ?"
 
@@ -23,4 +19,3 @@ let variant_to_string : t -> string =
  fun s ->
   match s with
   | Lima -> "lima"
-  | Kathmandu -> "kathmandu"
