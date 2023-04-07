ligolang/ligo

commit 624048ba5bb4b74dfc06f820019382f300630fb4

[#1584] Migrate CI for 'tools' projects
Problem: Serokell is now moving all the work from fork to the upstream
repository. We need to run CI checks for projects from 'tools'
directory.

Solution: Migrate CI jobs from Serokell fork pipeline to the upstream
pipeline.

tools/debugger/flake.nix
@@ -14,21 +14,31 @@
     haskell-nix.inputs.hackage.follows = "hackage";
   };
 
-  outputs = { self, utils, haskell-nix, hackage }:
+  outputs = { self, utils, haskell-nix, hackage, nixpkgs }:
     let haskellSystems = (builtins.attrNames haskell-nix.legacyPackages);
     in utils.lib.eachSystem haskellSystems (system:
       let
-        pkgs = haskell-nix.legacyPackages.${system};
+        haskellPkgs = haskell-nix.legacyPackages."${system}";
+        nixpkgsArgs = {
+          overlays = [(self: super: {
+            # Hash for one of headers is broken in nixpkgs at the moment :(
+            musl = super.musl.override (_: { useBSDCompatHeaders = false; });
+          })];
+          config = { allowUnfree = true; };
+          localSystem = system;
+        };
 
-        grammars = import ./../lsp/squirrel/grammar { inherit pkgs; };
+        pkgs = import nixpkgs nixpkgsArgs;
+
+        grammars = import ./../lsp/squirrel/grammar { pkgs = haskellPkgs; };
 
         ligo-debugger-package = pkgs:
-          (pkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; }).ligo-debugger;
+          (haskellPkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; }).ligo-debugger;
 
         ligo-debugger-exec = pkgs:
           (ligo-debugger-package pkgs).components.exes.ligo-debugger;
-        ligo-debugger-components = ligo-debugger-package (pkgs);
-        ligo-debugger-test = (ligo-debugger-package pkgs).components.tests.ligo-debugger-test;
+        ligo-debugger-components = ligo-debugger-package (haskellPkgs);
+        ligo-debugger-test = (ligo-debugger-package haskellPkgs).components.tests.ligo-debugger-test;
         archOut = {
           devShells = {
             default = pkgs.mkShell rec {
@@ -41,8 +51,8 @@
 
           packages = {
 
-            ligo-debugger = ligo-debugger-exec (pkgs);
-            ligo-debugger-static = ligo-debugger-exec (pkgs.pkgsCross.musl64);
+            ligo-debugger = ligo-debugger-exec (haskellPkgs);
+            ligo-debugger-static = ligo-debugger-exec (haskellPkgs.pkgsCross.musl64);
 
             ligo-debugger-extension = pkgs.callPackage ./vscode-plugin {
               # Since we are shipping it, we want to have a portable binary

.ci/.gitlab-ci-debugger.yml
@@ -1,0 +1,129 @@
+## Debugger
+
+include:
+  - '/.ci/.gitlab-ci-tools-common.yml'
+
+# Reads ligo version from 'LIGO_VERSION_FILE' env variable,
+# gets ligo binary from the corresponding release, and does
+# 'git add ligo' to make binary available for flake.
+.get_release_ligo_binary: &get_release_ligo_binary
+  # strip all comments form the version file and grep for version
+  - ligo_version="$(cat "$LIGO_VERSION_FILE" | sed '/^#/d' | grep -E '([0-9]{1,}\.)+[0-9]{1,}')"
+  - ligo_link="$(nix-shell --packages curl jq --run
+      "curl --silent https://gitlab.com/api/v4/projects/ligolang%2Fligo/releases/"$ligo_version"/assets/links |
+      jq -r '.[] | select(.name == \"Static Linux binary\") | .direct_asset_url'")"
+  - nix-shell --packages wget --run "wget --quiet -O ligo "$ligo_link""
+  - chmod +x ligo
+  - ./ligo --version
+  - git add ./ligo
+
+# This one is supposed to be used for jobs that want to use ligo binary from
+# 'ligo-build' job and have 'needs: [ ligo-build ]'. This script basically
+# does 'git add ligo' to make binary available for flake.
+.get_latest_ligo_binary: &get_latest_ligo_binary
+  - git add ./ligo
+
+# This one runs 'ligo-debugger-test' with flags in 'LIGO_TEST_FLAGS' env variable.
+.run_ligo_debugger_tests: &run_ligo_debugger_tests
+  # Gitlab CI terminal has 'TERM=dumb' environment variable.
+  # Guys from LIGO are using pretty-printing from 'ligo-utils' library.
+  # As you can see here (https://gitlab.com/serokell/ligo/ligo/-/blob/tooling/vendors/ligo-utils/simple-utils/snippet.ml#L15)
+  # if 'is_dumb' is true then we won't see any ANSI color sequences.
+  # However, we have some tests that rely on colored output, so, we want to disable this var.
+  - unset TERM
+  - export LIGO_BINARY_PATH=$PWD/ligo
+  - cd ./tools/debugger
+  - nix build $FLAKE_FLAGS .#tests.x86_64-linux.ligo-debugger-test
+  - cd ligo-debugger && ../result/bin/ligo-debugger-test $LIGO_TEST_FLAGS
+
+debugger-danger-instant-checks:
+  stage: tools-precheck
+  rules: &debugger_danger_checks
+    - if: $CI_PIPELINE_SOURCE == "merge_request_event"
+      changes:
+        - tools/debugger/**/*
+        - danger/**/*
+  needs: []
+  interruptible: true
+  script:
+    - nix develop ./tools/debugger#ci -c danger --fail-on-errors=true
+        --dangerfile=./danger/instant-checks.rb
+        --danger_id=instant
+
+debugger-danger-hlint:
+  stage: tools-precheck
+  rules: *debugger_danger_checks
+  needs: []
+  interruptible: true
+  script:
+    - nix develop ./tools/debugger#ci -c danger --fail-on-errors=true
+        --dangerfile=./danger/hlint.rb
+        --danger_id=hlint
+
+debugger-danger-premerge-checks:
+  stage: tools-check
+  rules: *debugger_danger_checks
+  needs: ["debugger-danger-instant-checks"]  # To get messages in MRs ordered right
+  when: always
+  interruptible: true
+  script:
+    - nix develop ./tools/debugger#ci -c danger --fail-on-errors=true
+        --dangerfile=./danger/premerge-checks.rb
+        --danger_id=premerge
+  allow_failure: true
+
+debugger-build:
+  stage: tools-build
+  rules: &debugger_mr_dev
+    - !reference [.debugger_mr_changes]
+    - !reference [.dev]
+  needs: []
+  script:
+    - nix build $FLAKE_FLAGS ./tools/debugger#packages.x86_64-linux.ligo-debugger
+
+debugger-test-ligo-recommended:
+  stage: tools-check
+  rules: *debugger_mr_dev
+  needs: [debugger-build]
+  variables:
+    LIGO_VERSION_FILE: "./tools/debugger/ligo-debugger/src/resources/versions/recommended"
+    LIGO_TEST_FLAGS: "-r"
+  before_script: *get_release_ligo_binary
+  script: *run_ligo_debugger_tests
+
+debugger-test-ligo-minimal-supported:
+  stage: push
+  rules:
+    - !reference [.vscode_production]
+  needs: []
+  when: manual
+  variables:
+    LIGO_VERSION_FILE: "./tools/debugger/ligo-debugger/src/resources/versions/minimal-supported"
+    LIGO_TEST_FLAGS: "-r -m"
+  before_script: *get_release_ligo_binary
+  script: *run_ligo_debugger_tests
+  allow_failure: true
+
+vscode-debugger-extension:
+  stage: tooling
+  rules:
+    - !reference [.debugger_mr_changes]
+    - !reference [.dev]
+    - !reference [.vscode_production]
+  needs: []
+  script:
+    - nix build $FLAKE_FLAGS ./tools/debugger#ligo-debugger-extension
+    - cp -Lr --no-preserve=mode,ownership,timestamps result/*.vsix ligo-debugger-extension.vsix
+  artifacts:
+    paths:
+      - ligo-debugger-extension.vsix
+
+vscode-debugger-extension-publish:
+  stage: push
+  rules:
+    - !reference [.vscode_production]
+  needs: [vscode-debugger-extension]
+  when: manual
+  script:
+    - nix develop ./tools/vsce --command vsce publish --packagePath *.vsix # Uses VSCE_PAT env variable as personal access token
+    - nix develop ./tools/vsce --command ovsx publish *.vsix # Uses OVSX_PAT env variable as personal access token

.ci/.gitlab-ci-lsp.yml
@@ -1,0 +1,137 @@
+## LSP/Squirrel
+
+include:
+  - '/.ci/.gitlab-ci-tools-common.yml'
+
+lsp-validate-cabal-files:
+  stage: tools-precheck
+  rules:
+    - !reference [.lsp_mr_changes]
+  needs: []
+  script:
+    - cd tools/lsp/squirrel
+    - nix develop $FLAKE_FLAGS ..#ci --command scripts/validate-cabal-files.sh
+
+lsp-lint:
+  stage: tools-precheck
+  rules:
+    - !reference [.lsp_mr_changes]
+  needs: []
+  script:
+    - nix --version
+    - nix build $FLAKE_FLAGS ./tools/lsp#checks.x86_64-linux.lint
+    # Nix has some trouble with git so we run it directly from the CI
+    - cd tools/lsp
+    - bash squirrel/scripts/check-trailing-whitespace.sh
+
+lsp-stylish:
+  stage: tools-precheck
+  rules:
+    - !reference [.lsp_mr_changes]
+  needs: []
+  script:
+    - cd tools/lsp/squirrel
+    - nix develop ..#ci -c ./scripts/validate-stylish.sh
+
+lsp-build:
+  stage: tools-build
+  rules: &lsp_mr_dev
+    - !reference [.lsp_mr_changes]
+    - !reference [.dev]
+  needs: []
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#packages.x86_64-linux.squirrel
+
+lsp-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [lsp-build]
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#checks.x86_64-linux.lsp-test
+
+lsp-integration-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [docker_extract, lsp-build]
+  script:
+    - git add ./ligo # Ligo binary is needed for this tests
+    - cd tools/lsp
+    - nix build $FLAKE_FLAGS .#tests.x86_64-linux.integration-test
+    - nix develop .#integration-test -c bash -c 'cd ./squirrel && ../result/bin/integration-test'
+
+lsp-sexp-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [lsp-build]
+  script:
+    - nix build $FLAKE_FLAGS 'git+file:///'$PWD'?dir=tools/lsp'#checks.x86_64-linux.squirrel-sexp-test
+
+lsp-grammar-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [lsp-build]
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#checks.x86_64-linux.squirrel-grammar-test
+
+lsp-handler-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [docker_extract, lsp-build]
+  script:
+    - git add ./ligo # Ligo binary is needed for this tests
+    - nix build $FLAKE_FLAGS ./tools/lsp#checks.x86_64-linux.lsp-handlers-test
+
+lsp-contract-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [lsp-build]
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#checks.x86_64-linux.ligo-contracts-test
+
+vscode-lsp-extension-linux:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [lsp-build]
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#vscode-extension-native
+
+vscode-extension-docker-test:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: [docker_extract, lsp-build]
+  script:
+    - git add ./ligo # Ligo binary is needed for these tests
+    - nix build $FLAKE_FLAGS 'git+file:///'$PWD'?dir=tools/lsp'#tests.x86_64-linux.vscode-extenstion-test-docker-image
+    - docker load -i result
+    - docker run --rm vscode-extension-test
+
+vscode-extension-publish-check:
+  stage: tools-check
+  rules: *lsp_mr_dev
+  needs: []
+  script:
+    - nix flake $FLAKE_FLAGS check ./tools/vsce
+
+vscode-lsp-extension:
+  stage: tooling
+  rules:
+    - !reference [.lsp_mr_changes]
+    - !reference [.dev]
+    - !reference [.vscode_production]
+  needs: []
+  script:
+    - nix build $FLAKE_FLAGS ./tools/lsp#vscode-extension
+    - cp -Lr --no-preserve=mode,ownership,timestamps result/*.vsix ligo-extension.vsix
+  artifacts:
+    paths:
+      - ligo-extension.vsix
+
+vscode-lsp-extension-publish:
+  stage: push
+  rules:
+    - !reference [.vscode_production]
+  needs: [vscode-lsp-extension]
+  when: manual
+  script:
+    - nix develop ./tools/vsce --command vsce publish --packagePath *.vsix # Uses VSCE_PAT env variable as personal access token
+    - nix develop ./tools/vsce --command ovsx publish *.vsix # Uses OVSX_PAT env variable as personal access token

.ci/.gitlab-ci-tools-common.yml
@@ -1,0 +1,25 @@
+# Env variables and yaml anchors/references used by jobs for projects from 'tools' directory
+variables:
+  FLAKE_FLAGS: --accept-flake-config -L
+  DANGER_GITLAB_API_TOKEN: "$TOKEN_API"
+
+.dev:
+  if: $CI_COMMIT_BRANCH == "dev"
+
+.vscode_production:
+  if: $CI_COMMIT_BRANCH == "vscode-production"
+
+.lsp_mr_changes:
+  if: $CI_PIPELINE_SOURCE == "merge_request_event"
+  changes:
+    - tools/lsp/**/*
+
+.debugger_mr_changes:
+  if: $CI_PIPELINE_SOURCE == "merge_request_event"
+  changes:
+    - tools/debugger/**/*
+
+.webide_mr_changes:
+  if: $CI_PIPELINE_SOURCE == "merge_request_event"
+  changes:
+    - tools/webide-new/**/*

.ci/.gitlab-ci-webide-new.yml
@@ -1,0 +1,44 @@
+## Webide
+
+include:
+  - '/.ci/.gitlab-ci-tools-common.yml'
+
+webide-backend:
+  stage: tools-build
+  rules: &webide_mr_dev
+    - !reference [.webide_mr_changes]
+    - !reference [.dev]
+  needs: []
+  script:
+    - nix build ./tools/webide-new#packages.x86_64-linux.backend
+
+webide-frontend:
+  stage: tools-build
+  rules: *webide_mr_dev
+  needs: []
+  script:
+    - nix build ./tools/webide-new#packages.x86_64-linux.frontend
+
+webide-backend-checks:
+  stage: tools-check
+  rules: *webide_mr_dev
+  needs: [webide-backend]
+  script:
+    - cd ./tools/webide-new
+    - nix build .#packages.x86_64-linux.ligo-bin --out-link ligo
+    - nix build .#packages.x86_64-linux.tezos-client --out-link tezos-client
+    - export LIGO_PATH=$PWD/ligo/bin/ligo
+    - export OCTEZ_CLIENT_PATH=$PWD/tezos-client/bin/octez-client
+    - export DOCKER_LIGO_VERSION=0.54.1
+    # These tests need docker, so they cannot be easily run as a flake check :(
+    - nix build .#tests.x86_64-linux.ligo-webide-backend-test
+    - cd ligo-webide-backend && ../result/bin/ligo-webide-backend-test
+
+webide-frontend-checks:
+  stage: tools-check
+  rules: *webide_mr_dev
+  needs: [webide-frontend]
+  script:
+    - nix build -L ./tools/webide-new#checks.x86_64-linux.frontend-tscompile
+    - nix build -L ./tools/webide-new#checks.x86_64-linux.frontend-tslint
+    - nix build -L ./tools/webide-new#checks.x86_64-linux.frontend-openapi

.gitlab-ci.yml
@@ -7,6 +7,9 @@ stages:
   - build                           # build, test, produces binaries/.deb/doc within docker
   - post-build-action               # Post build action
   - docker-extract                  # extract what have been built and expose them as artifact
+  - tools-precheck                  # preliminary checks done for projects from 'tools'
+  - tools-build                     # build projects from 'tools'
+  - tools-check                     # run tests for projects from 'tools'
   - tooling                         # build miscellaneous tooling-related things (website, changelog..)
   - push                            # deploy docker images, pages, releases (tag)
   - deploy                          # deploy web IDE, website preview
@@ -21,6 +24,10 @@ include:
   - '/.ci/.gitlab-ci-npm.yml'
   # Manage pipeline related to webide
   - '/.ci/.gitlab-ci-webide.yml'
+  # CI jobs for projects from 'tools'
+  - '/.ci/.gitlab-ci-lsp.yml'
+  - '/.ci/.gitlab-ci-debugger.yml'
+  - '/.ci/.gitlab-ci-webide-new.yml'
 .submodule-clone:
   before_script:
     - git submodule sync --recursive
@@ -76,8 +83,9 @@ docker_build:
       when: always
     - if: '$CI_PIPELINE_SOURCE == "merge_request_event" && $CI_PROJECT_PATH == "ligolang/ligo"'
       when: always
-  dependencies:
-    - changelog
+  needs:
+    - job: changelog
+      artifacts: true
   script:
     - DOCKER_BUILDKIT=1 docker build .
       --build-arg=ci_commit_tag=$CI_COMMIT_TAG
@@ -104,8 +112,9 @@ docker_extract:
       when: always
     - if: '$CI_PIPELINE_SOURCE == "merge_request_event" && $CI_PROJECT_PATH == "ligolang/ligo"'
       when: always
-  dependencies :
-    - docker_build
+  needs:
+    - job: docker_build
+      artifacts: true
   script:
     - docker load -i ligo.tar.gz
     - docker cp $(docker create ${LIGO_IMAGE_TAG}):/root/ligo .

commit c63c353919b8b748cad588a2c8b4d3218210cdc4

[Chore] Hotfix static debugger binary build
Problem: Debugger binary bundled with VSCode extension is not statically
linked.

Solution: Update set of nix pkgs that is used to build debugger package
in 'ligo-debugger-package'

This bug was introduced in 624048ba5bb4b74dfc06f820019382f300630fb4 =/

tools/debugger/flake.nix
@@ -33,7 +33,7 @@
         grammars = import ./../lsp/squirrel/grammar { pkgs = haskellPkgs; };
 
         ligo-debugger-package = pkgs:
-          (haskellPkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; }).ligo-debugger;
+          (pkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; }).ligo-debugger;
 
         ligo-debugger-exec = pkgs:
           (ligo-debugger-package pkgs).components.exes.ligo-debugger;