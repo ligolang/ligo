f5edc629efad27df17a1399a06056f38b240fe08
Anton Sorokin <anton.sorokin@serokell.io>
2023-04-24 13:45:33 +0300

    [Chore] Remove handler.mli
    Problem:
    `mli` files require some attention to keep them up-to-date,
    and there are no obvious benefits to have a `mli` file for `handler.ml`
    
    Solution:
    Remove `handler.mli` moving all documentation to `handler.ml`.

--- src/main/ligo_lsp/requests/handler.ml
+++ src/main/ligo_lsp/requests/handler.ml
@@ -1,25 +1,32 @@
 open Linol_lwt
 open Linol_lwt.Jsonrpc2
-open Utils
-module Hashtbl = Caml.Hashtbl
+open Lsp_helpers
 
+(** Stores the configuration pertaining to the LIGO language server. *)
 type config =
   { max_number_of_problems : int
+        (** The maximum number of diagnostics to be shown. Defaults to 100. *)
   ; logging_verbosity : MessageType.t
+        (** The level of verbosity when logging. Defaults to Info. *)
   ; disabled_features : string list
+        (** Disabled requests, i.e., they are not handled by the language server. Defaults to []. *)
   ; deprecated : bool
+        (** Enable support for the deprecated PascaLIGO syntax. Defaults to [false]. *)
   }
 
+(** We can send diagnostics to user or just save them to list in case of testing *)
 type notify_back_mockable =
   | Normal of notify_back
   | Mock of Jsonrpc2.Diagnostic.t list ref (* FIXME: collect logs for tests *)
 
+(** Environment available in Handler monad *)
 type handler_env =
   { notify_back : notify_back_mockable
   ; config : config
   ; docs_cache : (DocumentUri.t, Ligo_interface.file_data) Hashtbl.t
   }
 
+(** Handler monad : allows sending messages to user and reading docs cache *)
 type 'a handler = Handler of (handler_env -> 'a IO.t)
 
 module Handler = struct
@@ -42,20 +49,26 @@ let bind (Handler d : 'a Handler.t) (f : 'a -> 'b Handler.t) : 'b Handler.t =
 
 let ( let@ ) = bind
 
+(** Handler monad is a functor *)
 let fmap (f : 'a -> 'b) (x : 'a Handler.t) : 'b Handler.t =
   let@ x' = x in
   return @@ f x'
 
 
-let fmap_to (x : 'a Handler.t) (f : 'a -> 'b) : 'b Handler.t = fmap f x
+(** We can run things from [Linol_lwt.IO] monad here *)
 let lift_IO (m : 'a IO.t) : 'a Handler.t = Handler (fun _ -> m)
+
+(** Get env inside monad computation *)
 let ask : handler_env Handler.t = Handler IO.return
+
 let ask_notify_back : notify_back_mockable Handler.t = fmap (fun x -> x.notify_back) ask
 let ask_config : config Handler.t = fmap (fun x -> x.config) ask
 
 let ask_docs_cache : (DocumentUri.t, Ligo_interface.file_data) Hashtbl.t Handler.t =
   fmap (fun x -> x.docs_cache) ask
 
+
+(** Conditional computations *)
 
 let when_ (b : bool) (m : unit Handler.t) : unit Handler.t = if b then m else return ()
 
@@ -102,6 +115,7 @@ let send_log_msg ~(type_ : MessageType.t) (s : string) : unit Handler.t =
   | Mock _ -> return ()
 
 
+(** Send diagnostics (errors, warnings) to *)
 let send_diagnostic (s : Jsonrpc2.Diagnostic.t list) : unit Handler.t =
   let@ nb = ask_notify_back in
   match nb with
@@ -111,8 +125,10 @@ let send_diagnostic (s : Jsonrpc2.Diagnostic.t list) : unit Handler.t =
     return ()
 
 
+(** Message will appear in log in case [logging_verbosity = MessageType.Log] *)
 let send_debug_msg : string -> unit Handler.t = send_log_msg ~type_:MessageType.Log
 
+(** Displays a pop-up message *)
 let send_message ?(type_ : MessageType.t = Info) (message : string) : unit Handler.t =
   let@ nb = ask_notify_back in
   match nb with
@@ -122,6 +138,11 @@ let send_message ?(type_ : MessageType.t = Info) (message : string) : unit Handl
   | Mock _ -> return ()
 
 
+(**
+Use doc info from cache. In case it's not availiable, return default value.
+Also returns default value if `get_scope` for this file fails, unless
+`return_default_if_no_info = false` was specified
+*)
 let with_cached_doc
     ?(return_default_if_no_info = true)
     (uri : DocumentUri.t)
@@ -149,6 +170,14 @@ let with_cached_doc_pure
   with_cached_doc ?return_default_if_no_info uri default f'
 
 
+(** Like with_cached_doc, but parses a CST from code. If `strict` is passed, error
+    recovery for parsing is disabled, so default arg will be returned in case of
+    any parse error.
+    `on_error` allows to display a pop-up instead of just adding a debug message
+    in case of an error. This is helpful for displaying to user why his request failed.
+    We should not display pop-up when automated (e.g. document link) request failed,
+    but should when e.g. formatting failed because of syntax error.
+*)
 let with_cst
     ?(strict = false)
     ?(on_error : string -> unit handler =
