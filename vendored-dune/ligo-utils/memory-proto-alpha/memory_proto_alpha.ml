3a9863d52ae71ac32a3219f7e4603a68ed124ccd
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-04-26 10:18:07 +0530

    trp_016

--- vendored-dune/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
+++ vendored-dune/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
@@ -1,9 +1,9 @@
 (* module Name = struct let name = "alpha" end *)
 module Proto = Tezos_protocol_016_PtMumbai
 module Alpha_environment = Tp_environment_016_PtMumbai
-module Raw_protocol = Trp_016_PtMumbai
+module Raw_protocol = Trp_016
 module Parameters = Tp_016__params
-module Client = Tezos_client_016_PtMumbai
+module Client = Tc_016
 module Test_helpers = Tezos_016_PtMumbai_test_helpers
 
 type alpha_error = Alpha_environment.Error_monad.error
