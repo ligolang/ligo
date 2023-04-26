a0279395a0066b5d879615e6966726cb32fdf07a
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-04-26 10:16:44 +0530

    Macos needs -framework Security to link in rust libs
    Fixes,
    
    Undefined symbols for architecture arm64:
      "_SecRandomCopyBytes", referenced from:
          ring::rand::darwin::fill::h74bc87b165847143 in librustzcash.a(ring-41b9a4e939f6f5d9.ring.276ce3e3-cgu.8.rcgu.o)
      "_kSecRandomDefault", referenced from:
          ring::rand::darwin::fill::h74bc87b165847143 in librustzcash.a(ring-41b9a4e939f6f5d9.ring.276ce3e3-cgu.8.rcgu.o)
    ld: symbol(s) not found for architecture arm64
    
    Use framework flags only on macos via configurator

--- src/bin/configurator.ml
+++ src/bin/configurator.ml
@@ -1,10 +1,18 @@
 module C = Configurator.V1
 
 let () =
-  C.main ~name:"" (fun _ ->
+  let f _ =
       C.Flags.write_sexp
-        "win32-flags.sexp"
+        "platform-specific-compiler-flags.sexp"
         Caml.(
           if C.ocaml_config_var_exn (C.create "") "os_type" = "Win32"
           then ["-cclib"; "-lole32"; "-cclib"; "-luserenv"; "-cclib"; "-lbcrypt"]
-          else []))
+          else []);
+      C.Flags.write_sexp
+        "platform-specific-linker-flags.sexp"
+        Caml.(
+          if C.ocaml_config_var_exn (C.create "") "system" = "macosx"
+          then ["-cclib"; "-framework Security"]
+          else [])
+  in
+  C.main ~name:"" f
