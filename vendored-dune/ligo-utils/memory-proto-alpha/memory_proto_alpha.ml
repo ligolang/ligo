cb7a8f566798394fcfd24ecbf61b2c9fedc37d76
prometheansacrifice <2601505-prometheansacrifice@users.noreply.gitlab.com>
2023-04-26 10:18:01 +0530

    tp_016_params

--- vendored-dune/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
+++ vendored-dune/ligo-utils/memory-proto-alpha/memory_proto_alpha.ml
@@ -2,7 +2,7 @@
 module Proto = Tezos_protocol_016_PtMumbai
 module Alpha_environment = Tp_environment_016_PtMumbai
 module Raw_protocol = Trp_016_PtMumbai
-module Parameters = Tezos_protocol_016_PtMumbai_parameters
+module Parameters = Tp_016__params
 module Client = Tezos_client_016_PtMumbai
 module Test_helpers = Tezos_016_PtMumbai_test_helpers
 
