504b82f96399fb4f84fc2bc0dd8c3a14d0614171
Leonid Vasilev <leonid.vasilev@serokell.io>
2023-05-04 18:56:39 +0400

    [#1759] Support `weeder` in the debugger's CI
    Problem: recently we moved `ligo-squirrel` into debugger. We tried to strip
    redundant dependencies but we could miss something. We should have
    `weeder` job in CI that fails if the debugger has dead code.
    
    Solution: support `weeder`.

--- tools/debugger/flake.nix
+++ tools/debugger/flake.nix
@@ -11,10 +11,13 @@
   inputs = {
     utils.url = "github:numtide/flake-utils";
     hackage.flake = false;
+    haskell-nix-weeder.flake = false;
     haskell-nix.inputs.hackage.follows = "hackage";
+    # This input is needed for weeder 1 compilation
+    nixpkgs-legacy.url = "github:serokell/nixpkgs?rev=1714a2ead1a18678afa3cbf75dff3f024c579061";
   };
 
-  outputs = { self, utils, haskell-nix, hackage, nixpkgs }:
+  outputs = { self, utils, haskell-nix, hackage, nixpkgs, nixpkgs-legacy, haskell-nix-weeder }:
     let haskellSystems = (builtins.attrNames haskell-nix.legacyPackages);
     in utils.lib.eachSystem haskellSystems (system:
       let
@@ -29,17 +32,31 @@
         };
 
         pkgs = import nixpkgs nixpkgsArgs;
+        pkgs-legacy = nixpkgs-legacy.legacyPackages.${system};
 
         grammars = import ./ligo-debugger/grammar { pkgs = haskellPkgs; };
 
+        weeder-hacks = import haskell-nix-weeder { pkgs = pkgs-legacy; };
+
+        ligo-debugger-stack-project = pkgs:
+          (pkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; inherit weeder-hacks; });
+
         ligo-debugger-package = pkgs:
-          (pkgs.haskell-nix.callPackage ./ligo-debugger { inherit grammars; }).ligo-debugger;
+          (ligo-debugger-stack-project pkgs).ligo-debugger;
 
         ligo-debugger-exec = pkgs:
           (ligo-debugger-package pkgs).components.exes.ligo-debugger;
         ligo-debugger-components = ligo-debugger-package (haskellPkgs);
         ligo-debugger-test = (ligo-debugger-package haskellPkgs).components.tests.ligo-debugger-test;
-        lsp-test = (ligo-debugger-package haskellPkgs).components.tests.lsp-test;
+
+        # nixpkgs has weeder 2, but we use weeder 1 because `weeder-hacks`
+        # still don't support it.
+        weeder-legacy = pkgs-legacy.haskellPackages.callHackageDirect {
+          pkg = "weeder";
+          ver = "1.0.9";
+          sha256 = "0gfvhw7n8g2274k74g8gnv1y19alr1yig618capiyaix6i9wnmpa";
+        } {};
+
         archOut = {
           devShells = {
             default = pkgs.mkShell rec {
@@ -51,6 +68,13 @@
           };
 
           packages = {
+            weeder-script = weeder-hacks.weeder-script {
+              weeder = weeder-legacy;
+              hs-pkgs = ligo-debugger-stack-project haskellPkgs;
+              local-packages = [
+                { name = "ligo-debugger"; subdirectory = "ligo-debugger"; }
+              ];
+            };
 
             ligo-debugger = ligo-debugger-exec (haskellPkgs);
             ligo-debugger-static = ligo-debugger-exec (haskellPkgs.pkgsCross.musl64);
@@ -69,7 +93,6 @@
           inherit ligo-debugger-components;
           tests = {
             inherit ligo-debugger-test;
-            inherit lsp-test;
           };
         };
       in archOut);
