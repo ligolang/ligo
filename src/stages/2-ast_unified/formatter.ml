638bf73aab3070a8b9d64e419ca310e5e3f620e4
Rémi <lesenechal.remi@gmail.com>
2023-05-23 10:23:17 +0000

    printing ast unified with or without locs

--- src/stages/2-ast_unified/formatter.ml
+++ src/stages/2-ast_unified/formatter.ml
@@ -1,12 +1,12 @@
 open Simple_utils.Display
 
-let program_ppformat ~display_format ~no_colour f p =
+let program_ppformat ~show_loc ~display_format ~no_colour f p =
   (* The [no_colour] option is provided to all [_ppformat] functions by default,
      but not needed by all of them. Remove the [ignore] if you need it. *)
-  let () = ignore no_colour in
+  ignore no_colour ;
   match display_format with
-  | Human_readable | Dev -> PP.program f p
+  | Human_readable | Dev -> PP.program ~show_loc f p
 
 
-let program_format : 'a format =
-  { pp = program_ppformat; to_json = Yojson_conv.program_to_yojson }
+let program_format ~show_loc : 'a format =
+  { pp = program_ppformat ~show_loc ; to_json = Yojson_conv.program_to_yojson }
