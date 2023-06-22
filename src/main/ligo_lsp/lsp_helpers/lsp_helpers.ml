f94cef3f80567b618a4cd5a8985c910fe73dd1a2
Iurii Kravchenko <iurii.kravchenko@serokell.io>
2023-06-22 16:26:50 +0400

    [Chore] Improve diagnostics testible pp Problem: diagnostics testible eq uses hack wich isn't shown in testible pp Solution: separate testible pp and eq with actual pp and eq. Also add a warning about testible pp

--- src/main/ligo_lsp/lsp_helpers/lsp_helpers.ml
+++ src/main/ligo_lsp/lsp_helpers/lsp_helpers.ml
@@ -38,20 +38,22 @@ module Diagnostic = struct
   include Lsp.Types.Diagnostic
 
   let pp = Helpers_pretty.pp_with_yojson yojson_of_t
+  let eq = Caml.( = )
 
-  let eq a b =
-    (* We don't want to fix the numbers of identifiers during tests, so we
-      replace things like "Variable \"_#123\" not found."
-      to "Variable \"_#N\" not found." before comparisons *)
-    let remove_underscore_numeration s =
-      { s with
-        message = Str.global_replace (Str.regexp {|_#[0-9][0-9]*|}) "_#N" s.message
-      }
-    in
-    Caml.(remove_underscore_numeration a = remove_underscore_numeration b)
+  (* We don't want to fix the numbers of identifiers during tests, so we
+    replace things like "Variable \"_#123\" not found."
+    to "Variable \"_#N\" not found." before comparisons *)
+  let remove_underscore_numeration s =
+    { s with
+      message =
+      "(* This is a testable_pp. The actual result might be slightly different. *) "
+      ^ Str.global_replace (Str.regexp {|_#[0-9][0-9]*|}) "_#N" s.message
+    }
+  let testable_pp fmt a = pp fmt (remove_underscore_numeration a)
+  let testable_eq a b =
+    eq (remove_underscore_numeration a) (remove_underscore_numeration b)
 
-
-  let testable = Alcotest.testable pp eq
+  let testable = Alcotest.testable testable_pp testable_eq
 end
 
 module Locations = struct
