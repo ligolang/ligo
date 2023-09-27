0c5cad1c4b8fad1e20b75a079f94343bf23bf15d
Laurent Canis <laurent@marigold.dev>
2023-09-27 17:15:29 -0400

    remove pascaligo from v1

--- gitlab-pages/website/src/plugins/syntax/output/theme/Syntax/SyntaxSwitch.js
+++ gitlab-pages/website/src/plugins/syntax/output/theme/Syntax/SyntaxSwitch.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
 import styles from "./styles.module.css";
+
 function SyntaxSwitch(props) {
   const [_, setState] = useState(0);
   const [isNext, setIsNext] = useState(false);
-  const location = useLocation();
-
-  // All this thing is a trick to force rerender
+  const location = useLocation(); // All this thing is a trick to force rerender
   // because it looks like there's a bug with static generation
   // so we have to trigger a render to make sure the correct value is rendered
+
   useEffect(() => {
     setState(1);
   }, []);
   useEffect(() => {
-    let hidePascaligo = !!location.pathname?.startsWith("/docs/next");
+    let hidePascaligo = !location.pathname?.startsWith("/docs/0.72.0") && !location.pathname?.startsWith("/docs/0.73.0");
     setIsNext(hidePascaligo);
   }, [location]);
   return /*#__PURE__*/React.createElement("select", {
@@ -35,4 +35,5 @@ function SyntaxSwitch(props) {
     value: "pascaligo"
   }, "PascaLIGO"));
 }
+
 export default SyntaxSwitch;
