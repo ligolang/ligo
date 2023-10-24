6528329c19686b8c5c0b6fd03e02b846d8261f7d
E. Rivas <8037031-er433@users.noreply.gitlab.com>
2023-10-24 19:32:25 +0000

    Revert "Replace protocol list with Oxford"

--- src/environment/protocols.ml
+++ src/environment/protocols.ml
@@ -1,16 +1,20 @@
-type t = Oxford [@@deriving eq, compare]
+type t =
+  | Mumbai
+  | Nairobi
+[@@deriving eq, compare]
 
-let current = Oxford
-let in_use = Oxford (* Protocol we depend on *)
+let current = Nairobi
+let in_use = Nairobi (* Protocol we depend on *)
 
 (* this list is used to print the list of protocols in the CLI help *)
-let protocols_str : string list = [ "oxford" ]
+let protocols_str : string list = [ "mumbai"; "nairobi" ]
 
 let protocols_to_variant : string -> t option =
  fun p ->
   match p with
   | "current" -> Some current
-  | "oxford" -> Some Oxford
+  | "mumbai" -> Some Mumbai
+  | "nairobi" -> Some Nairobi
   | i when not (List.exists ~f:(String.equal i) protocols_str) -> None
   | _ -> failwith "internal error: forgot to add the protocol string form to the list ?"
 
@@ -18,4 +22,5 @@ let protocols_to_variant : string -> t option =
 let variant_to_string : t -> string =
  fun s ->
   match s with
-  | Oxford -> "oxford"
+  | Mumbai -> "mumbai"
+  | Nairobi -> "nairobi"
