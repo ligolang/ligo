b611d0f04df2f9b870fc02cecb762ab7d138fbc1
Leonid Vasilev <leonid.vasilev@serokell.io>
2023-11-22 18:51:35 +0400

    [Chore] Catch unhandled exceptions in following passes
    Problem: some passes in LIGO may raise assertion/`failwith` errors.
    Unhandled exceptions in contracts break LSP for them.
    
    Solution: add `try ... with` block in following passes and catch
    all unhandled exceptions.

--- src/main/ligo_lsp/lsp_helpers/get_scope.ml
+++ src/main/ligo_lsp/lsp_helpers/get_scope.ml
@@ -228,6 +228,7 @@ let following_passes_diagnostics
     ~(raise : (Main_errors.all, Main_warnings.all) Trace.raise)
     ~(stdlib_context : Ast_typed.signature)
     ~(syntax : Syntax_types.t)
+    ~(logger : type_:Lsp.Types.MessageType.t -> string -> unit Lwt.t)
     (raw_options : Raw_options.t)
     (env : Ast_typed.signature)
     (prg : Ast_typed.declaration list)
@@ -244,83 +245,92 @@ let following_passes_diagnostics
       ~has_env_comments:false
       ()
   in
-  let contract_sig, pr_sig_items, prg =
-    match env.sig_sort with
-    | Ss_contract contract_sig ->
-      (* There is an entrypoint in a contract, so we know the storage *)
-      Some contract_sig, env.sig_items, prg
-    | Ss_module ->
-      let storage_type = "never" in
-      let context = { stdlib_context with sig_items = env.sig_items } in
-      let virtual_main_typed, _virtual_main_core =
-        make_lsp_virtual_main ~raise ~options ~context ~syntax storage_type
-      in
-      ( (match virtual_main_typed.pr_sig.sig_sort with
-        | Ss_module -> None (* Should be impossible since virtual main is an entrypoint *)
-        | Ss_contract contract_sig -> Some contract_sig)
-      , env.sig_items @ virtual_main_typed.pr_sig.sig_items
-      , prg @ virtual_main_typed.pr_module )
-  in
-  let prg : Ast_typed.program =
-    { pr_module = prg
-    ; pr_sig =
-        { sig_sort =
-            Option.value_map
-              ~default:Ast_typed.Ss_module
-              ~f:(fun x -> Ss_contract x)
-              contract_sig
-        ; sig_items = pr_sig_items
-        }
-    }
-  in
-  let errors, warnings =
-    match contract_sig with
-    | Some contract_sig ->
-      Trace.try_with
-        ~fast_fail:false
-        (fun ~raise ~catch ->
-          let prg =
-            Trace.trace ~raise Main_errors.self_ast_typed_tracer
-            @@ Self_ast_typed.all_program prg
-          in
-          List.iter
-            ~f:(fun module_path ->
-              let _ast_aggregated =
-                Ligo_compile.Of_typed.apply_to_entrypoint_with_contract_type
-                  ~raise
-                  ~options:options.middle_end
-                  prg
-                  module_path
-                  contract_sig
-              in
-              ())
-            (Ligo_compile.Of_typed.get_modules_with_entries prg);
-          catch.errors (), catch.warnings ())
-        (fun ~catch e -> e :: catch.errors (), catch.warnings ())
-    | None -> [], []
-  in
-  let ast_aggregated =
-    Ligo_compile.Of_typed.compile_expression_in_context
-      ~self_pass:true
-      ~self_program:true
-      ~raise
-      ~options:options.middle_end
-      contract_sig
-      prg
-      (Ast_typed.e_a_unit ~loc:Simple_utils.Location.dummy ())
-  in
-  let ast_expanded =
-    Ligo_compile.Of_aggregated.compile_expression ~raise ast_aggregated
-  in
-  let mini_c = Ligo_compile.Of_expanded.compile_expression ~raise ast_expanded in
-  let open Lwt.Let_syntax in
-  let%map _mich = Ligo_compile.Of_mini_c.compile_expression ~raise ~options mini_c in
-  List.iter warnings ~f:raise.warning;
-  List.iter errors ~f:raise.error
+  try
+    let contract_sig, pr_sig_items, prg =
+      match env.sig_sort with
+      | Ss_contract contract_sig ->
+        (* There is an entrypoint in a contract, so we know the storage *)
+        Some contract_sig, env.sig_items, prg
+      | Ss_module ->
+        let storage_type = "never" in
+        let context = { stdlib_context with sig_items = env.sig_items } in
+        let virtual_main_typed, _virtual_main_core =
+          make_lsp_virtual_main ~raise ~options ~context ~syntax storage_type
+        in
+        ( (match virtual_main_typed.pr_sig.sig_sort with
+          | Ss_module ->
+            None (* Should be impossible since virtual main is an entrypoint *)
+          | Ss_contract contract_sig -> Some contract_sig)
+        , env.sig_items @ virtual_main_typed.pr_sig.sig_items
+        , prg @ virtual_main_typed.pr_module )
+    in
+    let prg : Ast_typed.program =
+      { pr_module = prg
+      ; pr_sig =
+          { sig_sort =
+              Option.value_map
+                ~default:Ast_typed.Ss_module
+                ~f:(fun x -> Ss_contract x)
+                contract_sig
+          ; sig_items = pr_sig_items
+          }
+      }
+    in
+    let errors, warnings =
+      match contract_sig with
+      | Some contract_sig ->
+        Trace.try_with
+          ~fast_fail:false
+          (fun ~raise ~catch ->
+            let prg =
+              Trace.trace ~raise Main_errors.self_ast_typed_tracer
+              @@ Self_ast_typed.all_program prg
+            in
+            List.iter
+              ~f:(fun module_path ->
+                let _ast_aggregated =
+                  Ligo_compile.Of_typed.apply_to_entrypoint_with_contract_type
+                    ~raise
+                    ~options:options.middle_end
+                    prg
+                    module_path
+                    contract_sig
+                in
+                ())
+              (Ligo_compile.Of_typed.get_modules_with_entries prg);
+            catch.errors (), catch.warnings ())
+          (fun ~catch e -> e :: catch.errors (), catch.warnings ())
+      | None -> [], []
+    in
+    let ast_aggregated =
+      Ligo_compile.Of_typed.compile_expression_in_context
+        ~self_pass:true
+        ~self_program:true
+        ~raise
+        ~options:options.middle_end
+        contract_sig
+        prg
+        (Ast_typed.e_a_unit ~loc:Simple_utils.Location.dummy ())
+    in
+    let ast_expanded =
+      Ligo_compile.Of_aggregated.compile_expression ~raise ast_aggregated
+    in
+    let mini_c = Ligo_compile.Of_expanded.compile_expression ~raise ast_expanded in
+    let open Lwt.Let_syntax in
+    let%map _mich = Ligo_compile.Of_mini_c.compile_expression ~raise ~options mini_c in
+    List.iter warnings ~f:raise.warning;
+    List.iter errors ~f:raise.error
+  with
+  | exn ->
+    let msg = Exn.to_string exn in
+    let stack = Printexc.get_backtrace () in
+    logger ~type_:Error
+    @@ Format.asprintf "Unexpected exception in following passes: %s%s" msg stack
 
 
 (** Used by LSP, we're trying to get as many errors/warnings as possible *)
 let get_defs_and_diagnostics
+    ~(logger : type_:Lsp.Types.MessageType.t -> string -> unit Lwt.t)
     (raw_options : Raw_options.t)
     (code_input : BuildSystem.Source_input.code_input)
     : defs_and_diagnostics Lwt.t
@@ -359,6 +369,7 @@ let get_defs_and_diagnostics
                            ~raise
                            ~stdlib_context
                            ~syntax
+                           ~logger
                            raw_options
                            env
                            prg
