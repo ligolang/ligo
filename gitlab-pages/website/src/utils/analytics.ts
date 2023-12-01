c8ab9bad60d3894714ec660c7473bc8e41236ef9
FAMCHON Baptiste <bfamchon@gmail.com>
2023-12-01 17:07:28 +0000

    fix: add tag manager

--- gitlab-pages/website/src/utils/analytics.ts
+++ gitlab-pages/website/src/utils/analytics.ts
@@ -1,7 +1,11 @@
-import * as ReactGA from "react-ga";
+import ReactGA from "react-ga4";
+import TagManager from "react-gtm-module";
 
 export const initGA = (id: string) => {
   if (process.env.NODE_ENV === "production") {
     ReactGA.initialize(id);
+    TagManager.initialize({
+      gtmId: id,
+    });
   }
 };
