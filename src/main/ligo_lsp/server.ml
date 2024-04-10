493bfb8ed8128599a69200cda623ddde85240584
Heitor Toledo Lassarote de Paula <heitor.toledo@serokell.io>
2024-04-10 17:18:44 -0300

    [Chore] Document lsp_server
    Problem: To finish, we should document the language server class.
    
    Solution: Refactor some code where I think is useful (e.g., move some
    top-level things into the function) and add documentation to methods.

--- src/main/ligo_lsp/server.ml
+++ src/main/ligo_lsp/server.ml
@@ -3,9 +3,8 @@ module Requests = Requests
 open Lsp_helpers
 open Requests.Handler
 
-(* one env per document *)
-let get_scope_buffers : Docs_cache.t = Docs_cache.create ()
-
+(** A sensible default configuration to use in case we have obtained no configuration from
+    the LSP client. *)
 let default_config : config =
   { max_number_of_problems = 100
   ; logging_verbosity = MessageType.Info
@@ -18,16 +17,25 @@ let default_config : config =
   }
 
 
+(** We support launching the language server with all capabilities, just semantic tokens,
+    or no semantic tokens enabled, to provide better responsiveness for semantic tokens.
+    If we ever migrate to OCaml version 5 or greater, we should handle this with multicore
+    rather than launching multiple LSP servers. *)
 type capability_mode =
-  | Only_semantic_tokens
-  | All_capabilities
-  | No_semantic_tokens
-
-let default_modes : capability_mode list = [ All_capabilities; No_semantic_tokens ]
+  | Only_semantic_tokens (** Enable just semantic tokens. *)
+  | All_capabilities (** Enable all capabilities. *)
+  | No_semantic_tokens (** Enable all capabilities, except semantic tokens. *)
 
 (* OCaml object methods are recursive by default, but OCaml doesn't play nicely with
    polymorphic recursion. The callers of this function have types that interact with GADTs
    and the solution I found was to extract this into a function outside the object. *)
+
+(** If we crash, the language client (like VSCode) might spam messages notifying the user
+    that a capability has failed. To prevent this, we provide this handler which will run
+    the provided handler and prevent the LSP server from dying, and will display one crash
+    message to the user (if the [bool ref] is [false]), which may be disabled if they
+    press "Don't show again". The [bool ref] is changed to [true] if the user has clicked
+    to not display the message again. The [default] value is used in case of a crash. *)
 let try_with_crash_handler
     (type a)
     (has_caught_crash : bool ref)
@@ -67,32 +75,51 @@ let try_with_crash_handler
   in
   with_run_in_IO
   @@ fun { unlift_IO } ->
-  try%lwt
-    let%lwt result = unlift_IO action in
-    IO.return result
-  with
+  try%lwt unlift_IO action with
   | exn ->
     let%lwt () = unlift_IO @@ log_crash_and_display_message exn in
     IO.return default
 
 
-(** Lsp server class
+(** LIGO Language Server class.
 
-   This is the main point of interaction beetween the code checking documents
-   (parsing, typing, etc...), and the code of linol.
+    This is the main point of interaction beetween the code checking documents (parsing,
+    typing, etc...), and the code of linol.
 
-   The [server] class defines a method for each of the action
-   that the lsp server receives, such as opening of a document, when a document
-   changes, etc.. By default, the method predefined does nothing (or errors out ?),
-   so that users only need to override methods that they want the server to
-   actually meaningfully interpret and respond to.
-*)
+    The [server] class defines a method for each of the action that the lsp server
+    receives, such as opening of a document, when a document changes, etc.. By default,
+    the predefined method does nothing, so that users only need to override methods that
+    they want the server to actually meaningfully interpret and respond to.
+
+    The capability mode provided to this class is used in the boilerplate to initialize
+    the language server in the language clients, allowing them to separately spawn a
+    process for semantic tokens only and no semantic tokens, or both of them. See
+    {!capability_mode}. *)
 class lsp_server (capability_mode : capability_mode) =
   object (self)
     inherit Linol_lwt.Jsonrpc2.server as super
+
+    (** Used internally by linol to spawn queries. *)
     method spawn_query_handler = Linol_lwt.spawn
+
+    (** A helper list with the default modes used by most request handlers to check
+        whether they are enabled or not. *)
+    val default_modes : capability_mode list = [ All_capabilities; No_semantic_tokens ]
+
+    (** A table containing the buffers for opened documents. See [Docs_cache]. *)
+    val get_scope_buffers : Docs_cache.t = Docs_cache.create ()
+
+    (** The current language server configuration. Defaults to [default_config]. *)
     val mutable config : config = default_config
+
+    (** The received LSP client capabilities. This will be filled during the initalize
+        request. Defaults to [ClientCapabilities.create ()]. *)
     val mutable client_capabilities : ClientCapabilities.t = ClientCapabilities.create ()
+
+    (** To have better accuracy in requests, there is a need to normalize file paths to
+        remove indirections such as [.], [..], or symlinks. However, calling
+        [Path.from_absolute] is expensive, and so this table caches file paths that were
+        previously normalized to avoid excessive IO operations for normalization. *)
     val file_normalization_tbl = Hashtbl.create (module String)
 
     (** If [true], indicates that we reached a fatal [failwith] and the user has
@@ -100,18 +127,25 @@ class lsp_server (capability_mode : capability_mode) =
         user has not yet acknowledged yet. *)
     val has_caught_crash : bool ref = ref false
 
+    (** A sensible function to use for requests that use [~normalize]. This is also the
+        function that's used by [ask_normalize]. See [file_normalization_tbl]. *)
     method private normalize (file : string) : Path.t =
       Hashtbl.find_or_add file_normalization_tbl file ~default:(fun () ->
           Path.from_absolute file)
     [@@alert "-from_absolute_performance"]
 
+    (** Stores the download options for metadata checks, which will provide diagnostics
+        for storages marked as TZIP-16-compliant. *)
     val mutable metadata_download_options : Tzip16_storage.download_options = `Unspecified
 
     (** Stores the path to the last ligo.json file, if found. *)
     val last_project_dir : Path.t option ref = ref None
 
+    (** Caches the mod res used by some requests to resolve LIGO package imports. *)
     val mod_res : Preprocessor.ModRes.t option ref = ref None
 
+    (** The [Handler.t] monad is used to run requests, but the server class operates in
+        the [IO.t] monad. This method is used to run handlers. *)
     method run_handler : type a. notify_back_mockable -> a Handler.t -> a IO.t =
       fun notify_back ->
         run_handler
@@ -124,29 +158,33 @@ class lsp_server (capability_mode : capability_mode) =
           ; metadata_download_options
           }
 
+    (** Like [on_doc], but directly calls [Requests.on_doc] while handling crashes using
+        [try_with_crash_handler]. *)
     method on_doc_with_handler ~process_immediately ?changes ~version file content =
       try_with_crash_handler has_caught_crash ~default:()
       @@ Requests.on_doc ~process_immediately ?changes ~version file content
 
+    (** Processes the document cache, taking the [capability_mode] and
+        [config.diagnostics_pull_mode] into consideration to avoid doing excessive work.
+        This will use [on_doc_with_handler] to appropriately handle crashes. *)
     method on_doc
         : ?changes:TextDocumentContentChangeEvent.t list
           -> version:[ `New of Ligo_interface.document_version | `Unchanged ]
           -> Path.t
           -> string
           -> unit t =
-      match capability_mode with
-      | Only_semantic_tokens ->
+      let process_immediately =
+        match capability_mode with
         (* No need for calculating definitions/diagnostics here *)
-        self#on_doc_with_handler ~process_immediately:false
-      | All_capabilities | No_semantic_tokens ->
-        let { diagnostics_pull_mode; _ } = config in
-        (match diagnostics_pull_mode with
-        | `OnDocUpdate -> self#on_doc_with_handler ~process_immediately:true
-        | `OnDocumentLinkRequest | `OnSave ->
-          self#on_doc_with_handler ~process_immediately:false)
+        | Only_semantic_tokens -> false
+        | All_capabilities | No_semantic_tokens ->
+          (match config.diagnostics_pull_mode with
+          | `OnDocUpdate -> true
+          | `OnDocumentLinkRequest | `OnSave -> false)
+      in
+      self#on_doc_with_handler ~process_immediately
 
-    (* We now override the [on_notify_doc_did_open] method that will be called
-       by the server each time a new document is opened. *)
+    (** Handles the notification for when a document is opened. *)
     method on_notif_doc_did_open ~notify_back document ~content : unit IO.t =
       self#run_handler (Normal notify_back)
       @@
@@ -164,8 +202,7 @@ class lsp_server (capability_mode : capability_mode) =
           file
           content
 
-    (* Similarly, we also override the [on_notify_doc_did_change] method that will be called
-       by the server each time a document is changed. *)
+    (** Handles the notification for when a document is changed. *)
     method on_notif_doc_did_change
         ~notify_back
         document
@@ -178,6 +215,8 @@ class lsp_server (capability_mode : capability_mode) =
          let file = DocumentUri.to_path ~normalize document.uri in
          self#on_doc ~changes ~version:(`New document.version) file new_content
 
+    (** Decodes the provided settings from JSON into [config] from ["ligoLanguageServer"]
+        and applies the metadata download options. *)
     method decode_apply_settings
         (notify_back : Linol_lwt.Jsonrpc2.notify_back)
         (settings : Yojson.Safe.t)
@@ -189,6 +228,7 @@ class lsp_server (capability_mode : capability_mode) =
         self#decode_and_store_ligo_language_server_config ligo_language_server);
       self#apply_metadata_options notify_back
 
+    (** Decodes the provided settings from JSON into [config] from ["ligoLanguageServer"]. *)
     method decode_and_store_ligo_language_server_config
         (ligo_language_server : Yojson.Safe.t)
         : unit =
@@ -249,6 +289,9 @@ class lsp_server (capability_mode : capability_mode) =
                | _ -> default_config.metadata_checks_download_timeout_sec)
            }
 
+    (** Applies the metadata download options. Warns the user if
+        [config.diagnostics_pull_mode] is [`OnDocumentLinkRequest] and that request is
+        disabled. *)
     method apply_metadata_options
         (notify_back : Linol_lwt.Jsonrpc2.notify_back)
         : unit IO.t =
@@ -279,6 +322,9 @@ class lsp_server (capability_mode : capability_mode) =
       in
       IO.return ()
 
+    (** Handles the notification for when the language client requests the language server
+        to initialize. Will decode and apply the configuration provided by the client and
+        cache the supported client capabilities. *)
     method! on_req_initialize
         ~(notify_back : Linol_lwt.Jsonrpc2.notify_back)
         (init_params : InitializeParams.t)
@@ -299,6 +345,9 @@ class lsp_server (capability_mode : capability_mode) =
       client_capabilities <- init_params.capabilities;
       super#on_req_initialize ~notify_back init_params
 
+    (** Checks whether the provided method name is a supported semantic tokens method
+        ([true]) or not ([false]). Returns [false] if the provided string is not a
+        semantic tokens method. *)
     method private is_supported_semantic_tokens : string -> bool =
       function
       | "textDocument/semanticTokens"
@@ -306,6 +355,8 @@ class lsp_server (capability_mode : capability_mode) =
       | "textDocument/semanticTokens/full" -> true
       | "textDocument/semanticTokens/full/delta" | _ -> false
 
+    (** Checks whether the provided method name refers to a method that is enabled
+        ([true]) or not ([false]). *)
     method is_request_enabled (req : string) : bool =
       match self#is_supported_semantic_tokens req, capability_mode with
       | false, (All_capabilities | No_semantic_tokens)
@@ -317,32 +368,44 @@ class lsp_server (capability_mode : capability_mode) =
        state associated to the file from the global hashtable state, to avoid
        leaking memory. We should also think about clearing diagnostics.
        Handle me with #1657. *)
+
+    (** Handles the notification for when a document is closed. *)
     method on_notif_doc_did_close ~notify_back:_ _ : unit IO.t = Linol_lwt.return ()
+
+    (** Declares support for hovers, if enabled. *)
     method! config_hover = Some (`Bool (self#is_request_enabled "textDocument/hover"))
 
+    (** Declares support for formatting, if enabled. *)
     method config_formatting =
       Some (`Bool (self#is_request_enabled "textDocument/formatting"))
 
+    (** Declares support for range formatting, if enabled. *)
     method config_range_formatting =
       Some (`Bool (self#is_request_enabled "textDocument/rangeFormatting"))
 
+    (** Declares support for go to declaration, if enabled. *)
     method config_declaration =
       Some (`Bool (self#is_request_enabled "textDocument/declaration"))
 
+    (** Declares support for go to definition, if enabled. *)
     method! config_definition =
       Some (`Bool (self#is_request_enabled "textDocument/definition"))
 
+    (** Declares support for go to implementation, if enabled. *)
     method config_implementation =
       Some (`Bool (self#is_request_enabled "textDocument/implementation"))
 
+    (** Declares support for document link, if enabled. *)
     method config_document_link_provider =
       if self#is_request_enabled "textDocument/documentLink"
       then Some (DocumentLinkOptions.create ())
       else None
 
+    (** Declares support for document symbol, if enabled. *)
     method! config_symbol =
       Some (`Bool (self#is_request_enabled "textDocument/documentSymbol"))
 
+    (** Declares support for rename and prepare rename, if enabled. *)
     method config_rename =
       if self#is_request_enabled "textDocument/rename"
       then (
@@ -354,18 +417,24 @@ class lsp_server (capability_mode : capability_mode) =
         Some (`RenameOptions rename_options))
       else Some (`Bool false)
 
+    (** Declares support for references, if enabled. *)
     method config_references =
       Some (`Bool (self#is_request_enabled "textDocument/references"))
 
+    (** Declares support for type definition, if enabled. *)
     method config_type_definition =
       Some (`Bool (self#is_request_enabled "textDocument/typeDefinition"))
 
+    (** Declares support for document highlight, if enabled. *)
     method config_highlight =
       Some (`Bool (self#is_request_enabled "textDocument/documentHighlight"))
 
+    (** Declares support for folding range, if enabled. *)
     method config_folding_range =
       Some (`Bool (self#is_request_enabled "textDocument/foldingRange"))
 
+    (** Declares support for completions, if enabled, and registers [.] and [@] as
+        additional trigger characters. *)
     method! config_completion =
       if self#is_request_enabled "textDocument/completion"
       then (
@@ -378,6 +447,8 @@ class lsp_server (capability_mode : capability_mode) =
         Some completion_options)
       else None
 
+    (** Declares support for semantic tokens (full and range), if enabled, and registers
+        supported token modifiers and token types. *)
     method config_semantic_tokens =
       if self#is_request_enabled "textDocument/semanticTokens"
       then (
@@ -401,6 +472,8 @@ class lsp_server (capability_mode : capability_mode) =
         | No_semantic_tokens -> None)
       else None
 
+    (** Declares support for execute command, if enabled, and registers supported
+        commands. *)
     method config_execute_command =
       (* Commands is an internally used thing, so handling all commands without
          looking at disabled features. However, we don't want "semantic tokens"
@@ -410,23 +483,24 @@ class lsp_server (capability_mode : capability_mode) =
       let commands =
         match capability_mode with
         | All_capabilities | No_semantic_tokens ->
-          (* [id] doesn't use [~normalize], so using [Path.from_absolute] here is fine. *)
           Requests.Commands.Ligo_lsp_commands.
-            [ (add_tzip16_attr ~normalize:Path.from_absolute).id ]
+            [ (add_tzip16_attr ~normalize:self#normalize).id ]
         | Only_semantic_tokens -> []
       in
       Some (ExecuteCommandOptions.create ~commands ())
-    [@@alert "-from_absolute_performance"]
 
+    (** Declares support for code lens, if enabled. *)
     method config_code_lens =
       Some
         (CodeLensOptions.create
            ~resolveProvider:(self#is_request_enabled "textDocument/codeLens")
            ())
 
+    (** Declares support for inlay hints, if enabled. *)
     method config_inlay_hint =
       Some (`Bool (self#is_request_enabled "textDocument/inlayHint"))
 
+    (** Provides the supported server capabilities as well as their configurations. *)
     method! config_modify_capabilities (c : ServerCapabilities.t) : ServerCapabilities.t =
       { c with
         hoverProvider = self#config_hover
@@ -449,6 +523,10 @@ class lsp_server (capability_mode : capability_mode) =
       ; codeLensProvider = self#config_code_lens
       }
 
+    (** Handles a LSP notification from the client to the server. After the
+        server is initialized, it will dynamically register file watchers for
+        LIGO files as well as the project file. It will also dynamically
+        register a watcher for configuration changes. *)
     method! on_notification
         : notify_back:(Server_notification.t -> unit IO.t)
           -> server_request:Linol_lwt.Jsonrpc2.send_request
@@ -657,6 +735,11 @@ class lsp_server (capability_mode : capability_mode) =
             @@ process_doc path)
         | notification -> super#on_notification ~notify_back ~server_request notification
 
+    (** Handles a LSP request from the client to the server. This method will
+        avoid running methods that have been disabled by the user, returning a
+        default value (like [[]] or [None]). It will also ensure that we are
+        working with the correct project root, recomputing the document and its
+        caches in case it was changed. *)
     method! on_request
         : type r.  notify_back:(Server_notification.t -> unit IO.t)
                   -> server_request:Linol_lwt.Jsonrpc2.send_request
