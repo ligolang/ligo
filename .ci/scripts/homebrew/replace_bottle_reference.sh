3f4106d726b79277d905b9e0d24e29d762e02ffb
Jasbir Dhillon <jasbir.dhillon@trili.tech>
2024-06-20 11:12:12 +0000

    Feat/update gitlab runners

--- .ci/scripts/homebrew/replace_bottle_reference.sh
+++ .ci/scripts/homebrew/replace_bottle_reference.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i "s/# bottle ventura/${BOTTLE_INFO_I7}/" HomebrewFormula/ligo.rb
-sed -i "s/# bottle arm64_ventura/${BOTTLE_INFO_M1}/" HomebrewFormula/ligo.rb
+sed -i "s/# bottle sonoma/${BOTTLE_INFO_I7}/" HomebrewFormula/ligo.rb
+sed -i "s/# bottle arm64_sonoma/${BOTTLE_INFO_M1}/" HomebrewFormula/ligo.rb
