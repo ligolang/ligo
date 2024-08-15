6eb840c3e7bfb44722cfd93dfadf092d3b39e367
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2024-08-15 16:13:23 +0200

    Refactoring of tree-sitter-typescript

--- lib/tree_sitter_typescript/api_funcs_desc.ml
+++ lib/tree_sitter_typescript/api_funcs_desc.ml
@@ -1,8 +1,9 @@
 open Ctypes
-open Tree_sitter.Api_types
 
-module Functions (S : FOREIGN) = struct
-  open S
+let ts_language = Tree_sitter.Api_types.ts_language
+
+module Functions (F: FOREIGN) = struct
+  open F
 
   let tree_sitter_typescript =
     foreign "tree_sitter_typescript" (void @-> returning (ptr ts_language))
