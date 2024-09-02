451e8b9c93a93945ede8b3b0d039d306df0ad5ec
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2024-09-02 12:11:54 +0200

    Parsing from the CLI.

--- lib/typescript_ast/decoder_main.ml
+++ lib/typescript_ast/decoder_main.ml
@@ -7,30 +7,40 @@ module Tree = Cst_shared.Tree
 module TS_types = Tree_sitter.Api.Types
 module TS_fun   = Tree_sitter.Api.Functions
 
-(* Example *)
+(* Parsing *)
 
-let code = "[1, null]; f(2)"
+let parse input =
+  (* Parsing the code *)
 
-(* Parsing the code *)
+  let tree : Decoder.ts_tree_ptr =
+    Decoder.parse_typescript_string input in
 
-let tree : Decoder.ts_tree_ptr =
-  Decoder.parse_typescript_string code
+  (* Getting ahold of the root *)
 
-(* Getting ahold of the root *)
+  let program_node : Decoder.ts_tree = TS_fun.ts_tree_root_node tree in
 
-let program_node : Decoder.ts_tree = TS_fun.ts_tree_root_node tree
+  (* Empty state for building the AST *)
 
-(* Empty state for building the AST *)
+  let buffer = Buffer.create 1023 in
 
-let buffer = Buffer.create 1023
+  let state =
+    Tree.mk_state ~buffer ~regions:false ~layout:true ~offsets:true `Byte in
 
-let state =
-  Tree.mk_state ~buffer ~regions:false ~layout:true ~offsets:true `Byte
+  (* Decoding the CST into an AST in [state] *)
 
-(* Decoding the CST into an AST in [state] *)
+  let () = Decoder.print_program state program_node in
 
-let () = Decoder.print_program state program_node
+  (* Printing the AST in [state] *)
 
-(* Printing the AST in [state] *)
+  Printf.printf "%s%!" (Buffer.contents @@ Tree.to_buffer state)
 
-let () = Printf.printf "%s%!" (Buffer.contents @@ Tree.to_buffer state)
+(* Reading the input TypeScript, parsing and printing the AST *)
+
+open Core
+
+let cli_args : string array = Sys.get_argv ()
+
+let () =
+  match Array.length cli_args with
+    2 -> parse @@ In_channel.read_all cli_args.(1)
+  | _ -> prerr_endline ("Usage: " ^ cli_args.(0) ^ " [file]")
