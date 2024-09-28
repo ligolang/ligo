77e7f1f564d59a5643e559a97bbe29d49c1fa371
Savely Krendelhoff <alicecandyom@proton.me>
2024-09-28 08:57:19 +0700

    [#1996] Implement ligodep for cameligo

--- src/stages/4-ast_core/ast_core.ml
+++ src/stages/4-ast_core/ast_core.ml
@@ -4,6 +4,7 @@ module Formatter = Formatter
 module Combinators = Combinators
 module Helpers = Helpers
 module Ligo_dep_jsligo = Ligo_dep_jsligo
+module Ligo_dep_cameligo = Ligo_dep_cameligo
 
 module Misc = struct
   include Misc
