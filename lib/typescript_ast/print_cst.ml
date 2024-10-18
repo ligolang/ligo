7ff6e8105213a4745309128183121bf43bd84aa3
Christian Rinderknecht <Christian.Rinderknecht@tezcore.com>
2024-10-18 19:29:12 +0200

    Added reading and printing of lexemes.

--- lib/typescript_ast/print_cst.ml
+++ lib/typescript_ast/print_cst.ml
@@ -9,6 +9,30 @@ open Ts_wrap
 let get_region : (ts_tree -> Region.t) ref =
   ref (fun _ -> failwith "Internal error: Print_cst.get_region")
 
+(* Input channel for reading lexemes *)
+
+let in_channel : In_channel.t option ref = ref None
+
+let close_input () =
+  match !in_channel with
+  | None -> ()
+  | Some channel -> In_channel.close channel
+
+(* Reading in the source a lexeme of a given region *)
+
+let read_region region : string =
+  let start_pos, stop_pos = region#byte_pos in
+  let start_cnum = start_pos.Lexing.pos_cnum
+  and stop_cnum = stop_pos.Lexing.pos_cnum in
+  let len = stop_cnum - start_cnum in
+  let buf = Bytes.create len in
+  match !in_channel with
+  | None -> ""
+  | Some in_chan ->
+    In_channel.seek in_chan (Int64.of_int_exn start_cnum);
+    let (_ : int) = In_channel.input in_chan ~buf ~pos:0 ~len (* 0 is EOF *) in
+    Bytes.to_string buf
+
 (* To print the AST in ASCII art *)
 
 module Tree = Cst_shared.Tree
@@ -24,14 +48,18 @@ let tree_of_named_children state node printer =
   let children = collect_named_children node in
   tree_of_list state node printer children
 
-let make_node state node =
-  let region = !get_region node in
-  Tree.make_node ~region state @@ string_of_ts_node_type node
-
 let make_unary state root printer child =
   let region = !get_region root
   and label = string_of_ts_node_type root in
   Tree.make_unary ~region state label printer child
+
+let make_node state node =
+  let region = !get_region node in
+  let lexeme = read_region region in
+  make_unary state node Tree.make_node lexeme
+(*  Printf.eprintf "lexeme=%S\n%!" lexeme;
+    Tree.make_node ~region state @@ string_of_ts_node_type node
+*)
 
 let mk_child_opt = Tree.mk_child_opt
 let mk_child = Tree.mk_child
@@ -86,6 +114,8 @@ let match_rest state node print_default =
 (* Printing the CST *)
 
 let rec print_program file map node =
+  (* Opening a read channel for lexemes *)
+  let () = in_channel := Some (In_channel.create file) in
   (* Setting up the extracting of source regions *)
   let () = get_region := Ts_wrap.get_region file map in
   (* Empty state for building the AST *)
@@ -93,6 +123,9 @@ let rec print_program file map node =
   let state = Tree.mk_state ~buffer ~regions:true ~layout:true ~offsets:true `Byte in
   (* Decoding the CST into an AST in [state] *)
   let () = tree_of_named_children state node print_statement in
+  (* Closing the input channel for reading lexemes *)
+  let () = close_input () in
+  (* Making the output string *)
   Buffer.contents @@ Tree.to_buffer state
 
 (* Statements
