73c198bbb0313a65ea07eb71614cc3e02170cd1f
Savely Krendelhoff <alicecandyom@proton.me>
2024-10-29 18:31:04 +0700

    [#1997] Document cmo and cmi modules

--- src/passes/10-checking/cmo.mli
+++ src/passes/10-checking/cmo.mli
@@ -1,12 +1,17 @@
+(** Record representing cmo file contents *)
 type t =
   { impl : Ast_typed.module_
   }
 [@@deriving bin_io]
 
 module Serialized : sig
+  (** Reads file in provided path and tries to parse it as cmo *)
   val input : Filename.t -> t option
+  (** Outputs cmo object in provided path *)
   val output : t -> Filename.t -> unit
+  (** Checks if file on provided path contains serialized cmo *)
   val is_cmo : Filename.t -> bool
-  (** Makes cmo path from original file path *)
+  (** Makes cmi path from original file path
+      "src/test/contracts/id.mligo" -> "src/test/contracts/id.cmo" *)
   val of_file_name : Filename.t -> Filename.t
 end
