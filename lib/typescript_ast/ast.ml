ddd5fa46fa763cc3ab2d2ca9c85c68adfd9e8184
Christian Rinderknecht <rinderknecht@free.fr>
2024-12-01 17:45:22 +0100

    Added tuple type.

--- lib/typescript_ast/ast.ml
+++ lib/typescript_ast/ast.ml
@@ -2603,16 +2603,16 @@ and template_type =
      rest_type: $ => seq('...', $.type)
     ]}
 *)
-and tuple_type = tuple_type_member list
+and tuple_type = tuple_type_member list brackets
 
 and tuple_type_member =
   | Tuple_parameter of tuple_parameter
   | Tuple_optional_parameter of optional_tuple_parameter
   | Tuple_optional_type of type_expr
   | Tuple_rest_type of type_expr
-  | Type_type of type_expr
+  | Tuple_type of type_expr
 
-and tuple_parameter = tuple_parameter_name * type_annotation
+and tuple_parameter = tuple_parameter_name * sym_qmark * type_annotation
 
 and tuple_parameter_name =
   | Tuple_parameter_ident of identifier
