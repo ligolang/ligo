91530fbba8ac53d74de77ad41c448ccf04546da8
Savely Krendelhoff <alicecandyom@proton.me>
2024-12-09 12:32:10 +0700

    [#1997] Improve ligo dep jsligo

--- src/main/build/ligo_dep_jsligo.ml
+++ src/main/build/ligo_dep_jsligo.ml
@@ -1,19 +1,27 @@
 open Core
 open Simple_utils
+module Deps = Set.Make (Filename)
+module Locations = Map.Make (Filename)
 
 let dependencies prg =
-  let rec f decl =
+  let rec f decl (set, locations) =
     let Location.{ wrap_content = decl; location = loc } = decl in
     match decl with
     | Ast_core.D_module { module_; _ } ->
       (match Location.unwrap module_ with
-      | M_struct decls -> List.concat_map decls ~f
-      | _ -> [])
+      | M_struct decls -> List.fold_right ~init:(set, locations) decls ~f
+      | _ -> set, locations)
     | D_import import_decl ->
       (match import_decl with
-      | Import_rename _ -> []
-      | Import_all_as { module_str; _ } -> [ Location.wrap ~loc module_str ]
-      | Import_selected { module_str; _ } -> [ Location.wrap ~loc module_str ])
-    | _ -> []
+      | Import_rename _ -> set, locations
+      | Import_all_as { module_str; _ } ->
+        Set.add set module_str, Map.set locations ~key:module_str ~data:loc
+      | Import_selected { module_str; _ } ->
+        Set.add set module_str, Map.set locations ~key:module_str ~data:loc)
+    | _ -> set, locations
   in
-  List.concat_map prg ~f
+  let set, locations = List.fold_right ~init:(Deps.empty, Locations.empty) prg ~f in
+  set
+  |> Set.to_list
+  |> List.map ~f:(fun module_str ->
+         Location.wrap ~loc:(Map.find_exn locations module_str) module_str)
