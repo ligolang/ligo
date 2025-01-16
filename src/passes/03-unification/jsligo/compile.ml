d2e044502b12d2fa4242bd94f906e63cf39d1266
Christian Rinderknecht <rinderknecht@free.fr>
2025-01-16 12:04:59 +0100

    Compiled P_bytes to unified AST.

--- src/passes/03-unification/jsligo/compile.ml
+++ src/passes/03-unification/jsligo/compile.ml
@@ -1035,11 +1035,13 @@ let pattern' (pattern : Eq'.pattern) : Folding'.pattern =
       | Element pattern -> O.{ pattern; ellipsis = false }
     in
     return @@ O.P_tuple_with_ellipsis (List.map ~f value)
+  | P_bytes pattern ->
+     let bytes = Hex.to_bytes (snd pattern#payload)
+     in return @@ O.P_literal (Literal_bytes bytes)
   | _ -> failwith "TODO: pattern'"
 
 
 (*
-  | P_bytes of bytes_literal (* 0xFFFA *)
   | P_false of Region.t (* false *)
   | P_int of int_literal (* 42 *)
   | P_object of pattern _object (* {x, y:z} *)
