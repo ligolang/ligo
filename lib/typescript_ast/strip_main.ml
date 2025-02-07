03b07c5523a2ebdd8a45a59d98ff3541d5e7e2cd
Christian Rinderknecht <rinderknecht@free.fr>
2025-02-07 13:23:09 +0100

    Formatting

--- lib/typescript_ast/strip_main.ml
+++ lib/typescript_ast/strip_main.ml
@@ -45,11 +45,11 @@ let () =
     (match Loc_map.scan file with
     | Ok line_map ->
       (match parse file line_map with
-       | Error msg -> Printf.eprintf "Error: %s\n%!" msg
-       | Ok ast ->
-          match Strip.statements ast with
-          | Ok _ -> Printf.printf "Stripped.\n%!"
-          | Error { region; value } ->
-             Printf.eprintf "Error: %s\n%s\n%!" value (region#compact `Byte))
+      | Error msg -> Printf.eprintf "Error: %s\n%!" msg
+      | Ok ast ->
+        (match Strip.statements ast with
+        | Ok _ -> Printf.printf "Stripped.\n%!"
+        | Error { region; value } ->
+          Printf.eprintf "Error: %s\n%s\n%!" value (region#compact `Byte)))
     | Error { region; value = _ } -> Printf.eprintf "Error: %s\n%!" (region#compact `Byte))
   | _ -> prerr_endline ("Usage: " ^ cli_args.(0) ^ " [file]")
