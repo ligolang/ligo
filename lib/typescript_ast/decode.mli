367619e1d948245922b9070f9c9a8ef98436a46a
Christian Rinderknecht <rinderknecht@free.fr>
2025-02-25 17:45:58 +0100

    Formatting

--- lib/typescript_ast/decode.mli
+++ lib/typescript_ast/decode.mli
@@ -5,24 +5,24 @@ module Ast = Typescript_ast.Ast
 
 (* Decoding an input TypeScript program into an AST *)
 
-val dec_program :
-    ?debug_arg:bool ->
-    filename:string ->
-    file:string ->
-    Loc_map.t ->
-    Ts_wrap.ts_tree ->
-    (Ast.t, string Region.reg) result
+val dec_program
+  :  ?debug_arg:bool
+  -> filename:string
+  -> file:string
+  -> Loc_map.t
+  -> Ts_wrap.ts_tree
+  -> (Ast.t, string Region.reg) result
 
 (* The parameter [node] is the root of a Typescript CST, *not of an
    expression*. That's why we have to find the expression below the
    root. This is because tree-sitter does not provide the generated
    parsers with multiple entry-points. *)
 
-val dec_standalone_expression :
-  file:string ->
-  Loc_map.t ->
-  Ts_wrap.ts_tree ->
-  (Ast.expression, string Region.reg) result
+val dec_standalone_expression
+  :  file:string
+  -> Loc_map.t
+  -> Ts_wrap.ts_tree
+  -> (Ast.expression, string Region.reg) result
 
 (* The parameter [node] is the root of a Typescript CST, *not of a
    type expression*. tree-sitter does not provide the generated
@@ -31,7 +31,7 @@ val dec_standalone_expression :
    so we fetch the type in the produced CST (last child of the root,
    which is an type_alias_declaration). *)
 
-val dec_standalone_type_expr :
-  Loc_map.t ->
-  Ts_wrap.ts_tree ->
-  (Ast.type_expr, string Region.reg) result
+val dec_standalone_type_expr
+  :  Loc_map.t
+  -> Ts_wrap.ts_tree
+  -> (Ast.type_expr, string Region.reg) result
