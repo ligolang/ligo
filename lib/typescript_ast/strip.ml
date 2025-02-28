1f2b2855a3be4e1b40b0f93a24875fb57aadf972
Christian Rinderknecht <rinderknecht@free.fr>
2025-02-28 16:22:11 +0100

    Restricted match rhs to an expression.

--- lib/typescript_ast/strip.ml
+++ lib/typescript_ast/strip.ml
@@ -2369,7 +2369,10 @@ and filter_match_clause (node : S.expr S.property reg) : (S.match_clause, _) res
        | [ parameter ] -> Ok (Some parameter)
        | _ :: param_2 :: _ ->
           mk_err Match_filter param_2.region in
-     let match_rhs = fun_body in
+     let* match_rhs =
+       match fun_body with
+       | S.Expr_body expr -> Ok expr
+       | Stmt_body stmts -> mk_err Match_clause_rhs stmts.region in
      Ok S.{ constructor; filter; match_rhs }
   | _ -> mk_err Match_clause_rhs node.region
 
