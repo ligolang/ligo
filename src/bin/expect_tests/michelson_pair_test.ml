40508be5244afbeff001cb93024017acf26538cc
Christian Rinderknecht <rinderknecht@free.fr>
2025-03-19 17:00:57 +0100

    More test contracts upgraded.

--- src/bin/expect_tests/michelson_pair_test.ml
+++ src/bin/expect_tests/michelson_pair_test.ml
@@ -39,24 +39,6 @@ let%expect_test _ =
   run_ligo_good [ "compile"; "contract"; contract "michelson_pair_tree.jsligo" ];
   [%expect
     {|
-    File "../../test/contracts/michelson_pair_tree.jsligo", line 10, characters 28-33:
-      9 | @entry
-     10 | const main = (action: unit, store: storage): return_ => {
-                                      ^^^^^
-     11 |   let foo = [3, [1, 2 as nat]];
-    :
-    Warning: unused variable "store".
-    Hint: replace it by "_store" to prevent this warning.
-
-    File "../../test/contracts/michelson_pair_tree.jsligo", line 10, characters 14-20:
-      9 | @entry
-     10 | const main = (action: unit, store: storage): return_ => {
-                        ^^^^^^
-     11 |   let foo = [3, [1, 2 as nat]];
-    :
-    Warning: unused variable "action".
-    Hint: replace it by "_action" to prevent this warning.
-
     { parameter unit ;
       storage (pair (int %three) (pair %four (int %one) (nat %two))) ;
       code { DROP ;
@@ -67,37 +49,3 @@ let%expect_test _ =
              PAIR ;
              NIL operation ;
              PAIR } } |}]
-
-(*
-let%expect_test _ =
-  run_ligo_good
-    [ "compile"; "contract"; contract "michelson_pair_tree_intermediary.ligo" ];
-  [%expect
-    {|
-    File "../../test/contracts/michelson_pair_tree_intermediary.ligo", line 6, characters 21-27:
-      5 |
-      6 | function main (const action : unit; const store : storage) : return is {
-      7 |   const foo : storage = ("foo",(1,2n)) ;
-    :
-    Warning: unused variable "action".
-    Hint: replace it by "_action" to prevent this warning.
-
-    File "../../test/contracts/michelson_pair_tree_intermediary.ligo", line 6, characters 42-47:
-      5 |
-      6 | function main (const action : unit; const store : storage) : return is {
-      7 |   const foo : storage = ("foo",(1,2n)) ;
-    :
-    Warning: unused variable "store".
-    Hint: replace it by "_store" to prevent this warning.
-
-    { parameter unit ;
-      storage (pair (string %three) (int %one) (nat %two)) ;
-      code { DROP ;
-             PUSH nat 2 ;
-             PUSH int 1 ;
-             PAIR ;
-             PUSH string "foo" ;
-             PAIR ;
-             NIL operation ;
-             PAIR } } |}]
-*)
