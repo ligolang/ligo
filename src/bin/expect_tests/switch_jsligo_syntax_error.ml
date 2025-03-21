51b6652aa7bc183d61d57dd7074a26403101df97
Christian Rinderknecht <rinderknecht@free.fr>
2025-03-21 13:00:23 +0100

    Upgraded switch tests.

--- src/bin/expect_tests/switch_jsligo_syntax_error.ml
+++ src/bin/expect_tests/switch_jsligo_syntax_error.ml
@@ -7,35 +7,15 @@ let%expect_test _ =
     ; "../../test/contracts/negative/switch_jsligo/empty_switch.jsligo"
     ];
   [%expect
-    {|
-    File "../../test/contracts/negative/switch_jsligo/empty_switch.jsligo", line 4, characters 4-5:
-      3 |     switch (n) {
-      4 |     };
-              ^
-      5 |     output = output + "World";
-    Ill-formed switch.
-    At this point, one of the following is expected:
-      * the keyword 'case' introducing a new case;
-      * the keyword 'default' introducing the default case. |}]
-
-let%expect_test _ =
-  run_ligo_bad
-    [ "print"
-    ; "ast-core"
-    ; "../../test/contracts/negative/switch_jsligo/default_in_between.jsligo"
-    ];
-  [%expect
-    {|
-    File "../../test/contracts/negative/switch_jsligo/default_in_between.jsligo", line 10, characters 6-10:
-      9 |         output = output + "###";
-     10 |       case 3:
-                ^^^^
-     11 |         output = output + "@@@";
-    Ill-formed switch.
-    At this point, if the case is complete, one of the following is
-    expected:
-      * another case (starting by 'case' or 'default');
-      * a closing brace '}' is no more cases. |}]
+      {|
+File "../../test/contracts/negative/switch_jsligo/empty_switch.jsligo", line 3, character 13 to line 4, character 3:
+  2 |   let output = "Hello";
+  3 |   switch (n) {
+                   ^
+  4 |   };
+      ^^^
+  5 |   output = output + "World";
+Empty switches are not supported in JsLIGO. |}]
 
 let%expect_test _ =
   run_ligo_bad
@@ -45,16 +25,12 @@ let%expect_test _ =
     ];
   [%expect
     {|
-    File "../../test/contracts/negative/switch_jsligo/more_than_one_default.jsligo", line 14, characters 6-13:
-     13 |         output = output + "###";
-     14 |       default:
-                ^^^^^^^
-     15 |         output = output + "***";
-    Ill-formed switch.
-    At this point, if the case is complete, one of the following is
-    expected:
-      * another case (starting by 'case' or 'default');
-      * a closing brace '}' is no more cases. |}]
+File "../../test/contracts/negative/switch_jsligo/more_than_one_default.jsligo", line 9, characters 4-37:
+  8 |     default: output = output + "###";
+  9 |     default: output = output + "***";
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ 10 |     };
+Multiple default switch cases are not supported in JsLIGO. |}]
 
 let%expect_test _ =
   run_ligo_bad
@@ -64,13 +40,13 @@ let%expect_test _ =
     ];
   [%expect
     {|
-    File "../../test/contracts/negative/switch_jsligo/break_outside_case1.jsligo", line 3, characters 4-9:
-      2 |     let output = "Hello";
-      3 |     break;
-              ^^^^^
-      4 |     return output;
+File "../../test/contracts/negative/switch_jsligo/break_outside_case1.jsligo", line 3, characters 2-7:
+  2 |   const output = "Hello";
+  3 |   break;
+        ^^^^^
+  4 |   return output;
 
-    Break statement is not supported in that position |}]
+Break statement is not supported in that position |}]
 
 let%expect_test _ =
   run_ligo_bad
@@ -80,13 +56,13 @@ let%expect_test _ =
     ];
   [%expect
     {|
-    File "../../test/contracts/negative/switch_jsligo/break_outside_case2.jsligo", line 4, characters 8-13:
-      3 |     if (output == "") {
-      4 |         break;
-                  ^^^^^
-      5 |     } else {
+File "../../test/contracts/negative/switch_jsligo/break_outside_case2.jsligo", line 3, characters 20-25:
+  2 |   let output = "Hello";
+  3 |   if (output == "") break; else output = output + " World";
+                          ^^^^^
+  4 |   return output;
 
-    Break statement is not supported in that position |}]
+Break statement is not supported in that position |}]
 
 let%expect_test _ =
   run_ligo_bad
@@ -96,13 +72,13 @@ let%expect_test _ =
     ];
   [%expect
     {|
-    File "../../test/contracts/negative/switch_jsligo/break_outside_case3.jsligo", line 6, characters 16-21:
-      5 |             if (output == "") {
-      6 |                 break;
-                          ^^^^^
-      7 |             } else {
+File "../../test/contracts/negative/switch_jsligo/break_outside_case3.jsligo", line 4, characters 30-35:
+  3 |   switch (n) {
+  4 |     case 1: if (output == "") break; else output = output + "World";
+                                    ^^^^^
+  5 |     case 2: output = output + "World";
 
-    Break statement is not supported in that position |}]
+Break statement is not supported in that position |}]
 
 let%expect_test _ =
   run_ligo_bad
@@ -112,10 +88,10 @@ let%expect_test _ =
     ];
   [%expect
     {|
-    File "../../test/contracts/negative/switch_jsligo/break_outside_case4.jsligo", line 7, characters 16-21:
-      6 |                 output = output + "World";
-      7 |                 break;
-                          ^^^^^
-      8 |             };
+File "../../test/contracts/negative/switch_jsligo/break_outside_case4.jsligo", line 4, characters 41-46:
+  3 |   switch (n) {
+  4 |     case 1: { output = output + "World"; break; };
+                                               ^^^^^
+  5 |     case 2: output = output + "World"; break;
 
-    Break statement is not supported in that position |}]
+Break statement is not supported in that position |}]
