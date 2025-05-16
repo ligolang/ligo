2359d2a798095f9866e5123d60d4e9537bc2fc26
Christian Rinderknecht <rinderknecht@free.fr>
2025-05-16 18:31:11 +0200

    Removed List.tail_opt

--- src/bin/expect_tests/contract_tests.ml
+++ src/bin/expect_tests/contract_tests.ml
@@ -1127,7 +1127,7 @@ Variable "create_contract" not found. |}];
           ^^^^^^^^^^
      14 |   ([toto.0], store)
 
-    Not all free variables could be inlined in Tezos.create_contract usage: gen#430. |}];
+    Not all free variables could be inlined in Tezos.create_contract usage: gen#429. |}];
   run_ligo_bad [ "compile"; "contract"; bad_contract "create_contract_no_inline.mligo" ];
   [%expect
     {|
@@ -1145,7 +1145,7 @@ Variable "create_contract" not found. |}];
           ^^^^^^^
      15 |   in [op], foo
 
-    Not all free variables could be inlined in Tezos.create_contract usage: foo#443. |}];
+    Not all free variables could be inlined in Tezos.create_contract usage: foo#442. |}];
   run_ligo_good [ "compile"; "contract"; contract "create_contract.mligo" ];
   [%expect
     {|
